7f10ca1314a4cc6efd70bfb02d440155d8ee56b3
Dan Kelley <kelley.dan@gmail.com>
2014-03-29 14:03:59 -0300

    rename oce408*R as 408*R

--- 408/408.R
+++ 408/408.R
@@ -1,4 +1,4 @@
-if (!interactive()) png("oce408.png", width=5, height=4, unit="in", res=150, type='cairo', pointsize=8)
+if (!interactive()) png("408.png", width=5, height=4, unit="in", res=150, type='cairo', pointsize=8)
 library(oce)
 data(section)
 plot(section, xtype="longitude", ztype="image")
