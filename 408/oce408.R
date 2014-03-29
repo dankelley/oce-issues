a10f5b7c26d2b5298fa1173d22b349a2c26488cd
Dan Kelley <kelley.dan@gmail.com>
2014-03-29 12:17:54 -0300

    408

---
+++ 408/oce408.R
@@ -0,0 +1,6 @@
+if (!interactive()) png("oce408.png", width=5, height=4, unit="in", res=150, type='cairo', pointsize=8)
+library(oce)
+data(section)
+plot(section, xtype="longitude", ztype="image")
+if (!interactive()) dev.off()
+
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-dirs=413 414 415
+dirs=408 413 414 415
 all:
 	for dir in $(dirs); do cd $$dir ; make ; cd .. ; done
 clean:
