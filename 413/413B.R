95aa0ff158acf515a993ff07b9c40d42a95d1a89
Dan Kelley <kelley.dan@gmail.com>
2014-04-02 09:33:59 -0300

    better tests of layout() problems

--- 413/413B.R
+++ 413/413B.R
@@ -1,7 +1,22 @@
 if (!interactive()) png("413B.png", width=5, height=5, unit="in", res=150, pointsize=9)
 library(oce)
 #source('~/src/oce/R/imagep.R')
+
+## Try a range of thinner palette regions.  NOTE: the space to the right
+## of the main image is dictated by the initial par("mar"), which is of course
+## not adjusted by this procedure.  Try e.g.
+##       par(mar=c(3, 3, 1, 1))
+## to tighten things up.
+
 d <- 0.14
+d <- 0.13
+d <- 0.12
+d <- 0.11
+d <- 0.10
+d <- 0.09
+d <- 0.08
+d <- 0.07
+
 par(c('mai','pin','fin')) # before layout
 layout(matrix(1:4,nrow=2,byrow=TRUE), widths=c(1-d,d))
 omai <- par('mai')
--- 413/README.md
+++ 413/README.md
@@ -2,5 +2,8 @@
 to thin out the margins to avoid the "margins too wide" error.  The thinning
 depends on font size and device size.
 
-413B.R: test with drawPalette() on RHS
+413B.R: test with drawPalette() on RHS.
+
+STATUS (2014-04-02): both R files work.  See notes in 413B.R regarding the 'd'
+value, and also margins.
 
