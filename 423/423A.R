e8816ebdac43c2b3e319a1a68bdfd1d56976b9d5
Dan Kelley <kelley.dan@gmail.com>
2014-04-12 19:54:35 -0300

    imrove 423 tests

--- 423/423A.R
+++ 423/423A.R
@@ -1,6 +1,5 @@
 ## http://www.timeanddate.com/worldclock/astronomy.html?n=286&month=4&year=2014&obj=sun&afl=-1&day=1
 library(oce)
-source("~/src/oce/R/sun.R")
 lat <- 44.8544
 lon <- -63.1992
 
@@ -8,10 +7,12 @@ lon <- -63.1992
 ## rise 06:55AM local; 08:55 UTC
 rise <- as.POSIXct("2014-04-01 09:55:00", tz="UTC")
 sr <- sunAngle(rise, lon=lon, lat=lat)
+cat("at sunrise, get alt=", sr$altitude, "(expect 0) and azimuth=", sr$azimuth, "(expect 83)\n")
 
 ## fall 7:42PM local; 10:42PM UTC = 22:42 UTC
 fall <- as.POSIXct("2014-04-01 22:42:00", tz="UTC")
 sf <- sunAngle(fall, lon=lon, lat=lat)
+cat("at sunrise, get alt=", sf$altitude, "(expect 0) and azimuth=", sf$azimuth, "(expect 278)\n")
 
 t <- seq(rise-3600, to=fall+3600, by="1 min")
 plot(t, sunAngle(t, lon=lon, lat=lat)$altitude, type='l')
--- 423/423B.R
+++ 423/423B.R
@@ -1,5 +1,4 @@
 library(oce)
-source("~/src/oce/R/sun.R")
 lat <- 44.8544
 lon <- -63.1992
 
@@ -7,10 +6,12 @@ lon <- -63.1992
 ## rise 07:51AM local; 10:51 UTC
 rise <- as.POSIXct("2014-04-01 10:51:00", tz="UTC")
 mr <- moonAngle(rise, lon=lon, lat=lat)
+cat("at moonrise, get alt=", mr$altitude, "(expect 0) and azimuth=", mr$azimuth, "(expect 73)\n")
 
 ## fall 10:06PM=22:06 local; 25:06 UTC or 01:06 on next day
 fall <- as.POSIXct("2014-04-02 01:06:00", tz="UTC")
 mf <- moonAngle(fall, lon=lon, lat=lat)
+cat("at moonfall, get alt=", mf$altitude, "(expect 0) and azimuth=", mf$azimuth, "(expect 290)\n")
 
 t <- seq(rise-3600, to=fall+3600, by="1 min")
 plot(t, moonAngle(t, lon=lon, lat=lat)$altitude, type='l')
