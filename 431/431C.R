a92773cff6c1916058c379f096f87cf1373bd90e
Dan Kelley <kelley.dan@gmail.com>
2014-04-22 14:41:44 -0300

    431 fix error in test

--- 431/431C.R
+++ 431/431C.R
@@ -1,10 +1,14 @@
 library(oce)
 data(topoWorld)
 
-cm <- colormapAlpha(file='http://www.beamreach.org/maps/gmt/share/cpt/GMT_globe.cpt')
+cm1 <- colormapAlpha(name='gmt_relief')
+cm2 <- colormapAlpha(file='http://www.beamreach.org/maps/gmt/share/cpt/GMT_globe.cpt')
 
 if (!interactive()) png("431C.png")
-par(mar=c(3, 3, 1, 1))
-imagep(topoWorld, breaks=cm$breaks, col=cm$col)
+par(mfrow=c(2,1))
+c <- colorizeAlpha(colormap=cm1)
+imagep(topoWorld, breaks=c$breaks, col=c$col)
+c <- colorizeAlpha(colormap=cm2)
+imagep(topoWorld, breaks=c$breaks, col=c$col)
 if (!interactive()) dev.off()
 
