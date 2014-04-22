c5ae63d2dd1c4db1b400e5b4441187d08753a87c
Dan Kelley <kelley.dan@gmail.com>
2014-04-22 14:46:14 -0300

    431

--- 431/431C.R
+++ 431/431C.R
@@ -1,14 +1,20 @@
+## Demonstrate two different colormaps.  
+## Q: is this two-stage process too cumbersome?
+
 library(oce)
 data(topoWorld)
 
+## Set up colormaps and then -- well, what to call 'c1' and 'c2'?  Possibly
+## call them colorscheme, in which case perhaps colorize() should be named
+## colorScheme()???
 cm1 <- colormapAlpha(name='gmt_relief')
+c1 <- colorizeAlpha(colormap=cm1)
 cm2 <- colormapAlpha(file='http://www.beamreach.org/maps/gmt/share/cpt/GMT_globe.cpt')
+c2 <- colorizeAlpha(colormap=cm2)
 
 if (!interactive()) png("431C.png")
 par(mfrow=c(2,1))
-c <- colorizeAlpha(colormap=cm1)
-imagep(topoWorld, breaks=c$breaks, col=c$col)
-c <- colorizeAlpha(colormap=cm2)
-imagep(topoWorld, breaks=c$breaks, col=c$col)
+imagep(topoWorld, breaks=c1$breaks, col=c1$col)
+imagep(topoWorld, breaks=c2$breaks, col=c2$col)
 if (!interactive()) dev.off()
 
