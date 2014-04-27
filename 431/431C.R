7bcb387bac88b65ff982759304ac03849545d722
Dan Kelley <kelley.dan@gmail.com>
2014-04-27 15:17:22 -0300

    431 more tests

--- 431/431C.R
+++ 431/431C.R
@@ -7,9 +7,9 @@ data(topoWorld)
 ## Set up colormaps and then -- well, what to call 'c1' and 'c2'?  Possibly
 ## call them colorscheme, in which case perhaps colorize() should be named
 ## colorScheme()???
-c1 <- colorizeAlpha(colormap='gmt_relief')
-cm2 <- colormapAlpha(file='http://www.beamreach.org/maps/gmt/share/cpt/GMT_globe.cpt')
-c2 <- colorizeAlpha(colormap=cm2)
+c1 <- colorize(colormap='gmt_relief')
+cm2 <- colormap('http://www.beamreach.org/maps/gmt/share/cpt/GMT_globe.cpt')
+c2 <- colorize(colormap=cm2)
 
 if (!interactive()) png("431C.png")
 par(mfrow=c(2,1))
