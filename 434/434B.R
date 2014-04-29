397fd3ca89bc7f96b449a9af0a558d3ab4e41474
Dan Kelley <kelley.dan@gmail.com>
2014-04-29 18:37:28 -0300

    434 fixed

---
+++ 434/434B.R
@@ -0,0 +1,18 @@
+if (!interactive()) png("434B.png", width=7, height=7, unit="in", res=150, pointsize=12)
+
+library(oce)
+cm <- Colormap(name="gmt_globe")
+
+par(mfrow=c(2,1))
+par(mar=c(3,3,1,1))
+plot(seq_along(cm$x0), cm$x0, pch=21, bg=cm$col0, type='p')
+grid()
+points(seq_along(cm$x0), cm$x0, pch=21, bg=cm$col0)
+points(seq_along(cm$x1), cm$x1, pch=21, bg=cm$col1)
+
+plot(seq_along(cm$x0), cm$x0, cex=2, pch=21, bg=cm$col0, type='p', xlim=c(20, 23), ylim=c(-600, 300))
+grid()
+points(seq_along(cm$x0), cm$x0, cex=2, pch=21, bg=cm$col0, type='p')
+points(seq_along(cm$x1), cm$x1, cex=2, pch=21, bg=cm$col1)
+
+if (!interactive()) dev.off()
---
+++ 434/README.md
@@ -0,0 +1,3 @@
+Issue 434: color breaks were wrong on topo example (Hudson's Bay was green, a
+land colour).
+
