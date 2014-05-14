81506fb8b76e91ec9e8af0e8774c01ef4e5c3cd5
Dan Kelley <kelley.dan@gmail.com>
2014-05-14 18:32:23 -0300

    colormap() auto-range extended

--- 441/441.R
+++ 441/441.R
@@ -7,7 +7,7 @@ z <- rnorm(20)
 ## which are detailed, so there is little point in restating them here.  If
 ## the present tests are correct, those conditions are defined by lack of
 ## reported errors from the stopifnot() tests.
-ZLIM <- range(z)
+ZLIM <- rangeExtended(z)
 
 ## There are a *lot* of possible tests.  Either z is given or not.  That's 2
 ## tests.  Either zlim is given or not.  Now up to 4 tests.  Either breaks is
@@ -29,13 +29,13 @@ stopifnot(!any(is.na(cm$zcol)))
 ## 2. breaks alone (illegal to also give zlim)
 cm <- colormap(breaks=seq(0, 3, 0.1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(0, 3)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(0, 3))))
 
 ## 3. name alone or with zlim
 source('~/src/oce/R/colors.R')
 cm <- colormap(name="gmt_globe")
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(-10000, 10000)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(-10000, 10000))))
 cm <- colormap(name="gmt_globe", zlim=c(-1,1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(-1, 1)))
@@ -43,12 +43,12 @@ stopifnot(all.equal(cm$zlim, c(-1, 1)))
 ## 4. breaks plus name (latter ignored since former given)
 cm <- colormap(breaks=0:5, name="gmt_globe")
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(0, 5)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(0, 5))))
 
 ## 5. (x0,col0,x1,col1) alone or with zlim
 cm <- colormap(x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(0, 1.5)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(0, 1.5))))
 cm <- colormap(zlim=c(-10,10), x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(-10, 10)))
@@ -56,12 +56,12 @@ stopifnot(all.equal(cm$zlim, c(-10, 10)))
 ## 6. z plus breaks
 cm <- colormap(z=z, breaks=seq(0, 3, 0.1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(0, 3)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(0, 3))))
 
 ## 7. z plus name, alone or with zlim
 cm <- colormap(z=z, name="gmt_globe")
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(-10000, 10000)))
+stopifnot(all.equal(cm$zlim, rangeExtended(c(-10000, 10000))))
 stopifnot(!any(is.na(cm$zcol)))
 cm <- colormap(z=z, name="gmt_globe", zlim=c(-1,1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
