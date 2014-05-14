db4406d4fdcd56f890a1bfa0aca36f861b119b66
Dan Kelley <kelley.dan@gmail.com>
2014-05-14 15:19:47 -0300

    zlim+breaks issue

--- 441/441.R
+++ 441/441.R
@@ -26,13 +26,10 @@ stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(-10,10)))
 stopifnot(!any(is.na(cm$zcol)))
 
-## 2. breaks alone or with zlim
+## 2. breaks alone (illegal to also give zlim)
 cm <- colormap(breaks=seq(0, 3, 0.1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(0, 3)))
-cm <- colormap(zlim=c(-1,1), breaks=seq(0, 3, 0.1))
-stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(-1, 1)))
 
 ## 3. name alone or with zlim
 source('~/src/oce/R/colors.R')
@@ -43,13 +40,10 @@ cm <- colormap(name="gmt_globe", zlim=c(-1,1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(-1, 1)))
 
-## 4. breaks plus name (latter ignored since former given), alone or with zlim
+## 4. breaks plus name (latter ignored since former given)
 cm <- colormap(breaks=0:5, name="gmt_globe")
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(0, 5)))
-cm <- colormap(zlim=c(-3,3), breaks=0:5, name="gmt_globe")
-stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(-3, 3)))
 
 ## 5. (x0,col0,x1,col1) alone or with zlim
 cm <- colormap(x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
@@ -59,14 +53,10 @@ cm <- colormap(zlim=c(-10,10), x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5),
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(-10, 10)))
 
-## 6. z plus breaks, alone or with zlim
+## 6. z plus breaks
 cm <- colormap(z=z, breaks=seq(0, 3, 0.1))
 stopifnot(length(cm$breaks) == 1 + length(cm$col))
 stopifnot(all.equal(cm$zlim, c(0, 3)))
-cm <- colormap(z=z, breaks=seq(0, 3, 0.1), zlim=c(-1,1))
-stopifnot(length(cm$breaks) == 1 + length(cm$col))
-stopifnot(all.equal(cm$zlim, c(-1, 1)))
-stopifnot(!any(is.na(cm$zcol)))
 
 ## 7. z plus name, alone or with zlim
 cm <- colormap(z=z, name="gmt_globe")
