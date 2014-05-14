214ebcf91329e55f06bd34d2482245dd87b6273f
Dan Kelley <kelley.dan@gmail.com>
2014-05-14 12:32:05 -0300

    443

--- 443/443.R
+++ 443/443.R
@@ -1,5 +1,5 @@
 library(oce)
-
+source('~/src/oce/R/colors.R')
 ## extract some data to use
 data(adp)
 t <- adp[['time']]
@@ -11,11 +11,14 @@ spd <- sqrt(u^2 + v^2)
 
 ## ok, lets use it to plot a time series of pressure, colored for pressure
 par(mfrow=c(2,1))
-cm <- colormap(p)
+omar <- par('mar')
+cm <- colormap(p, debug=3)
+unique(cm$zcol)
 drawPalette(colormap=cm)
-oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=c(3, 3, 1, 4))
+oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=par('mar'))
 
 ## now where I specify the breaks
+par(mar=omar)
 cm <- colormap(p, breaks=seq(39, 42, length.out=31))
 drawPalette(colormap=cm)
-oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=c(3, 3, 1, 4))
+oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=par('mar'))
