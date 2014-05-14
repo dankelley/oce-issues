02cd14d5b007db9a923a4982982e26df7c5c7f87
Dan Kelley <kelley.dan@gmail.com>
2014-05-14 11:22:15 -0300

    441 more tests

--- 441/441.R
+++ 441/441.R
@@ -3,32 +3,95 @@ source('~/src/oce/R/colors.R')
 set.seed(441)
 z <- rnorm(20)
 
-## Part 1: test zlim in various conditions.  See the docs for
-## those conditions, which are labourious to state here, and there
-## is no point because all that matters is the docs and whether
-## they are obeyed, and the tests below are intended to do the
-## latter.
+## Part 1: test zlim in various conditions.  The docs explain those conditions,
+## which are detailed, so there is little point in restating them here.  If
+## the present tests are correct, those conditions are defined by lack of
+## reported errors from the stopifnot() tests.
 ZLIM <- range(z)
-message("ZLIM: ", paste(range(z), collapse=" to "))
-cm1 <- colormap(z=z)
-stopifnot(all.equal(cm1$zlim, ZLIM))
-cm2 <- colormap(z=z, breaks=seq(0, 3, 0.1))
-stopifnot(all.equal(cm2$zlim, c(0, 3)))
-cm3 <- colormap(z=z, zlim=c(-5, 5), breaks=seq(0, 3, 0.1))
-stopifnot(all.equal(cm3$zlim, c(-5, 5)))
-cm4 <- colormap(z=z, name="gmt_globe")
-stopifnot(all.equal(cm4$zlim, c(-10000, 10000)))
 
-x <- seq(0, 1, length.out=40)
-y <- sin(2 * pi * x)
-cm5 <- colormap(y, breaks=100)
-stopifnot(all.equal(cm5$zlim, range(y)))
+## There are a *lot* of possible tests.  Either z is given or not.  That's 2
+## tests.  Either zlim is given or not.  Now up to 4 tests.  Either breaks is
+## given or not, now up to 8 tests.  Ditto name for 16 tests.  Ditto x0 for 32
+## tests.  Of course some cases do not apply, e.g. it's an error to give both
+## breaks and name.  Still, a *lot* of tests.  Hopefully bugs will be found
+## from a subset, because there are other bugs of high priority right now...
 
-## Below should rais an error, and it does, so no need to interrupt the flow
+## 1. z alone or with zlim
+cm <- colormap(z=z)
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, ZLIM))
+stopifnot(!any(is.na(cm$zcol)))
+cm <- colormap(z=z, zlim=c(-10,10))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-10,10)))
+stopifnot(!any(is.na(cm$zcol)))
+
+## 2. breaks alone or with zlim
+cm <- colormap(breaks=seq(0, 3, 0.1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(0, 3)))
+cm <- colormap(zlim=c(-1,1), breaks=seq(0, 3, 0.1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-1, 1)))
+
+## 3. name alone or with zlim
+source('~/src/oce/R/colors.R')
+cm <- colormap(name="gmt_globe")
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-10000, 10000)))
+cm <- colormap(name="gmt_globe", zlim=c(-1,1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-1, 1)))
+
+## 4. breaks plus name (latter ignored since former given), alone or with zlim
+cm <- colormap(breaks=0:5, name="gmt_globe")
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(0, 5)))
+cm <- colormap(zlim=c(-3,3), breaks=0:5, name="gmt_globe")
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-3, 3)))
+
+## 5. (x0,col0,x1,col1) alone or with zlim
+cm <- colormap(x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(0, 1.5)))
+cm <- colormap(zlim=c(-10,10), x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-10, 10)))
+
+## 6. z plus breaks, alone or with zlim
+cm <- colormap(z=z, breaks=seq(0, 3, 0.1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(0, 3)))
+cm <- colormap(z=z, breaks=seq(0, 3, 0.1), zlim=c(-1,1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-1, 1)))
+stopifnot(!any(is.na(cm$zcol)))
+
+## 7. z plus name, alone or with zlim
+cm <- colormap(z=z, name="gmt_globe")
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-10000, 10000)))
+stopifnot(!any(is.na(cm$zcol)))
+cm <- colormap(z=z, name="gmt_globe", zlim=c(-1,1))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-1, 1)))
+stopifnot(!any(is.na(cm$zcol)))
+
+## 8. z plus (x0,col0,x1,col1) alone [z wins] or with zlim [zlim wins]
+cm <- colormap(z=z, x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, ZLIM))
+stopifnot(!any(is.na(cm$zcol)))        # FIXME
+cm <- colormap(z, zlim=c(-10,10), x0=c(0,1), col0=c('red', 'blue'), x1=c(0.5, 1.5), col1=c("pink", "green"))
+stopifnot(length(cm$breaks) == 1 + length(cm$col))
+stopifnot(all.equal(cm$zlim, c(-10, 10)))
+stopifnot(!any(is.na(cm$zcol)))        # FIXME
+
+## Below should raise an error, and it does, so no need to interrupt the flow
 ## here with an error!
 ##try(str(colormap(zclip=TRUE)))
 
 ## Part 2: test whether clipping is obeyed
-
 
 
