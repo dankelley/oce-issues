f32bcd1d3968751ff03b41b7c4af82b1d129c299
Dan Kelley <kelley.dan@gmail.com>
2014-05-14 19:14:52 -0300

    447 ok now

--- 447/447.R
+++ 447/447.R
@@ -1,15 +1,17 @@
 library(oce)
-source('~/src/oce/R/colors.R')
+# source('~/src/oce/R/colors.R')
 data(adp)
 t <- adp[['time']]
 p <- adp[['pressure']]
-par(mfrow=c(2,1))
+par(mfrow=c(2,1), mar=c(3, 3, 1, 1))
 cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=TRUE)
+stopifnot(!any(is.na(cm$zcol)))
 drawPalette(colormap=cm)
-oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
-## note the white circles above the breaks
+oce.plot.ts(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
+abline(h=c(39, 40))
 
 ## change the missingColor to pink
-cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=TRUE, missingColor='pink')
+cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=FALSE, missingColor='pink')
 drawPalette(colormap=cm)
-oce.plot.ts(t, p, type='p', pch=21, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
+oce.plot.ts(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
+abline(h=c(39, 40))
