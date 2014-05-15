051a61b79806f2e3e390f7eb3e219968dedd4a5e
Dan Kelley <kelley.dan@gmail.com>
2014-05-15 06:10:24 -0300

    447 possibly fixed

--- 447/447b.R
+++ 447/447b.R
@@ -1,17 +1,21 @@
+if (!interactive()) pdf("447b.pdf")
 library(oce)
-# source('~/src/oce/R/colors.R')
+## source('~/src/oce/R/colors.R')
 data(adp)
 t <- adp[['time']]
 p <- adp[['pressure']]
 par(mfrow=c(2,1), mar=c(3, 3, 1, 1))
+## pink missingColor
 cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=FALSE, missingColor='pink')
 stopifnot(!any(is.na(cm$zcol)))
 drawPalette(colormap=cm)
 oce.plot.ts(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
 abline(h=c(39, 40))
 
-## change the missingColor to pink
+## default (gray) missingColor
 cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=FALSE)
 drawPalette(colormap=cm)
 oce.plot.ts(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol, mar=c(3, 3, 1, 4)) 
 abline(h=c(39, 40))
+if (!interactive()) dev.off()
+
