34d5c6659cf33fde166eca4236022e94b3bdebf8
Dan Kelley <kelley.dan@gmail.com>
2014-05-15 19:08:41 -0300

    working on 449 (broken)

--- 449/449.R
+++ 449/449.R
@@ -1,27 +1,30 @@
 if (!interactive()) pdf("449.pdf")
 library(oce)
-## source('~/src/oce/R/colors.R')
+source('~/src/oce/R/colors.R')
 data(adp)
 t <- adp[['time']]
 p <- adp[['pressure']]
-par(mfcol=c(2,2), mar=c(3, 3, 1, 1))
+#par(mfcol=c(2,2), mar=c(3, 3, 1, 1))
+par(mar=c(3, 3, 1, 1))
+par(mfcol=c(2,1))
 
 omar <- par('mar')
-cm <- colormap(p, breaks=seq(39, 40, 0.1), debug=3)
-stopifnot(!any(is.na(cm$zcol)))
-drawPalette(colormap=cm)
-plot(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol) 
+if (T){
+cm1 <- colormap(p, breaks=seq(39, 40, 0.1), debug=3)
+stopifnot(!any(is.na(cm1$zcol)))
+drawPalette(colormap=cm1)
+plot(t, p, type='p', pch=21, cex=1.5, bg=cm1$zcol) 
 abline(h=c(39, 40))
 mtext("TEST 1 (zclip=FALSE)", side=3)
-
+}
 par(mar=omar)
-cm <- colormap(p, zlim=c(39, 40))
-stopifnot(!any(is.na(cm$zcol)))
-drawPalette(colormap=cm)
-plot(t, p, type='p', pch=21, cex=1.5, bg=cm$zcol) 
+cm2 <- colormap(p, zlim=c(39, 40), debug=3)
+stopifnot(!any(is.na(cm2$zcol)))
+drawPalette(colormap=cm2, debug=4)
+plot(t, p, type='p', pch=21, cex=1.5, bg=cm2$zcol) 
 abline(h=c(39, 40))
 mtext("TEST 2 (zclip=FALSE)", side=3)
-
+stop()
 par(mar=omar)
 cm <- colormap(p, breaks=seq(39, 40, 0.1), zclip=TRUE, debug=3)
 stopifnot(!any(is.na(cm$zcol)))
