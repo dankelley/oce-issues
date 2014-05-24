bee03b8cb40e9f23cf0a6222734c9ea49ab2fa21
Dan Kelley <kelley.dan@gmail.com>
2014-05-24 09:25:38 -0300

    458 fixed

--- 458/458a.R
+++ 458/458a.R
@@ -1,8 +1,10 @@
 library(oce)
-source('~/src/oce/R/adp.rdi.R')
+# source('~/src/oce/R/adp.rdi.R')
+if (!interactive()) png("458a_%d.png", width=7, height=7, unit="in", res=150, pointsize=11)
 for (file in list.files(pattern="^T1_.*000$")) {
-    cat('about to read:', file, '\n')
-    d <- read.adp.rdi(file, debug=4)
-    cat(' ... read OK\n')
+    d <- read.adp.rdi(file, debug=0)
+    plot(toEnu(d))
 }
+
+if (!interactive()) dev.off()
 
--- 458/458b.R
+++ 458/458b.R
@@ -1,7 +1,9 @@
 library(oce)
+# source('~/src/oce/R/adp.rdi.R')
+if (!interactive()) png("458b_%d.png", width=7, height=7, unit="in", res=150, pointsize=11)
 for (file in rev(list.files(pattern="^T1_.*000$"))) {
-    cat('about to read:', file, '\n')
-    d <- read.oce(file, debug=6)
-    cat(' ... read OK\n')
+    d <- read.adp.rdi(file, debug=0)
+    plot(toEnu(d))
 }
+if (!interactive()) dev.off()
 
