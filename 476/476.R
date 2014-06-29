c2aa511090f20daac8f86098fa8784c04ca822d3
Dan Kelley <kelley.dan@gmail.com>
2014-06-29 11:48:36 -0300

    476

--- 476/476.R
+++ 476/476.R
@@ -3,15 +3,15 @@ try({
     source("~/src/oce/R/map.R")
 })
 halifax <- list(latitude=44.6478, longitude=-63.5714)
-for (proj in c("mercator", "stereographic")) {
-    map <- mapproject(halifax$longitude, halifax$latitude, proj=proj)
+projections <- c("mercator", "stereographic")
+err <- matrix(nrow=length(projections), ncol=2)
+for (i in seq_along(projections)) {
+    map <- mapproject(halifax$longitude, halifax$latitude, proj=projections[i])
     lonlat <- map2lonlat(map$x, map$y)
-    errLon <- abs(lonlat$longitude - halifax$longitude)
-    errLat <- abs(lonlat$latitude - halifax$latitude)
-    message("projection: ", proj,
-            ", errLon: ", format(errLon, digits=2),
-            ", errLan: ", format(errLat, digits=2))
-    allow <- 10 / 111e3                    # 10 m
-    stopifnot(errLon < allow)
-    stopifnot(errLat < allow)
+    err[i, 1] <- abs(lonlat$longitude - halifax$longitude)
+    err[i, 2] <- abs(lonlat$latitude - halifax$latitude)
 }
+rownames(err) <- projections
+colnames(err) <- c("Lon Err", "Lat Err")
+print(err)
+message("RMS error: ", format(sqrt(mean(err^2)), digits=2), " deg with default tolerance.")
