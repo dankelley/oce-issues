1d56bd5d56def730d51633784f9ff6562007e454
Dan Kelley <kelley.dan@gmail.com>
2014-06-29 11:15:49 -0300

    476: more projections

--- 476/476.R
+++ 476/476.R
@@ -3,13 +3,15 @@ try({
     source("~/src/oce/R/map.R")
 })
 halifax <- list(latitude=44.6478, longitude=-63.5714)
-map <- mapproject(halifax$longitude, halifax$latitude, proj="mercator")
-lonlat <- map2lonlat(map$x, map$y)
-errLon <- abs(lonlat$longitude - halifax$longitude)
-errLat <- abs(lonlat$latitude - halifax$latitude)
-message("errLon: ", errLon)
-message("errLan: ", errLat)
-allow <- 10 / 111e3                    # 10 m
-stopifnot(errLon < allow)
-stopifnot(errLat < allow)
-
+for (proj in c("mercator", "stereographic")) {
+    map <- mapproject(halifax$longitude, halifax$latitude, proj=proj)
+    lonlat <- map2lonlat(map$x, map$y)
+    errLon <- abs(lonlat$longitude - halifax$longitude)
+    errLat <- abs(lonlat$latitude - halifax$latitude)
+    message("projection: ", proj,
+            ", errLon: ", format(errLon, digits=2),
+            ", errLan: ", format(errLat, digits=2))
+    allow <- 10 / 111e3                    # 10 m
+    stopifnot(errLon < allow)
+    stopifnot(errLat < allow)
+}
