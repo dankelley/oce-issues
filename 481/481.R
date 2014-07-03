e78255e174a1d66787d2b87508654ff154416ac1
Dan Kelley <kelley.dan@gmail.com>
2014-07-03 10:16:30 -0300

    481

---
+++ 481/481.R
@@ -0,0 +1,12 @@
+if (!interactive()) png("481.png")
+library(oce)
+try({
+    source('~/src/oce/R/section.R')
+})
+data(section)
+GS <- subset(section, 109<=stationId&stationId<=129)
+GS <- sectionSort(GS, by="longitude")
+GSG <- sectionGrid(GS, p=seq(0, 1600, 25))
+plot(GSG, which=c(1,99), map.xlim=c(-80,-60))
+if (!interactive()) dev.off()
+
