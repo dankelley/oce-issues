a8faaefa72c41e4af78159ff47b1ed35b2905503
Dan Kelley <kelley.dan@gmail.com>
2014-07-04 15:06:54 -0300

    482

--- 482/482.R
+++ 482/482.R
@@ -6,6 +6,10 @@ ML <- l@metadata$header$radiance_mult_band_10
 AL <- l@metadata$header$radiance_add_band_10
 K1 <- l@metadata$header$k1_constant_band_10
 K2 <- l@metadata$header$k2_constant_band_10
+print(ML)
+print(ML)
+print(K1)
+print(K2)
 d <- tirs1 * (2^16 - 1)            # convert from range 0 to 1
 Llambda <- ML * d + AL
 dd <- K2 / log(K1 / Llambda + 1)
