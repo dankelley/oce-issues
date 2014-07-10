33ff9d19a2bb31aa82be5901f85945cddab81534
Dan Kelley <kelley.dan@gmail.com>
2014-07-10 15:25:22 -0300

    486

---
+++ 486/486B.R
@@ -0,0 +1,18 @@
+library(oce)
+## Winter-time Nova Scotia and surrounding seas
+ns <- read.landsat("~/google_drive/LC80080292014065LGN00", band="tirs1")
+tirs1 <- ns[["tirs1"]]
+ML <- ns@metadata$header$radiance_mult_band_10
+AL <- ns@metadata$header$radiance_add_band_10
+K1 <- ns@metadata$header$k1_constant_band_10
+K2 <- ns@metadata$header$k2_constant_band_10
+Llambda <- ML * tirs1 + AL
+d <- K2 / log(K1 / Llambda + 1)
+temperature <- d - 273.15
+temperature[tirs1 == 0] <- NA
+ns <- landsatAdd(ns, temperature, "temperature")
+if (!interactive()) png("486B.png", width=7, height=7, unit="in", res=100)
+plot(ns, band="temperature", col=oceColorsJet)
+title(ns[['time']])
+if (!interactive()) dev.off()
+
