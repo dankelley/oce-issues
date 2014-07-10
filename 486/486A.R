38e5117512b3eb43d700a8f57e284eda72b35e1b
Dan Kelley <kelley.dan@gmail.com>
2014-07-10 10:36:21 -0300

    landsat

--- 486/486A.R
+++ 486/486A.R
@@ -1,27 +1,14 @@
-if (!interactive()) png("486.png")
+if (!interactive()) png("486_%d.png")
 library(oce)
 try({
     source('~/src/oce/R/landsat.R')
 })
-l <- read.landsat("~/google_drive/LC80080292014065LGN00", debug=1)
+l <- read.landsat("~/google_drive/LC80080292014065LGN00")
 
 landsat <- landsatTrim(l, 
                        list(longitude=-64.572, latitude=45.295),
                        list(longitude=-64.481, latitude=45.363))
-plot(landsat, band='aerosol')
-plot(landsat, band=1) # FAILS
+for (b in 1:11)
+    plot(landsat, band=b)
 
-save(landsat, file="landsat.rda")
-library(tools)
-resaveRdaFiles("landsat.rda")
-# 
-# dim <- dim(landsat[["band", "panchromatic"]])
-# if (!interactive()) png("landsat.png", width=dim[1], height=dim[2], pointsize=24)
-# ## Cape Split from openstreetmap
-# CS <- list(longitude=-mean(c(64.4924,64.4896)),latitude=mean(c(45.3277, 45.3296)))
-# CSUTM <- lonlat2utm(CS, zone=landsat[["zoneUTM"]], km=TRUE)
-# 
-# plot(landsat, band="panchromatic")
-# points(CS$longitude, CS$latitude, bg='pink', pch=22, lwd=2, cex=3)
-# 
 if (!interactive()) dev.off()
