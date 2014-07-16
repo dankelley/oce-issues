49ec47b6b0796dc800da14fe9d0bfdae715a0c73
Dan Kelley <kelley.dan@gmail.com>
2014-07-16 07:03:25 -0300

    imagep

--- 489/489A.R
+++ 489/489A.R
@@ -1,8 +1,8 @@
 rm(list=ls())
 library(oce)
-#try({
+try({
     source('~/src/oce/R/imagep.R')
-#})
+})
 
 
 data(adp)
@@ -14,12 +14,15 @@ if (!interactive()) png('489A.png')
 
 col <- colorRampPalette(c("white", "#ffe45e", "#FF7F00", "red", "#7F0000"))
 breaks <- seq(0, 1.5, 0.25)
-par(mfrow=c(2,1))
+par(mfrow=c(2,1), mar=c(3, 3, 4, 1))
 imagep(t, d, spd, col=col, filledContour = TRUE, breaks=breaks, debug =1)
 oceContour(t, d, spd, levels=breaks[2], add=TRUE)
 
 cm <- colormap(spd, breaks=breaks, col=col)
 imagep(t, d, spd, colormap=cm, filledContour = TRUE, debug =1)
 oceContour(t, d, spd, levels=breaks[2], add=TRUE)
+
+mtext("EXPECT: colors in both panels should match; lower is correct", line=1,
+      col="red", font=2, cex=1.2)
 
 if (!interactive()) dev.off()
--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ dirs=356 402 406 408 409 412 413 414 415 416 418 420 421 423 424\
      448 449 450 451 452 453 454 456 458 459 462 464 465 475 479\
      481 482 490
 colormapdirs=431 434 435 437 441 443 447 449 450 452 453 454
-imagep=490
+imagep=489 490
 # Exceptions (since requires private data): 478 486
 
 all:
 	for dir in $(dirs); do cd $$dir ; make ; cd .. ; done
 imagep:
-	for dir in $(imagep); do cd $$dir ; make ; make view ; cd .. ; done
+	for dir in $(imagep); do cd $$dir ; make clean ; make ; make view ; cd .. ; done
 clean:
 	-rm *~
 	for dir in $(dirs); do cd $$dir ; make clean ; cd .. ; done
