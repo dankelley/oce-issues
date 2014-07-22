578b509ffde133b05f5b770c6ef2f98673584cdd
Dan Kelley <kelley.dan@gmail.com>
2014-07-22 20:13:27 -0300

    484

--- 484/484.R
+++ 484/484.R
@@ -5,13 +5,13 @@ try({
     source("~/src/oce/R/imagep.R")
 })
 if (!exists("l")) { # cache if rerunning interactively
-    l <- read.landsat('~/google_drive/LC80080292014065LGN00', band=8)
+    system.time(l <- read.landsat('~/google_drive/LC80080292014065LGN00', band='pan'))
 }
 ## below are some tests of new indexing scheme
 ##    d <- l[["pan", 10]]
 ##    d <- l[["pan", TRUE]]
-plot(l)
-plot(l, decimate=200) # really blocky so can see if worked
+system.time(plot(l))
+system.time(plot(l, decimate=200)) # really blocky so can see if worked
 ## Next will probably choke a 4GB machine but it's OK on an 8GB machine
 ## plot(l, decimate=FALSE) #very fine
 if (!interactive()) dev.off()
