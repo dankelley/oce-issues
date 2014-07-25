551e0cca812fa96c465a713a1fcebf1fbf5bb40f
Dan Kelley <kelley.dan@gmail.com>
2014-07-25 09:50:06 -0300

    landsat

--- 501/501.R
+++ 501/501.R
@@ -3,7 +3,7 @@ library(oce)
 try({
     source("~/src/oce/R/landsat.R")
 })
-d <- read.landsat("~/google_drive/LE71910202005194ASN00",band="panchromatic", debug=3)
+d <- read.landsat("~/google_drive/LE71910202005194ASN00",band="panchromatic",debug=3)
 plot(d)
 if (!interactive()) dev.off()
 
