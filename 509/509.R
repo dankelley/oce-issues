4eccb6169be640959130e2336ce6acfb84f5424f
Dan Kelley <kelley.dan@gmail.com>
2014-07-28 07:20:50 -0300

    509

---
+++ 509/509.R
@@ -0,0 +1,13 @@
+if (!interactive()) png("509.R")
+library(oce)
+try({
+    source("~/src/oce/R/landsat.R")
+})
+options(oceDebug=3)
+l <- read.landsat('/data/archive/landsat/LE71910202005194ASN00', band="red")
+im <- l[["red"]] / 2^16-1
+landsatAdd(l, im, 'tc')
+summary(l)
+plot(l, band="tc")
+if (!interactive()) dev.off()
+
