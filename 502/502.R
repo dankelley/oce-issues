2a5e005f85bf10a9d4625201be0b5267a061749a
Dan Kelley <kelley.dan@gmail.com>
2014-07-28 09:44:10 -0300

    502

--- 502/502.R
+++ 502/502.R
@@ -1,10 +1,11 @@
+## https://github.com/dankelley/oce-issues/blob/master/502/502.R
 if (!interactive()) png("502.png")
 library(oce)
 try({
     source("~/src/oce/R/landsat.R")
 })
-d <- read.landsat("/data/archive/landsat/LE71910202005194ASN00",
-                  band=c("red", "green", "blue"), debug=3)
+
+d <- read.landsat("/data/archive/landsat/LC80130272014148LGN00", band=c("red", "green", "nir"), debug=3)
 plot(d, col="natural")
 if (!interactive()) dev.off()
 
