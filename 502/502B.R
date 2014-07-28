32d9c6476ec71641b6fced304cc92cbd3ca454f7
Dan Kelley <kelley.dan@gmail.com>
2014-07-28 19:01:21 -0300

    502

--- 502/502B.R
+++ 502/502B.R
@@ -1,3 +1,4 @@
+## Landsat 8
 if (!interactive()) png("502B.png")
 library(oce)
 try({
@@ -5,8 +6,8 @@ try({
 })
 d <- read.landsat('/data/archive/landsat/LC80130272014148LGN00', band=c('red', 'green', 'nir'), debug=3)
 d <- decimate(d, by=20)
-# options(landsat=1) # TEST -- try 1 through 5 (see R/landsat.R)
-plot(d, col='natural', debug=3)
+options(oceDebug=1) # also reaches into [["nir"]] etc
+source("~/src/oce/R/landsat.R");system.time(plot(d, col='natural'))
 mtext(paste(d[["id"]], d[["time"]]), font=2, col='purple')
 if (!interactive()) dev.off()
 
