88d0f0c5c304760efb8fa4d49409b83a7e672db3
Dan Kelley <kelley.dan@gmail.com>
2014-07-29 10:35:35 -0300

    510

--- 510/510.R
+++ 510/510.R
@@ -1,8 +1,8 @@
 library(oce)
-try({
-    source("~/src/oce/R/drifter.R")
-    source("~/src/oce/R/oce.R")
-})
+## try({
+##     source("~/src/oce/R/drifter.R")
+##     source("~/src/oce/R/oce.R")
+## })
 d <- read.oce("1900617_prof.nc")
 dg <- drifterGrid(d) ## was giving an error in approx()
 
