b70590891cdcc61c18d5014cf123de0f14200f09
Dan Kelley <kelley.dan@gmail.com>
2014-07-29 13:37:15 -0300

    510

---
+++ 510/510C.R
@@ -0,0 +1,11 @@
+library(oce)
+source('~/src/oce/R/drifter.R')
+d <- read.oce("6901067_prof.nc")
+g <- drifterGrid(d, p=seq(0, 100, 1))
+par(mfrow=c(2,1))
+t <- g[["time"]]
+z <- -g[["pressure"]][,1]
+imagep(t, z, t(g[['temperature']]), ylim=c(-100,0), zlim=c(0,20))
+imagep(t, z, t(g[['salinity']]), ylim=c(-100,0))
+
+
