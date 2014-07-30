08d643ff908cb329988ddbea431f375ed05adaa6
Dan Kelley <kelley.dan@gmail.com>
2014-07-30 06:57:03 -0300

    513

--- 513/513.R
+++ 513/513.R
@@ -4,5 +4,7 @@ try({
     source("~/src/oce/R/landsat.R")
 })
 d <- read.landsat('/data/archive/landsat/LC80130272014148LGN00', band='red')
+d <- decimate(d, by=33) # because temp plot does not decimate and is SLOW
+#options(oceDebug=2) # get debugging in [["temperature"]]
 plot(d, band="temperature")
 
