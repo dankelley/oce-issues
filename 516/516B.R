87646a09f425dd8bcbbf57fa4d082db90fc0f0e4
Dan Kelley <kelley.dan@gmail.com>
2014-08-02 10:30:22 -0300

    516B.R

--- 516/516B.R
+++ 516/516B.R
@@ -11,10 +11,10 @@ data(topoWorld)
 par(mfrow=c(2,1), mar=c(1, 1, 2, 1))
 mapPlot(coastlineWorld)
 mapImage(topoWorld, breaks=seq(-2000, 2000, 500))
-mtext(paste('EXPECT: colors NOT clipped to c(-2000, 2000) using breaks'), col=6, font=2)
+mtext(paste('EXPECT: deep water blue, high land red-brown'), col=6, font=2)
 mapPlot(coastlineWorld)
 mapImage(topoWorld, zlim=c(-2000, 2000))
-mtext(paste('EXPECT: identical to above'), col=6, font=2)
+mtext(paste('EXPECT: as above but different color transitions'), col=6, font=2)
 
 if (!interactive()) dev.off()
 
