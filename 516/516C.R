ecc0c2b19e5f56ca86d3f0af3504dd2487d3edff
Dan Kelley <kelley.dan@gmail.com>
2014-08-03 07:54:42 -0300

    516C.R title clarification

--- 516/516C.R
+++ 516/516C.R
@@ -13,21 +13,21 @@ par(mfrow=c(2,2), mar=c(1, 1, 2, 1))
 
 mapPlot(coastlineWorld)
 mapImage(topoWorld, breaks=seq(-2000, 2000, 500), debug=3)
-mtext(paste('EXPECT: deep=blue, mountain=red-brown'), col=6, font=2)
-mtext('(a) ', line=-1.2, adj=1)
+mtext(paste('EXPECT: as (c) but projected'), col=6, font=2)
+mtext('(a)', line=0.2, adj=1)
 
 mapPlot(coastlineWorld)
 mapImage(topoWorld, zlim=c(-2000, 2000))
-mtext(paste('EXPECT: as (a) but different color transitions'), col=6, font=2)
-mtext('(b) ', line=-1.2, adj=1)
+mtext(paste('EXPECT: as (d) but projected'), col=6, font=2)
+mtext('(b)', line=0.2, adj=1)
 
 imagep(topoWorld, breaks=seq(-2000, 2000, 500), debug=3)
-mtext(paste('EXPECT: same as (a) but not projected'), col=6, font=2)
-mtext('(c) ', line=-1.2, adj=1)
+mtext(paste('EXPECT: 500-m colour bands'), col=6, font=2)
+mtext('(c)', line=0.2, adj=1)
 
 imagep(topoWorld, zlim=c(-2000, 2000))
-mtext(paste('EXPECT: as (c) but not projected'), col=6, font=2)
-mtext('(d) ', line=-1.2, adj=1)
+mtext(paste('EXPECT: blending colours'), col=6, font=2)
+mtext('(d)', line=0.2, adj=1)
 
 if (!interactive()) dev.off()
 
