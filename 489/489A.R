5fc2bdec0814b519338518ef5e97d3527ed028b0
Dan Kelley <kelley.dan@gmail.com>
2014-08-03 14:56:40 -0300

    489.R simplified and clarified

--- 489/489A.R
+++ 489/489A.R
@@ -9,20 +9,26 @@ data(adp)
 t <- adp[['time']]
 d <- adp[['distance']]
 spd <- sqrt(adp[['v']][,,1]^2 + adp[['v']][,,2]^2)
+spd[10:40, 1:30] <- 0
+spd[10:40, 60:84] <- 1.1
 
 if (!interactive()) png('489A.png')
 
+par(mfrow=c(2,1), mar=c(3, 3, 5, 1))
+
 col <- colorRampPalette(c("white", "#ffe45e", "#FF7F00", "red", "#7F0000"))
-breaks <- seq(0, 1.5, 0.25)
-par(mfrow=c(2,1), mar=c(3, 3, 4, 1))
-imagep(t, d, spd, col=col, filledContour = TRUE, breaks=breaks)
-oceContour(t, d, spd, levels=breaks[2], add=TRUE)
-
+breaks <- seq(0, 1.5, 0.2)
 cm <- colormap(spd, breaks=breaks, col=col)
-imagep(t, d, spd, colormap=cm, filledContour = TRUE)
+imagep(t, d, spd, colormap=cm, filledContour=TRUE,
+       mar=c(2, 2, 2, 0.5), drawTimeRange=FALSE)
 oceContour(t, d, spd, levels=breaks[2], add=TRUE)
+mtext("EXPECT: v=0 box at bottom, v=1 at top", adj=0, col="purple", font=2)
+mtext("(a) ", adj=1)
 
-mtext("EXPECT: colors in both panels should match; lower is correct", line=1,
-      col="purple", font=2, cex=1.2)
+imagep(t, d, spd, col=col, filledContour=TRUE, breaks=breaks,
+       mar=c(2, 2, 2, 0.5), drawTimeRange=FALSE, debug=4)
+oceContour(t, d, spd, levels=breaks[2], add=TRUE)
+mtext("EXPECT: as (a)", adj=0, col="purple", font=2)
+mtext("(b) ", adj=1)
 
 if (!interactive()) dev.off()
