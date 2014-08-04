9c4d7b64793016834239521fa9d37fc12cbd5b17
Clark Richards <clark.richards@gmail.com>
2014-08-03 23:08:22 -0400

    create 517

---
+++ 517/517A.R
@@ -0,0 +1,33 @@
+library(oce)
+data(topoWorld)
+data(coastlineWorld)
+## source('~/src/R-richards/oce/R/colors.R')
+## source('~/src/R-richards/oce/R/map.R')
+
+if (!interactive()) png('01.png', width=1000, height=700, type='cairo')
+par(mfrow=c(2,2), mar=c(2,2,1,1))
+
+mapPlot(coastlineWorld)
+mapImage(topoWorld, col=oceColorsJet, breaks=seq(-2000, 2000, 500))
+mtext('EXPECT: dark blue < -2000 dark red > 2000', col=6)
+mtext('(a)', adj=1)
+
+mapPlot(coastlineWorld)
+mapImage(topoWorld, col=oceColorsJet, breaks=seq(-2000, 2000, 500), zclip=TRUE, missingColor = 'grey')
+mtext('EXPECT: grey for abs(z) > 2000', col=6)
+mtext('(b)', adj=1)
+
+cm <- colormap(topoWorld[['z']], col=oceColorsJet, breaks=seq(-2000, 2000, 500), debug=10)
+mapPlot(coastlineWorld)
+mapImage(topoWorld, colormap=cm, debug=10)
+mtext('EXPECT: same as (a)', col=6)
+mtext('(c)', adj=1)
+
+cm <- colormap(topoWorld[['z']], col=oceColorsJet, breaks=seq(-2000, 2000, 500), zclip=TRUE, debug=10)
+mapPlot(coastlineWorld)
+mapImage(topoWorld, colormap=cm, debug=10)
+mtext('EXPECT: same as (b)', col=6)
+mtext('(d)', adj=1)
+
+
+if (!interactive()) dev.off()
