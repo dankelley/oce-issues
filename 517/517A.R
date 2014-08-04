7c3037af59237c40070baa6925d85e11b4a07491
Dan Kelley <kelley.dan@gmail.com>
2014-08-04 08:58:56 -0300

    517

--- 517/517A.R
+++ 517/517A.R
@@ -1,34 +1,41 @@
 library(oce)
 data(topoWorld)
-topoWorld <- decimate(topoWorld, by=3) # speed plotting 10X
+#topoWorld <- decimate(topoWorld, by=3) # speed plotting 10X
+topoWorld <- decimate(topoWorld, by=4) # speed plotting 10X
 data(coastlineWorld)
 source('~/src/oce/R/colors.R')
 source('~/src/oce/R/map.R')
 
-if (!interactive()) png('517A.png', width=1000, height=700, type='cairo')
-par(mfrow=c(2,2), mar=c(2,2,1,1))
+lim <- 1000
+
+if (!interactive()) png('517A.png', width=800, height=450, type='cairo')
+par(mfrow=c(2,2), mar=c(1,1,1.5,1))
+##par(mfrow=c(2,1), mar=c(1,1,1.5,1))
 
 mapPlot(coastlineWorld)
-mapImage(topoWorld, col=oceColorsPalette, breaks=seq(-2000, 2000, 10))
-mtext('EXPECT: solid blue < -2000; solid red > 2000', font=2, col=6)
-mtext('(a)', line=-1.2, adj=1, font=2)
+mapImage(topoWorld, col=oceColorsPalette, breaks=seq(-lim, lim, 10))
+mtext(sprintf('EXPECT: solid blue < %.0f; solid red > %.0f', -lim, lim),
+      font=2, col=6, adj=0)
+mtext('(a) ', adj=1)
 
 mapPlot(coastlineWorld)
-mapImage(topoWorld, col=oceColorsPalette, breaks=seq(-2000, 2000, 10), zclip=TRUE, missingColor = 'grey')
-mtext('EXPECT: grey if abs(z) > 2000', font=2, col=6)
-mtext('(b)', line=-1.2, adj=1, font=2)
+mapImage(topoWorld, col=oceColorsPalette, breaks=seq(-lim, lim, 10), zclip=TRUE, missingColor = 'grey')
+mtext(sprintf('EXPECT: grey if abs(z) > %.0f', lim), font=2, col=6, adj=0)
+mtext('(b) ', adj=1)
 
-cm <- colormap(topoWorld[['z']], col=oceColorsPalette, breaks=seq(-2000, 2000, 10), debug=10)
+
+cm <- colormap(topoWorld[['z']], col=oceColorsPalette, breaks=seq(-lim, lim, 10), debug=10)
 mapPlot(coastlineWorld)
 mapImage(topoWorld, colormap=cm, debug=10)
-mtext('EXPECT: same as (a)', font=2, col=6)
-mtext('(c)', line=-1.2, adj=1, font=2)
+mtext('EXPECT: same as (a)', font=2, col=6, adj=0)
+mtext('(c) ', adj=1)
 
-cm <- colormap(topoWorld[['z']], col=oceColorsPalette, breaks=seq(-2000, 2000, 10), zclip=TRUE, debug=10)
+cm <- colormap(topoWorld[['z']], col=oceColorsPalette, breaks=seq(-lim, lim, 10), zclip=TRUE, debug=10)
 mapPlot(coastlineWorld)
 mapImage(topoWorld, colormap=cm, debug=10)
-mtext('EXPECT: same as (b)', font=2, col=6)
-mtext('(d)', line=-1.2, adj=1, font=2)
+mtext('EXPECT: same as (b)', font=2, col=6, adj=0)
+mtext('(d) ', adj=1)
+
 
 if (!interactive()) dev.off()
 
