8e5268d4982d1dc30e8796eda8b4a24a41e9883c
Dan Kelley <kelley.dan@gmail.com>
2014-08-10 18:54:29 -0300

    520

--- 520/520.R
+++ 520/520.R
@@ -2,10 +2,27 @@ if (!interactive()) png("520.png")
 library(oce)
 try({
     source("~/src/oce/R/ctd.R")
+    source("~/src/oce/R/coastline.R")
+    source("~/src/oce/R/map.R")
 })
 data(ctd)
-ctd[['longitude']] <- -5
-ctd[['latitude']] <- -30
-plot(ctd)
+par(mfrow=c(2,2))
+
+ctd[['latitude']] <- 20
+ctd[['longitude']] <- -50
+plot(ctd, which='map')
+mtext("non-projected", font=2, col='purple', adj=0)
+plot(ctd, which='map', projection="automatic")
+mtext("projected", font=2, col='purple', adj=0)
+
+ctd[['latitude']] <- 72
+ctd[['longitude']] <- 0
+plot(ctd, which='map')
+mtext("non-projected", font=2, col='purple', adj=0)
+
+plot(ctd, which='map', proj='automatic')
+mtext("projected", font=2, col='purple', adj=0)
+
+
 if (!interactive()) dev.off()
 
