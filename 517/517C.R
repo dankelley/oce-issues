66d376a3b5122a09aa6a27bc31648c155ed3e85e
Dan Kelley <kelley.dan@gmail.com>
2014-08-16 13:12:12 -0300

    517

--- 517/517C.R
+++ 517/517C.R
@@ -10,19 +10,26 @@ if (!interactive()) png('517C.png', width=700, height=300, type='cairo', pointsi
 par(mfrow=c(1,2), mar=c(2, 2, 3, 1))
 
 
+## Original test
 lonlim <- c(-70,-50)
 latlim <- c(40,50)
+
+## Tests to zero in on pixel so I can check it
+lonlim <- c(-65,-59.5)
+latlim <- c(45.5,48)
+
 #topo <- decimate(topoWorld, by=2) # coarse to illustrate filled contours
 topo <- subset(topoWorld, latlim[1] < latitude & latitude < latlim[2])
 topo <- subset(topo, (360+lonlim[1]) < longitude & longitude < (360+lonlim[2]))
 breaks <- seq(-2000, 0, 100)
 col <- oceColorsGebco
-cm <- colormap(col=col, breaks=breaks, missingColor="purple")
+cm <- colormap(col=col, breaks=breaks, missingColor="red")
 
 ## panel a
 mapPlot(coastlineWorld, type='l',
         longitudelim=lonlim, latitudelim=latlim,
         proj="polyconic", orientation=c(90,-60,0), grid=TRUE)
+Z <- topo[['z']]
 mapImage(topo, col=col, breaks=breaks, debug=3)
 mapLines(coastlineWorld)
 mtext('(a)', adj=1)
