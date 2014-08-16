9dc498d99fe659dd83bc8b4d40e0e83dd0d379ab
Dan Kelley <kelley.dan@gmail.com>
2014-08-16 10:36:25 -0300

    working on 517C.R

--- 517/517C.R
+++ 517/517C.R
@@ -16,7 +16,7 @@ latlim <- c(40,50)
 topo <- subset(topoWorld, latlim[1] < latitude & latitude < latlim[2])
 topo <- subset(topo, (360+lonlim[1]) < longitude & longitude < (360+lonlim[2]))
 breaks <- seq(-2000, 0, 100)
-col <- oceColorsGebco
+col <- oceColorsPalette
 cm <- colormap(col=col, breaks=breaks, missingColor="red")
 
 ## panel a
