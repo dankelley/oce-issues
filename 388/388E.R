56a6a381922a85af419537dfae0bc21356594f0c
Dan Kelley <kelley.dan@gmail.com>
2014-08-19 18:53:59 -0300

    388F.R shows promise in UHL

--- 388/388E.R
+++ 388/388E.R
@@ -1,6 +1,6 @@
 ## idea: add a little to longitude and measure xy departure
-if (!interactive()) pdf("388E.pdf", pointsize=9)
-par(mfrow=c(2,2), mgp=c(2, 0.7, 0), mar=c(3, 3, 2, 1))
+if (!interactive()) pdf("388E.pdf", height=6, pointsize=9)
+par(mfrow=c(2,2), mgp=c(2, 0.7, 0), mar=c(2, 2, 1, 0.3))
 library(oce)
 library(proj4)
 data(coastlineWorld)
@@ -14,22 +14,29 @@ analyze <- function(proj, a=1000e3, dlon=1)
     if (useOce) {
         xy <- lonlat2map(longitude, latitude, proj=proj)
         xy2 <- lonlat2map(longitude+dlon, latitude, proj=proj)
-        bad <- a < abs(xy$x-xy2$x)
-        plot(xy$x, xy$y, type='l', asp=1)
-        points(xy$x[bad], xy$y[bad], col='red')
+        xy3 <- lonlat2map(longitude-dlon, latitude, proj=proj)
+        plot(xy$x, xy$y, type='l', asp=1, xlab="", ylab="", col='darkgray')
+        badR <- a < abs(xy$x-xy2$x)
+        points(xy$x[badR], xy$y[badR], col='red')
+        badL <- a < abs(xy$x-xy3$x)
+        points(xy$x[badL], xy$y[badL], col='blue')
     } else {
         xy <- project(cbind(longitude, latitude), proj=proj)
         xy2 <- project(cbind(longitude+dlon, latitude), proj=proj)
-        bad <- a < abs(xy[,1]-xy2[,1])
-        plot(xy, type='l', asp=1)
-        points(xy[,1][bad], xy[,2][bad], col='red')
+        xy3 <- project(cbind(longitude-dlon, latitude), proj=proj)
+        plot(xy, type='l', asp=1, xlab="", ylab="", col='darkgray')
+        badR <- a < abs(xy[,1]-xy2[,1])
+        points(xy[,1][badR], xy[,2][badR], col='red')
+        badL <- a < abs(xy[,1]-xy3[,1])
+        points(xy[,1][badL], xy[,2][badL], col='blue')
     }
+    list(badR=badR, badL=badL)
 }
 
-analyze("+proj=moll")
-analyze("+proj=moll +lon_0=-60")
-analyze("+proj=merc")
-analyze("+proj=merc +lon_0=-60")
+b1 <- analyze("+proj=moll")
+b2 <- analyze("+proj=moll +lon_0=-60")
+b3 <- analyze("+proj=merc")
+b4 <- analyze("+proj=merc +lon_0=-60")
 
 if (!interactive()) dev.off()
 
---
+++ 388/388F.R
@@ -0,0 +1,42 @@
+rm(list=ls())
+if (!interactive()) pdf("388F.pdf", pointsize=9)
+par(mar=c(2, 2, 3, 1), mgp=c(2, 0.7, 0))
+
+lon <- seq(-180,180,8)
+proj <- "+proj=moll +lon_0=-60"
+#proj <- "+proj=merc +lon_0=-60"
+#proj <- "+proj=stere +lon_0=-60"
+library(plyr)
+library(proj4)
+latitude <- seq(-90, 90, length.out=20)
+#latitude <- seq(30, 80, 10)
+n <- length(latitude)
+longitude <- rep(0, n)
+for (i in 1:n) {
+    ## message("latitude: ", latitude[i])
+    range <- 180
+    ## 7 iterations yields +- 0.00256 deg or 256 m
+    for (iteration in 1:7) {
+        lon <- longitude[i] + seq(-range, range, length.out=10)
+        x <- laply(lon, function(lon) project(cbind(lon,latitude[i]), proj=proj)[1,1])
+        longitude[i] <- lon[which.max(abs(diff(x)))]
+        ##message(" ", longitude[i], " +- ", diff(lon[1:2]))
+        range <- range / 5
+    }
+}
+summary(longitude)
+LON <- longitude
+LAT <- latitude
+xy <- project(list(longitude=longitude, latitude=latitude), proj=proj)
+D <- 0.1                               # trace LH side of globe (works on tests at 0.01)
+xy2 <- project(list(longitude=longitude+D, latitude=latitude), proj=proj)
+x <- c(xy$x, xy2$x)
+y <- c(xy$y, rev(xy2$y))
+plot(x, y, asp=1, type='o')
+grid()
+library(oce)
+data(coastlineWorld)
+mapPlot(coastlineWorld, proj=proj)
+lines(x, y, col='red')
+
+if (!interactive()) dev.off()
