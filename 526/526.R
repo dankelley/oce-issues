c28fd3f576c139eb9b3ee92e4280d3cf4bb5e3e9
Dan Kelley <kelley.dan@gmail.com>
2014-09-21 10:16:23 -0300

    working on issue 526

--- 526/526.R
+++ 526/526.R
@@ -4,15 +4,15 @@ require(ocedata)
 data(coastlineWorldMedium)
 if (!interactive())
     png(filename="526.png" ,hei=5, wid=8, uni='in', res=600)
+par(mfrow=c(1,2), mar=c(2, 2, 1, 1))
 mapPlot(coastlineWorldMedium,
         longitudelim=c(-180,-120), latitudelim=c(35,60),
         proj="+proj=laea +lat_0=40 +lat_1=60 +lon_0=-150",
-        #proj="lambert", parameters=c(lat0=40,lat1=60),  orientation=c(90, -150, 0),
-        debug=3)
-message("issue A: the auto-created grid is no good")
-message("issue B: no axis labels on edges of box")
+        debug=0)
+mapPlot(coastlineWorldMedium,
+        longitudelim=c(-180,-120), latitudelim=c(35,60),
+        proj="lambert", parameters=c(lat0=40,lat1=60),  orientation=c(90, -150, 0),
+        debug=1)
 # mapGrid(dlat=15, dlon=15, col='red') # ok
-## mapZones()
-## mapMeridians(seq(35,60,by=5))
 if (!interactive()) dev.off()
 
--- oce364.R
+++ oce364.R
@@ -1,5 +1,6 @@
 library(oce)
 par(mfcol=c(2,2))
+data(adp)
 imagep(adp[['time']], adp[['distance']], adp[['v']][,,1])
 ## turn off time range for rest of plots
 options(oceDrawTimeRange=FALSE)
