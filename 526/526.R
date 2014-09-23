26dcf70f0135b381e588a0f46d7587707590dc72
Dan Kelley <kelley.dan@gmail.com>
2014-09-23 09:40:18 -0300

    .

--- 526/526.R
+++ 526/526.R
@@ -8,7 +8,7 @@ par(mfrow=c(1,2), mar=c(2, 2, 1, 1))
 mapPlot(coastlineWorldMedium,
         longitudelim=c(-180,-120), latitudelim=c(35,60),
         proj="+proj=laea +lat_0=40 +lat_1=60 +lon_0=-150",
-        debug=0)
+        debug=1)
 mapPlot(coastlineWorldMedium,
         longitudelim=c(-180,-120), latitudelim=c(35,60),
         proj="lambert", parameters=c(lat0=40,lat1=60),  orientation=c(90, -150, 0),
