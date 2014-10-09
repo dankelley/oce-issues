ba90564d495805246d0d5e11bd0c194d5a436f1c
Dan Kelley <kelley.dan@gmail.com>
2014-10-09 08:40:32 -0300

    533

--- 533/533A.R
+++ 533/533A.R
@@ -1,15 +1,32 @@
 library(oce)
 try({source('~/src/oce/R/map.R')})
 data(coastlineWorld)
-par(mar=c(2, 2, 3, 1))
+par(mfrow=c(2,1), mar=c(2, 2, 3, 1))
 lonlim <- c(-180, 180)
-latlim <- c(60, 120)
+latlim <- c(60, 120) # centre the pole
+
 p <- '+proj=stere +lat_0=90'
 mapPlot(coastlineWorld, projection=p, longitudelim=lonlim, latitudelim=latlim, grid=FALSE)
 mapGrid(15, 15, polarCircle=1/2)
+mtext(p, side=3, adj=1)
+## mapAxis(side=1, debug=1)
+                                        #mapAxis(side=1, lon=seq(-180, 180, 45)) # buggy labels
+mapAxis(side=1, lat=seq(0, 90, 15), debug=1)
+mapAxis(side=1, lon=c(-15, 0, 15, 30, 45), debug=1)
 
-mapAxis(side=c(1,3))
+## mapAxis(side=c(1,3), debug=1)
 mtext("EXPECT: no mess at dateline", font=2, col="purple", adj=0, line=2)
 mtext("EXPECT: longitudes on all sides", font=2, col="purple", adj=0, line=1)
-#mapAxis(longitude=c(-15,0, 15))
-#mapAxis(debug=1)
+
+lonlim <- c(-100, -50)
+latlim <- c(30, 60)
+
+lonlim <- c(-100, -50)
+latlim <- c(30, 60)
+
+
+p <- "+proj=lcc +lon_0=-100"
+mapPlot(coastlineWorld, longitudelim=c(-130,-55), latitudelim=c(35,60), projection=p)
+mapGrid(15, 15, polarCircle=1/2)
+mapAxis(side=1, debug=1)
+mtext(p, side=3, adj=1)
