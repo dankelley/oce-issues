30a92186f2dcf9b547a6db14b8f441b2a77902f3
Dan Kelley <kelley.dan@gmail.com>
2014-10-31 15:00:02 -0300

    531B test

---
+++ 531/531B.R
@@ -0,0 +1,12 @@
+if (!interactive()) png("531B.png")
+library(oce)
+try({
+    source("~/src/oce/R/oce.R")
+    source("~/src/oce/R/imagep.R")
+})
+t <- seq.POSIXt(ISOdate(2012, 1, 1, tz='UTC'), ISOdate(2012, 12, 31, tz='UTC'), 100)
+x <- rnorm(t)
+oce.plot.ts(t, x, xlim=as.POSIXct(c('2012-03-01', '2012-06-01'), tz='UTC'), drawTimeRange=FALSE,
+            debug=3)
+mtext("EXPECT: graph extending across plot box", font=2, col="purple", line=0, adj=0)
+if (!interactive()) dev.off()
