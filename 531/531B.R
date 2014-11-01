5b3dc82f3dc5438cd6f842d853969c251cf8f056
Dan Kelley <kelley.dan@gmail.com>
2014-11-01 09:40:08 -0300

    cleanup

--- 531/531B.R
+++ 531/531B.R
@@ -1,9 +1,9 @@
 if (!interactive()) png("531B.png")
 library(oce)
-try({
-    source("~/src/oce/R/oce.R")
-    source("~/src/oce/R/imagep.R")
-})
+## try({
+##     source("~/src/oce/R/oce.R")
+##     source("~/src/oce/R/imagep.R")
+## })
 t <- seq.POSIXt(ISOdate(2012, 1, 1, tz='UTC'), ISOdate(2012, 12, 31, tz='UTC'), 100)
 x <- rnorm(t)
 oce.plot.ts(t, x, xlim=as.POSIXct(c('2012-03-01', '2012-06-01'), tz='UTC'), drawTimeRange=FALSE,
