ea6af476c5af23b3dadbac96841c54fc9fbbbfb3
Dan Kelley <kelley.dan@gmail.com>
2014-11-01 15:23:33 -0300

    537B

---
+++ 537/537B.R
@@ -0,0 +1,11 @@
+if (!interactive()) png("537B.png")
+library(oce)
+try({
+    source("~/src/oce/R/map.R")
+})
+data(coastlineWorld)
+mapPlot(coastlineWorld, projection="mollweide")
+mapLines(coastlineWorld, col='purple')
+mtext("EXPECT: mapLines() without ugly horizontal lines", font=2, col="purple", adj=0)
+if (!interactive()) dev.off()
+
