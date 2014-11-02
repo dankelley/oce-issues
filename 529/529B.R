2919bc3bfcc51a0ee38a86c1b2157b024884a29e
Dan Kelley <kelley.dan@gmail.com>
2014-11-02 09:10:34 -0400

    529

--- 529/529B.R
+++ 529/529B.R
@@ -7,12 +7,12 @@ data(coastlineWorld)
 lonlim <- c(-20, 20)
 latlim <- c(50, 80)
 
-if (!interactive()) png("529A.png")
+if (!interactive()) png("529B.png")
 par(mfrow=c(1,2), mar=c(3, 3, 3, 1))
-p <- 'stereographic'
+p <- "stereographic"
 mapPlot(coastlineWorld, projection=p, longitudelim=lonlim, latitudelim=latlim, main=p)
-p <- '+proj=stere'
+p <- '+proj=stere +lat_0=90'
 mapPlot(coastlineWorld, projection=p, longitudelim=lonlim, latitudelim=latlim, main=p)
-mtext("PROBLEM: curved meridians", font=2, line=0.1, col="purple", adj=0)
+mtext("NB. lat_0 fixes curved meridians", font=2, line=0.1, col="purple", adj=0)
 if (!interactive()) dev.off()
 
--- 529/529D.R
+++ 529/529D.R
@@ -7,12 +7,12 @@ data(coastlineWorld)
 lonlim <- c(-20, 20)
 latlim <- c(50, 80)
 
-if (!interactive()) png("529A.png")
+if (!interactive()) png("529D.png", height=400, width=700)
 par(mfrow=c(1,2), mar=c(3, 3, 3, 1))
-p <- 'stereographic'
+p <- '+proj=sterea +lat_0=90'
 mapPlot(coastlineWorld, projection=p, longitudelim=lonlim, latitudelim=latlim, main=p)
-p <- '+proj=stere'
+p <- '+proj=sterea +lat_0=89.9'
 mapPlot(coastlineWorld, projection=p, longitudelim=lonlim, latitudelim=latlim, main=p)
-mtext("PROBLEM: curved meridians", font=2, line=0.1, col="purple", adj=0)
+mtext("NB. sterea not the solution", font=2, line=0.1, col="purple", adj=0)
 if (!interactive()) dev.off()
 
