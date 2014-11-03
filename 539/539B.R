6bbe6caf1a4764f7cb914bee25ce8c89ffe10a63
Dan Kelley <kelley.dan@gmail.com>
2014-11-03 13:19:41 -0400

    539

--- 539/539B.R
+++ 539/539B.R
@@ -1,14 +1,23 @@
 library(oce)
+try({source("~/src/oce/R/map.R")})
 data(coastlineWorld)
 if (!interactive()) png('539B-%03d.png')
 lons <- seq(179, -179, -10)
 options(oceProj4Test=1)
-for (lon in lons[1:3]) {
-  par(mar=rep(1, 4), bg=NA)
-  p <- paste('+proj=ortho +lat_0=30 +lon_0=', lon, sep='')
-  mapPlot(coastlineWorld, projection=p)
-  mapPoints(0, 90, col='red')
-  mtext(p, adj=1)
+ilon <- 1:3
+ilon <- seq_along(lons)
+par(mar=rep(1, 4), bg=NA)
+for (i in ilon) {
+    lon <- lons[i]
+    p <- paste('+proj=ortho +lat_0=30 +lon_0=', lon, sep='')
+    if (i == 1) {
+        mapPlot(coastlineWorld, projection=p)
+        xlim <- par("usr")[1:2]
+        ylim <- par("usr")[3:4]
+    } else {
+        mapPlot(coastlineWorld, projection=p, xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
+    }
+    mapPoints(0, 90, col='red')
 }
 mtext("EXPECT: poles at same spot", font=2, col="purple", adj=0)
 if (!interactive()) dev.off()
