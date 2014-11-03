33d08bedefbd326c4e7fbaf5d820a69ef1caa0ad
Dan Kelley <kelley.dan@gmail.com>
2014-11-03 09:34:55 -0400

    539

---
+++ 539/539A.R
@@ -0,0 +1,15 @@
+library(oce)
+data(coastlineWorld)
+if (!interactive()) png('539A-%03d.png')
+lons <- seq(179, -179, -10)
+#options(oceProj4Test=1)
+for (lon in lons[1:3]) {
+  par(mar=rep(1, 4), bg=NA)
+  p <- paste('+proj=ortho +lat_0=30 +lon_0=', lon, sep='')
+  mapPlot(coastlineWorld, projection=p)
+  mapPoints(0, 90, col='red')
+  mtext(p, adj=1)
+}
+mtext("EXPECT: poles at same spot", font=2, col="purple", adj=0)
+if (!interactive()) dev.off()
+
---
+++ 539/README.md
@@ -0,0 +1,7 @@
+DK notes:
+1. only need a few panels to see wobble.  Later I'll make
+   the test be numeric.
+2. I also think there's a problem with mapPlot() drawing a new
+   screen.  What it does now, which only happens if bg=NA, is a 
+   bug but it's nice because it shows the pole drift, so I will
+   not bother with it until the wobble is removed.
