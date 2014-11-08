72751a8a008aa08bcad76fbeeae2695148973260
Dan Kelley <kelley.dan@gmail.com>
2014-11-08 08:54:20 -0400

    531

--- 531/531.R
+++ 531/531.R
@@ -3,16 +3,52 @@ library(oce)
 try({source("~/src/oce/R/imagep.R")})
 try({source("~/src/oce/R/oce.R")})
 data(adp)
-par(mfrow=c(2,1))
+par(mfcol=c(2,3))
+
+## column 1: trimming both ends of x axis
 oce.plot.ts(adp[['time']], adp[['temperature']],
             xlim=c(adp[['time']][5], adp[['time']][30]),
-            col='grey', lwd=10, type='l',
-            drawTimeRange=FALSE)
-mtext('Expect: similar lines on graphs', adj=0, font=2, col="purple")
-mtext('oce.plot.ts', adj=1)
+            mar=c(3, 3, 3, 1),
+            type='o', drawTimeRange=FALSE)
+mtext('Trim @ both ends\nEXPECT: similar in col', adj=0, font=2, col="purple")
+mtext('oce.plot.ts ', adj=1, line=-1)
+#lines(adp[['time']], adp[['temperature']])
+plot(adp[['time']], adp[['temperature']],
+     xlim=c(adp[['time']][5], adp[['time']][30]), type='o')
 lines(adp[['time']], adp[['temperature']])
-plot(adp[['time']], adp[['temperature']], xlim=c(adp[['time']][5], adp[['time']][30]), col='grey', lwd=10, type='l')
+mtext('plot ', adj=1, line=-1)
+
+## column 2: trimming only right-hand end of x axis
+offset <- 1800                         # half an hour
+oce.plot.ts(adp[['time']], adp[['temperature']],
+            xlim=c(adp[['time']][1]-offset, adp[['time']][30]),
+            mar=c(3, 3, 3, 1),
+            type='o', drawTimeRange=FALSE)
+mtext('Trim @ left end\nEXPECT: similar in col', adj=0, font=2, col="purple")
+mtext('oce.plot.ts ', adj=1, line=-1)
 lines(adp[['time']], adp[['temperature']])
-mtext('plot', adj=1)
+plot(adp[['time']], adp[['temperature']],
+     xlim=c(adp[['time']][1]-offset, adp[['time']][30]), type='o')
+lines(adp[['time']], adp[['temperature']])
+mtext('plot ', adj=1, line=-1)
+
+## column 3: trimming only right-hand end of x axis; xaxs='i'
+offset <- 1800                         # half an hour
+oce.plot.ts(adp[['time']], adp[['temperature']],
+            xlim=c(adp[['time']][1]-offset, adp[['time']][30]),
+            xaxs="i",
+            mar=c(3, 3, 3, 1),
+            type='o', drawTimeRange=FALSE)
+mtext('Trim @ left end; xaxs="i"\nEXPECT: similar in col', adj=0, font=2, col="purple")
+mtext('oce.plot.ts ', adj=1, line=-1)
+lines(adp[['time']], adp[['temperature']])
+plot(adp[['time']], adp[['temperature']],
+     xaxs="i",
+     xlim=c(adp[['time']][1]-offset, adp[['time']][30]), type='o')
+lines(adp[['time']], adp[['temperature']])
+mtext('plot ', adj=1, line=-1)
+
+
+
 if (!interactive()) dev.off()
 
--- 531/531B.R
+++ 531/531B.R
@@ -1,12 +1,38 @@
 if (!interactive()) png("531B.png")
 library(oce)
-## try({
-##     source("~/src/oce/R/oce.R")
-##     source("~/src/oce/R/imagep.R")
-## })
-t <- seq.POSIXt(ISOdate(2012, 1, 1, tz='UTC'), ISOdate(2012, 12, 31, tz='UTC'), 100)
+try({
+    source("~/src/oce/R/oce.R")
+    source("~/src/oce/R/imagep.R")
+})
+options(oceDrawTimeRange=FALSE)
+
+t <- seq.POSIXt(ISOdate(2012, 1, 1, tz='UTC'), ISOdate(2012, 12, 31, tz='UTC'), 7200)
 x <- rnorm(t)
-oce.plot.ts(t, x, xlim=as.POSIXct(c('2012-03-01', '2012-06-01'), tz='UTC'), drawTimeRange=FALSE,
-            debug=3)
-mtext("EXPECT: graph extending across plot box", font=2, col="purple", line=0, adj=0)
+par(mfcol=c(2,2))
+xlim <- as.POSIXct(c('2012-03-01', '2012-06-01'), tz='UTC')
+oce.plot.ts(t, x, xlim=xlim, drawTimeRange=FALSE, col='gray')
+abline(v=xlim, col='red', lwd=2)
+mtext(paste('usr[1:2]=c(', paste(numberAsPOSIXct(par('usr')[1:2]), collapse=", "), ")"),
+      side=1, line=1.75, col='red', cex=3/4)
+mtext("oce.plot.ts ", side=3, adj=1, cex=0.9, col='blue')
+mtext("EXPECT: same across column", font=2, col="purple", line=0.75, adj=0)
+
+plot(t, x, xlim=xlim, type='l', xlab='', col='gray')
+abline(v=xlim, col='red', lwd=2)
+mtext("plot ", side=3, adj=1, cex=0.9, col='blue')
+mtext(paste('usr[1:2]=c(', paste(numberAsPOSIXct(par('usr')[1:2]), collapse=", "), ")"),
+      side=1, line=1.75, col='red', cex=3/4)
+
+# Column 2
+oce.plot.ts(t, x, xlim=xlim, type='l', xlab='', xaxs="i", col='gray')
+abline(v=xlim, col='red', lwd=2)
+mtext("oce.plot.ts(..., xaxs=\"i\")", side=3, adj=1, cex=0.9, col='blue')
+mtext(paste('usr[1:2]=c(', paste(numberAsPOSIXct(par('usr')[1:2]), collapse=", "), ")"),
+      side=1, line=1.75, col='red', cex=3/4)
+plot(t, x, xlim=xlim, type='l', xlab='', xaxs="i", col='gray')
+abline(v=xlim, col='red', lwd=2)
+mtext("plot(..., xaxs=\"i\")", side=3, adj=1, cex=0.9, col='blue')
+mtext(paste('usr[1:2]=c(', paste(numberAsPOSIXct(par('usr')[1:2]), collapse=", "), ")"),
+      side=1, line=1.75, col='red', cex=3/4)
+
 if (!interactive()) dev.off()
