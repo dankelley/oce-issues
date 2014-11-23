dd9216efc237678cf4d7759b9c76175a1a9b769a
Dan Kelley <kelley.dan@gmail.com>
2014-11-23 15:01:55 -0400

    546

--- 546/546.R
+++ 546/546.R
@@ -2,9 +2,6 @@ library(oce)
 try(source("~/src/oce/R/map.R"))
 data(coastlineWorld)
 if (!interactive()) png("546.png")
-x <- 17266632
-y <- 14745394
-#try(source("~/src/oce/R/map.R"));map2lonlat(x, y, proj="+proj=wintri")
-mapPlot(coastlineWorld, projection="+proj=wintri", fill="lightgray", debug=3)
+mapPlot(coastlineWorld, projection="+proj=wintri", fill="gray")
 if (!interactive()) dev.off()
 
