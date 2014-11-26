74bb5b64ac40e56240f475ee0313dbf0c86c2572
Dan Kelley <kelley.dan@gmail.com>
2014-11-26 12:53:15 -0400

    545

--- 545/545B.R
+++ 545/545B.R
@@ -2,7 +2,7 @@ library(oce)
 try(source("~/src/oce/R/map.R"))
 if (!interactive()) png("545B.png")
 data(coastlineWorld)
-par(mfrow=c(1,3), mar=c(2, 2, 3, 1))
+par(mfrow=c(1,2), mar=c(2, 2, 3, 1))
 lolim <- c(-130, 50)
 lalim <- c(70, 110)
 options(issue545B=1) # set temporary test code in map.R (author only)
@@ -10,15 +10,9 @@ mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
         proj="stereographic", orientation=c(90, -135, 0), fill=1:5)
 mtext("mapproj", adj=1)
 
-options(issue545B=FALSE)
 mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
         proj="+proj=stere +lat_0=90 +lon_0=-135", fill=1:5)
-mtext("proj4\nissue545B=FALSE", adj=1)
-
-options(issue545B=TRUE)
-mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
-        proj="+proj=stere +lat_0=90 +lon_0=-135", fill=1:5)
-mtext("proj4\nissue545B=TRUE", adj=1)
+mtext("proj4", adj=1)
 
 if (!interactive()) dev.off()
 
--- 545/545C.R
+++ 545/545C.R
@@ -2,22 +2,16 @@ library(oce)
 try(source("~/src/oce/R/map.R"))
 if (!interactive()) png("545C.png")
 data(coastlineWorld)
-par(mfrow=c(1,3), mar=c(2, 2, 3, 1))
+par(mfrow=c(1,2), mar=c(2, 2, 3, 1))
 lolim <- c(-130, 50)
 lalim <- c(70, 110)
 mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
         proj="stereographic", orientation=c(90, -135, 0), fill=1:5)
 mtext("mapproj", adj=1)
 
-options(issue545C=FALSE)
 mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
         proj="+proj=stere +lat_0=90 +lon_0=-135", fill=1:5)
-mtext("proj4\nissue545C=FALSE", adj=1)
-
-options(issue545C=TRUE)
-mapPlot(coastlineWorld, longitudelim=lolim, latitudelim=lalim,
-        proj="+proj=stere +lat_0=90 +lon_0=-135", fill=1:5)
-mtext("proj4\nissue545C=TRUE", adj=1)
+mtext("proj4", adj=1)
 
 if (!interactive()) dev.off()
 
