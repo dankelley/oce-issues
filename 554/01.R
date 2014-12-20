b909dea5e7d8e8cab1654b3c163762e7f8899ada
Dan Kelley <kelley.dan@gmail.com>
2014-12-20 15:18:17 -0400

    554

---
+++ 554/01.R
@@ -0,0 +1,12 @@
+library(oce)
+try(source("~/src/oce/R/ctd.R"))
+try(source("~/src/oce/R/sw.R"))
+library(gsw)
+data(ctd)
+SA1<-ctd[["SA"]]
+SA2<-ctd[["SA/gsw"]]
+CT1<-ctd[["CT"]]
+CT2<-ctd[["CT/gsw"]]
+message("following should be small: ", sum(abs(SA1-SA2)))
+message("following should be small: ", sum(abs(CT1-CT2)))
+
---
+++ 554/README.md
@@ -0,0 +1,4 @@
+01.R tests whether gsw and old teos give same SA and CT (yes)
+
+02.R plots TS diagrams for comparison with matlab
+
