f94cf6a915e6282d6ac0e81ee2df1d4d254714e5
Dan Kelley <kelley.dan@gmail.com>
2015-02-04 18:49:55 -0400

    issue579 fixed

--- 579/579_a.R
+++ 579/579_a.R
@@ -1,3 +1,7 @@
 library(oce)
 data(coastlineWorld)
+if (!interactive()) png("579_a.png", width=500, height=300)
+par(mar=rep(1, 4))
 mapPlot(coastlineWorld,projection="+proj=hammer")
+if (!interactive()) dev.off()
+
