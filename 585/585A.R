d5b100579104a58fd0df537bd3af5675f68d6b60
Dan Kelley <kelley.dan@gmail.com>
2015-02-12 15:08:03 -0400

    585

---
+++ 585/585A.R
@@ -0,0 +1,6 @@
+library(oce)
+data(adp)
+if (!interactive()) png("585A.png")
+plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE)
+mtext('EXPECT: 3 color levels between -1 and 1', col='magenta', font=2)
+if (!interactive()) dev.off()
