8a41f439dd021fdf4e0b546082b4826fe93c9e77
Dan Kelley <kelley.dan@gmail.com>
2015-02-14 14:18:05 -0400

    .

--- 585/585A.R
+++ 585/585A.R
@@ -2,17 +2,18 @@ library(oce)
 # source("~/src/oce/R/imagep.R")
 # source("~/src/oce/R/adp.R")
 data(adp)
-if (!interactive()) png("585A.png", pointsize=11)
-par(mfrow=c(2,2))
+if (!interactive()) png("585A_%d.png", pointsize=11)
+par(mfrow=c(3,1))
+
+cm <- colormap(z=adp[["v"]])
 
 plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE)
-mtext('EXPECT: 3 color levels between -1 and 1', col='magenta', font=2, cex=2/3)
-plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE, col=oceColorsJet)
-mtext('EXPECT: 3 jet color levels between -1 and 1', col='magenta', font=2, cex=2/3)
+mtext('EXPECT: 3 color levels between -1 and 1', col='magenta', font=2)
 
-plot(adp, which=1, zlim=c(-1,1), drawTimeRange=FALSE)
-mtext('EXPECT: many colors between -1 and 1', col='magenta', font=2, cex=2/3)
-plot(adp, which=1, zlim=c(-1,1), drawTimeRange=FALSE, col=oceColorsJet)
-mtext('EXPECT: many jet colors between -1 and 1', col='magenta', font=2, cex=2/3)
+plot(adp, which=1, zlim=c(-1, 1), drawTimeRange=FALSE)
+mtext('EXPECT: many color levels between -1 and 1', col='magenta', font=2)
+
+plot(adp, which=1, colormap=cm, drawTimeRange=FALSE)
+mtext('EXPECT: many jet color levels between -1 and 1', col='magenta', font=2)
 
 if (!interactive()) dev.off()
