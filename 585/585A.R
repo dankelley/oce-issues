682313b326a2e400237fac642bb3e33110cd5c44
Dan Kelley <kelley.dan@gmail.com>
2015-02-14 14:05:59 -0400

    585 may be fixed

--- 585/585A.R
+++ 585/585A.R
@@ -1,8 +1,18 @@
 library(oce)
-try(source("~/src/oce/R/imagep.R"))
-try(source("~/src/oce/R/adp.R"))
+# source("~/src/oce/R/imagep.R")
+# source("~/src/oce/R/adp.R")
 data(adp)
-if (!interactive()) png("585A.png")
-plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE, debug=3)
-mtext('EXPECT: 3 color levels between -1 and 1', col='magenta', font=2)
+if (!interactive()) png("585A.png", pointsize=11)
+par(mfrow=c(2,2))
+
+plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE)
+mtext('EXPECT: 3 color levels between -1 and 1', col='magenta', font=2, cex=2/3)
+plot(adp, which=1, breaks=seq(-1, 1, 0.5), drawTimeRange=FALSE, col=oceColorsJet)
+mtext('EXPECT: 3 jet color levels between -1 and 1', col='magenta', font=2, cex=2/3)
+
+plot(adp, which=1, zlim=c(-1,1), drawTimeRange=FALSE)
+mtext('EXPECT: many colors between -1 and 1', col='magenta', font=2, cex=2/3)
+plot(adp, which=1, zlim=c(-1,1), drawTimeRange=FALSE, col=oceColorsJet)
+mtext('EXPECT: many jet colors between -1 and 1', col='magenta', font=2, cex=2/3)
+
 if (!interactive()) dev.off()
