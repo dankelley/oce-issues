fff4fbbf1c5225b56faa5565ee6e9a5b1698bba2
Dan Kelley <kelley.dan@gmail.com>
2015-02-16 07:30:31 -0400

    588

--- 588/588C.R
+++ 588/588C.R
@@ -4,9 +4,10 @@ f <- '050107_20130620_2245cast4.rsk'
 d <- read.oce('050107_20130620_2245cast4.rsk')
 if (!interactive()) png("588C_%d.png")
 plotScan(d)
-title(f)
+mtext("All data", line=0, adj=1, col='red', font=2)
 plotScan(ctdTrim(d))
+mtext("after ctdTrim()", line=0, adj=1, col='red', font=2)
 plotScan(subset(d, 1010 < scan & scan < 1070), type='p')
-title(paste(f, "after ctdTrim()"))
+mtext("after subset() to check anomaly", line=0, adj=1, col='red', font=2)
 if (!interactive()) dev.off()
 
