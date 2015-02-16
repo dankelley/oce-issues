e0c04685d416731de8b73f574b22d4cf73456ac3
Dan Kelley <kelley.dan@gmail.com>
2015-02-16 07:13:56 -0400

    588

---
+++ 588/588C.R
@@ -0,0 +1,11 @@
+library(oce)
+## source("~/src/oce/R/ctd.R")
+f <- '050107_20130620_2245cast4.rsk'
+d <- read.oce('050107_20130620_2245cast4.rsk')
+if (!interactive()) png("588C_%d.png")
+plotScan(d)
+title(f)
+plotScan(ctdTrim(d))
+title(paste(f, "after ctdTrim()"))
+if (!interactive()) dev.off()
+
