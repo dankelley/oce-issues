2c1fcb06036076a8805cee108aad0d6d9cc41887
Dan Kelley <kelley.dan@gmail.com>
2015-03-31 12:45:47 -0300

    616

--- 616/616e.R
+++ 616/616e.R
@@ -1,14 +1,17 @@
 rm(list=ls())
+library(oce)
+p <- "+proj=ortho"
 inc <- 2 # 0.5
 pch <- 20
 cex <- 1/4
 grid <- expand.grid(lon=seq(-180, 180, inc), lat=seq(-90, 90, inc), KEEP.OUT.ATTRS=FALSE)
-write.table(grid, file="lonlat.dat", row.names=FALSE, col.names=FALSE)
-system("cat lonlat.dat | proj '+proj=ortho' | sed -e 's/*/Inf/g' > xy.dat")
-xy <- read.table("xy.dat", col.names=c("x", "y"), header=FALSE)
-d <- data.frame(lon=grid$lon, lat=grid$lat, x=xy$x, y=xy$y)
-write.table(d, file="616d_results.dat", row.names=FALSE)
-if (!interactive()) png("616d.png", unit="in", width=6, height=3, res=150, pointsize=7)
+#write.table(grid, file="lonlat.dat", row.names=FALSE, col.names=FALSE)
+#system("cat lonlat.dat | proj '+proj=ortho' | sed -e 's/*/NA/g' > xy.dat")
+xy <- lonlat2map(grid$lon, grid$lat, projection=p)
+#xy <- read.table("xy.dat", col.names=c("x", "y"), header=FALSE)
+d <- data.frame(lon=grid$lon, lat=grid$lat, x=round(xy$x), y=round(xy$y))
+write.table(d, file="616e_results.dat", row.names=FALSE)
+if (!interactive()) png("616e.png", unit="in", width=6, height=3, res=150, pointsize=7)
 par(mfrow=c(1,2), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 plot(d$x, d$y, asp=1, pch=pch, cex=cex, col='red')
 ## using 'o' avoids overpainting, which made it look like the N pole
--- 616/README.md
+++ 616/README.md
@@ -6,8 +6,10 @@
 * 616c.R see what xy points result from the fake poles. The printout shows that
   a lot are coming out infinite, and at essentially x=0.
 
-* 616d.R tests external (system level) proj, to see what points can be mapped.
-  The graph is interesting and may prove useful.
+* 616d.R tests external (system level) proj (vsn 4.8.0), to see what points can
+  be mapped.  The graph is interesting and may prove useful.
+
+* 616e.R shows that results match proj to within about 0.1 percent.
 
 **NEXT** try d.R but within R, to check proj there. (The results of c seem hard
-to explain)
+to explain) 
