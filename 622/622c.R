be99881f561a7dcbfd734ce84b62528270ac35ba
Dan Kelley <kelley.dan@gmail.com>
2015-04-05 10:55:41 -0300

    .

---
+++ 622/622c.R
@@ -0,0 +1,38 @@
+library(oce)
+
+## CASE 1: pressure should start at about 10.3
+d <- read.oce("a.rsk")
+head(d[['pressure']])
+head(d[['salinity']])
+processingLogShow(d)
+p <- d[["pressure"]]
+S <- d[["salinity"]]
+
+## CASE 2: pressure should start at about 10.3
+d <- read.logger("a.rsk")
+head(d[['pressure']])
+head(d[['salinity']])
+processingLogShow(d)
+stopifnot(all.equal(d[["pressure"]], p))
+stopifnot(all.equal(d[["salinity"]], S))
+
+## CASE 3: pressure should start at about 10.3
+d <- read.logger("a.rsk", patm=FALSE)
+head(d[['pressure']])
+head(d[['salinity']])
+processingLogShow(d)
+stopifnot(all.equal(d[["pressure"]], p))
+stopifnot(all.equal(d[["salinity"]], S))
+
+## CASE 4: pressure should start near 0 dbar
+d <- read.logger("a.rsk", patm=TRUE)
+head(d[['pressure']])
+head(d[['salinity']])
+processingLogShow(d)
+stopifnot(all.equal(d[["salinity"]], S))
+
+## CASE 5: salinity should be slightly different from above
+d <- read.logger("a.rsk", patm=10)
+head(d[['pressure']])
+head(d[['salinity']])
+processingLogShow(d)
