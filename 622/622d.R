079711ac8ef5bcdca141f0ebfb7d4a0e0302dd30
Dan Kelley <kelley.dan@gmail.com>
2015-04-06 08:04:38 -0300

    logger

--- 622/622d.R
+++ 622/622d.R
@@ -1,33 +1,50 @@
 library(oce)
+source("~/src/oce/R/oce.R")
+source("~/src/oce/R/logger.R")
 
 if (!interactive()) png("622d_%d.png")
 
-## CASE 1: pressure should start at about 10.3
+## CASE 1: pressure should start at about 0
+d <- read.oce("a.rsk", patm=TRUE)
+plot(ctdTrim(d))
+cat("note that waterDepth is wrong")
+summary(d)
+stopifnot(min(d[["pressure"]]) < 1)
+
+## CASE 2: pressure should start at about 10.3
+d <- read.oce("a.rsk", patm=FALSE)
+plot(ctdTrim(d))
+cat("note that waterDepth is wrong")
+summary(d)
+stopifnot(min(d[["pressure"]]) > 9)
+
+## CASE 3: pressure should start at about 10.3
 d <- read.oce("a.rsk")
 plot(ctdTrim(d))
 cat("note that waterDepth is wrong")
 summary(d)
+stopifnot(min(d[["pressure"]]) > 9)
 
-## CASE 2: pressure should start at about 10.3
+## CASE 4: pressure should start at about 10.3
 d <- read.logger("a.rsk")
 plot(ctdTrim(d))
 summary(d)
 p <- d[["pressure"]]
 S <- d[["salinity"]]
 
-## CASE 3: pressure should start at about 10.3
+## CASE 5: pressure should start at about 10.3
 d <- read.logger("a.rsk", patm=FALSE)
 plot(ctdTrim(d))
 summary(d)
 stopifnot(all.equal(d[["pressure"]], p))
 stopifnot(all.equal(d[["salinity"]], S))
 
-## CASE 4: pressure should start near 0 dbar
+## CASE 6: pressure should start near 0 dbar
 d <- read.logger("a.rsk", patm=TRUE)
 plot(ctdTrim(d))
 summary(d)
 stopifnot(all.equal(d[["salinity"]], S))
 
-## CASE 5: salinity should be slightly different from above
+## CASE 7: salinity should be slightly different from above
 d <- read.logger("a.rsk", patm=10)
 summary(d)
