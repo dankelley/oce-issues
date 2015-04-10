cb9350436629d2484bbf285d8d81e0ea3f2fe84b
Dan Kelley <kelley.dan@gmail.com>
2015-04-10 19:24:15 -0300

    628

---
+++ 628/628a.R
@@ -0,0 +1,10 @@
+library(oce)
+source("~/src/oce/R/map.R") # has temporary change to test wintri
+if (!interactive()) png("628a.png")
+data(coastlineWorld)
+lon <- coastlineWorld[["longitude"]]
+lat <- coastlineWorld[["latitude"]]
+xy <- lonlat2map(lon, lat, projection="+proj=wintri")
+plot(xy$x, xy$y, type='l', asp=1)
+#mapPlot(coastlineWorld, proj="+proj=wintri", grid=FALSE)
+if (!interactive()) dev.off()
---
+++ 628/628b.R
@@ -0,0 +1,6 @@
+library(oce)
+source("~/src/oce/R/map.R") # has temporary change to test wintri
+if (!interactive()) png("628b.png")
+data(coastlineWorld)
+mapPlot(coastlineWorld, proj="+proj=wintri")
+if (!interactive()) dev.off()
