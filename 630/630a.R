8ffc2d7cc26809405cf8ea9e9a4db33d119db6ff
Dan Kelley <kelley.dan@gmail.com>
2015-04-14 08:26:53 -0300

    630

---
+++ 630/630a.R
@@ -0,0 +1,11 @@
+library(oce)
+try(source("~/src/oce/R/sw.R"))
+data(ctd)
+salinity <- ctd[["salinity"]]
+temperature <- ctd[["temperature"]]
+pressure <- ctd[["pressure"]]
+cond1 <- swCSTp(salinity, temperature, pressure, eos="unesco")
+ctd <- ctdAddColumn(ctd, cond1, "conductivity")
+cond2 <- swCSTp(ctd)
+misfit <- sqrt(mean((cond1-cond2)^2))
+stopifnot(misfit == 0)
