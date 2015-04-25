767a5ae523778dc1508e43a94ce0e02409cbe649
Dan Kelley <kelley.dan@gmail.com>
2015-04-25 15:07:09 -0300

    636

--- 636/636a.R
+++ 636/636a.R
@@ -1,14 +1,25 @@
+rm(list=ls())
+source("~/src/oce/R/colors.R")
 library(oce)
-try(source("~/src/oce/R/colors.R"))
 if (!interactive()) png("636a.png")
 par(mfrow=c(1,1))
 data(topoWorld)
+if (FALSE){
 par(mfrow=c(2,1))
 cm <- colormap(name="gmt_globe")
 imagep(topoWorld, breaks=cm$breaks, col=cm$col)
 cm <- colormap(name="gmt_globe", subdivisions=10)
 str(cm)
-imagep(topoWorld, breaks=cm$breaks, col=cm$col)
+#imagep(topoWorld, breaks=cm$breaks, col=cm$col)
+imagep(topoWorld, colormap=cm)#breaks=cm$breaks, col=cm$col)
 mtext("NOTE: broken near coastline (land bridge in Bering St)", font=2, col="purple")
 if (!interactive()) dev.off()
-
+}
+#NEW TEST
+cm <- colormap(x0=c(-5000,-1000,   0,1000),
+               x1=c(-1000,    0,1000,2000),
+               col0=col2rgb(c("purple","blue","white","brown")),
+               col1=col2rgb(c("blue",  "white","brown","black")), debug=3)
+str(cm)
+imagep(topoWorld, breaks=cm$breaks, col=cm$col, xlim=360+c(-70,-20), ylim=c(10, 60), asp=1)
+#imagep(topoWorld, colormap=cm, xlim=360+c(-70,-20), ylim=c(10, 60), asp=1)
