09dd0018d84754223eabf3221211fd32573e4aab
Dan Kelley <kelley.dan@gmail.com>
2015-05-03 11:29:59 -0300

    .

--- 640/03.R
+++ 640/03.R
@@ -3,17 +3,17 @@ R <- 4
 lon0 <- 180
 lon <- lat <- NULL
 lon <- c(lon, 170 + R * cos(-theta))
-lat <- c(lat, 180 + R * sin(-theta))
+lat <- c(lat, R * sin(-theta))
 lon <- c(lon, NA)
 lat <- c(lat, NA)
 
 lon <- c(lon, 180 + R * cos(-theta))
-lat <- c(lat, 180 + R * sin(-theta))
+lat <- c(lat, R * sin(-theta))
 lon <- c(lon, rep(NA,2))
 lat <- c(lat, rep(NA,2))
 
 lon <- c(lon, 190 + R * cos(-theta))
-lat <- c(lat, 180 + R * sin(-theta))
+lat <- c(lat, R * sin(-theta))
 nlon <- length(lon)
 
 cat("nlon=", nlon, "\n")
@@ -21,12 +21,14 @@ par(mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 plot(lon, lat, pch=20, type='o', cex=2/3)
 text(lon, lat, seq_along(lon)+1, pos=2)
 abline(v=lon0)
-system("R CMD SHLIB lonchop.c") 
-dyn.load("lonchop.so")
-mod <- .C("lonchop", n=as.integer(nlon), lon=as.double(lon), lat=as.double(lat),
-          lon0=as.double(lon0),
-          no=integer(1), lono=double(2*nlon), lato=double(2*nlon),
+system("R CMD SHLIB polygon.c") 
+dyn.load("polygon.so")
+nx <- length(lon)
+mod <- .C("polygon_subdivide_vertically",
+          n=as.integer(nlon), x=as.double(lon), y=as.double(lat),
+          x0=as.double(lon0),
+          no=integer(1), xo=double(2*nx), lato=double(2*nx),
           NAOK=TRUE)
 #data.frame(lon=lon, lat=lat)
 lines(lon, lat)
-points(mod$lono[1:mod$no], mod$lato[1:mod$no], col='red', cex=2)
+#points(mod$lono[1:mod$no], mod$lato[1:mod$no], col='red', cex=2)
--- 640/polygon.c
+++ 640/polygon.c
@@ -6,25 +6,24 @@
 #define POSITIVE(x) ((x) > 0.0)
 /*
 
-   system("R CMD SHLIB lonchop1.c") 
-   dyn.load("lonchop1.so")
+   system("R CMD SHLIB polygon_subdivide.c") 
+   dyn.load("polygon_subdivide.so")
 
 */
 
-void lonchop(int *n, double *lon, double *lat, double *lon0, int *no, double *lono, double *lato)
+void polygon_subdivide_vertically(int *n, double *x, double *y, double *x0,
+    int *no, double *xo, double *yo)
 {
-  Rprintf("lonchop(*n=%d, ..., *lon0=%f, ...)\n", *n, *lon0);
+  Rprintf("polygon_chop(*n=%d, ..., *x0=%f, ...)\n", *n, *x0);
   // Buffers: 1 holds the main path, 2 holds sidelobes.
   // FIXME is double size enough?
-  int N = 2*(*n);
-  double *lonbuf1 = (double*)R_alloc(N, sizeof(double));
-  double *latbuf1 = (double*)R_alloc(N, sizeof(double));
-  double *lonbuf2 = (double*)R_alloc(N, sizeof(double));
-  double *latbuf2 = (double*)R_alloc(N, sizeof(double));
-  unsigned int *seg_start = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
-  unsigned int *seg_end = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
-  unsigned int iseg=0, nseg = 0;
-  int ibuf1 = 0, ibuf2 = 0;
+  //int N = 2*(*n);
+  // double *xbuf = (double*)R_alloc(N, sizeof(double));
+  // double *ybuf = (double*)R_alloc(N, sizeof(double));
+  unsigned int *poly_start = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
+  unsigned int *poly_end = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
+  unsigned int ipoly=0, npoly = 0;
+  int ibuf = 0;
 
   // Separate steps to make it easier to write/debug/read.
 
@@ -32,87 +31,69 @@ void lonchop(int *n, double *lon, double *lat, double *lon0, int *no, double *lo
   // Skip any NA at start
   int start;
   for (start = 0; start < (*n)-1; start++) {
-    if (!ISNA(lon[start]))
+    if (!ISNA(x[start]))
       break;
   }
   //Rprintf("first non-NA point at start=%d\n", start);
-  seg_start[nseg] = start;
+  poly_start[npoly] = start;
   int i = start;
   while (i < (*n)) {
     // Find first non-NA
-    while (ISNA(lon[i]) && (i < (*n))) {
-      //Rprintf("seeking first non-NA; lon[%d] %f\n", i, lon[i]);
+    while (ISNA(x[i]) && (i < (*n))) {
+      //Rprintf("seeking first non-NA; x[%d] %f\n", i, x[i]);
       i++;
     }
-    //Rprintf("seg_start[%d] = %d\n", nseg, i);
-    seg_start[nseg] = i;
+    //Rprintf("poly_start[%d] = %d\n", npoly, i);
+    poly_start[npoly] = i;
     // Find last non-NA
-    while (!ISNA(lon[i]) && (i < (*n))) {
-      //Rprintf("seeking last non-NA; lon[%d] %f\n", i, lon[i]);
+    while (!ISNA(x[i]) && (i < (*n))) {
+      //Rprintf("seeking last non-NA; x[%d] %f\n", i, x[i]);
       i++;
     }
-    //Rprintf("seg_end[%d] = %d\n", nseg, i);
-    seg_end[nseg] = i-1;
-    nseg++;
+    //Rprintf("poly_end[%d] = %d\n", npoly, i);
+    poly_end[npoly] = i-1;
+    npoly++;
     i++;
   }
-  if (nseg == 0) 
-    error("no segments\n");
-  for (iseg = 0; iseg < nseg; iseg++)
-    Rprintf("seg %d @ %d to %d\n", iseg, seg_start[iseg], seg_end[iseg]);
-
-  int j = 0;
-  double epsilon=0.001;
-  epsilon=0.1;
-
-  for (iseg = 0; iseg < nseg; iseg++) {
-    // Find whether first point is to east or west of lon0. Call this
-    // "first_side". Then proceed through the data, storing points in 
-    // one of two buffers. For each point, determine the side, named
-    // "this_side". If it is on the same as first point, then store the
-    // (lon,lat) into the "buf1" buffer. Otherwise, store into the "buf2"
-    // buffer. When we pass back into first_side, emit buf2 into
-    // (lono, lato) and reset buf2. When all this is done, the final
-    // point will be back on first_side (or will assume so). Thus,
-    // when buf1 is emited to (lono, lato), we will have completed one
-    // side.  FIXME: think more on the logic.
-    int first_side = POSITIVE(lon[start]-(*lon0));
-    Rprintf("SEGMENT %d\n", iseg);
-    for (int i = seg_start[iseg]; i <= seg_end[iseg]; i++) {
-      int this_side  = POSITIVE(lon[i]-(*lon0));
-      if (this_side != first_side) {
-	// Done with this segment
-	first_on_right = this_on_right;
-      } else {
-	// FIXME: handle NA
-	// see if crossing
-	double l = lon[i] - (*lon0);
-	double r = lon[i+1] - (*lon0);
-	// Rprintf("i %d %f %f -> %f %f (%f)\n", i, lon[i], lon[i+1], l, r, *lon0);
-	if ((l * r) < 0.0) {
-	  Rprintf("crosses for i between %d and %d\n", i, i+1);
-	  double LAT = lat[i] + (lat[i+1]-lat[i])*((*lon0)-lon[i])/(lon[i+1]-lon[i]);
-	  Rprintf(" lat %f\n", LAT);
-	  lono[j] = lon[i];
-	  lato[j] = lat[i];
-	  j++;
-	  lono[j] = (*lon0) - epsilon;
-	  lato[j] = LAT;
-	  j++;
-	  lono[j] = (*lon0) + epsilon;
-	  lato[j] = LAT;
-	  j++;
-	} else {
-	  lono[j] = lon[i];
-	  lato[j] = lat[i];
-	  j++;
-	}
+  if (npoly == 0) 
+    error("no polygons\n");
+  // Process each polygon individually
+  for (ipoly = 0; ipoly < npoly; ipoly++) {
+    Rprintf("\npoly %d @ %d to %d\n", ipoly, poly_start[ipoly], poly_end[ipoly]);
+    // Find intersection segments
+    int j;
+    unsigned int *intersection = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
+    int nintersection;
+    //int nseg = 1 + poly_end[ipoly] - poly_start[ipoly];
+    //Rprintf("  nseg=%d\n", nseg);
+    nintersection = 0;
+    for (i = poly_start[ipoly], j=poly_end[ipoly]; i <= poly_end[ipoly]; j=i++) {
+      Rprintf("   x[%d] %f  x[%d] %f\n", i, x[i], j, x[j]);
+      if ((x[i] <= (*x0) && (*x0) <= x[j]) || (x[j] <= (*x0) && (*x0) <= x[i])) {
+	Rprintf("     the previous point intersects x0=%f\n", (*x0));
+	nintersection++;
       }
     }
+    if (nintersection == 0) {
+      for (i = poly_start[ipoly]; i < poly_end[ipoly]; i++) {
+	xo[(*no)] = x[i];
+	yo[(*no)] = y[i];
+	Rprintf("     xo[%d]=%f yo[%d]=%f\n", (*no), x[i], (*no), y[i]);
+	(*no)++;
+      }
+      xo[(*no)] = NA_REAL;
+      yo[(*no)] = NA_REAL;
+      Rprintf("     xo[%d]=%f yo[%d]=%f\n", (*no), x[i], (*no), y[i]);
+      (*no)++;
+    } else {
+      Rprintf("   MUST SUBDIVIDE\n");
+    }
+    double epsilon=0.001;
+    epsilon=0.1;
   }
-  *no = j+1;
 }
 
+#if 0
 void lonchop3(int *n, double *lon, double *lat, double *lon0, double *lono, double *lato)
 {
   Rprintf("lonchop3(*n=%d, ..., *lon0=%f, ...)\n", *n, *lon0);
@@ -126,7 +107,7 @@ void lonchop3(int *n, double *lon, double *lat, double *lon0, double *lono, doub
   unsigned int *seg_start = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
   unsigned int *seg_end = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
   unsigned int iseg=0, nseg = 0;
-  int ibuf1 = 0, ibuf2 = 0;
+  int ibuf = 0;
 
   // Separate steps to make it easier to write/debug/read.
 
@@ -276,3 +257,5 @@ void lonchop1(int *n, double *lon, double *lat, double *lon0, double *lono, doub
     }
   }
 }
+#endif
+
