e2768de8faa8555c1b442e547212b01360e2f0b7
Dan Kelley <kelley.dan@gmail.com>
2015-05-05 18:24:47 -0300

    polygon getting closer

--- 640/polygon.c
+++ 640/polygon.c
@@ -1,6 +1,6 @@
 /* vim: set noexpandtab shiftwidth=2 softtabstop=2 tw=70: */
 #define DEBUG 1
-//#define DOLEFT
+#define DOLEFT
 #define DORIGHT
 #include <R.h>
 #include <Rdefines.h>
@@ -13,30 +13,18 @@
 */
 
 #define SAVE(x,y) {\
-  if ((*no) >= (*nomax)) error("Ran out of space; contact developer.\n");\
+  if ((*no) >= (*nomax)) error("Ran out of space (limit %d pairs); contact developer.\n", (*nomax));\
   xo[(*no)]=(x);\
   yo[(*no)]=(y);\
   ++(*no);\
   if (DEBUG) Rprintf(" [ %7.2f %7.2f @ %3d ]\n", (x), (y), (*no));\
 }
 
-// On input, no is size provided for xo and yo. An error results if we
-// run out of space. (It may be sufficient to use no as double
-// n.) Otherwise, no is set to the number filled in by the present
-// procedure.
 void polygon_subdivide_vertically(int *n, double *x, double *y, double *x0,
     int *nomax, int *no, double *xo, double *yo) {
-  Rprintf("polygon_chop(*n=%d, ..., *x0=%f, ...)\n", *n, *x0);
-  // Buffers: 1 holds the main path, 2 holds sidelobes.  FIXME is
-  // double size enough?
-  //int N = 2*(*n); double *xbuf = (double*)R_alloc(N,
-  //sizeof(double)); double *ybuf = (double*)R_alloc(N,
-  //sizeof(double));
-
-  unsigned int *poly_start = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
-
-  unsigned int *poly_end = (unsigned int*)R_alloc(*(n), sizeof(unsigned int));
-
+  Rprintf("polygon_chop(*n=%d, ..., *x0=%f, *nomax=%d, ...)\n", *n, *x0, *nomax);
+  unsigned int *poly_start = (unsigned int*)R_alloc(*(nomax), sizeof(unsigned int));
+  unsigned int *poly_end = (unsigned int*)R_alloc(*(nomax), sizeof(unsigned int));
   unsigned int ipoly=0, npoly = 0;
   (*no) = 0; // set to 0 by R anyway, but this protects against R changes
 
@@ -49,29 +37,25 @@ void polygon_subdivide_vertically(int *n, double *x, double *y, double *x0,
     if (!ISNA(x[start]))
       break;
   }
-  //Rprintf("first non-NA point at start=%d\n", start);
   poly_start[npoly] = start;
   int i = start;
   while (i < (*n)) {
     // Find first non-NA
     while (ISNA(x[i]) && (i < (*n))) {
-      //Rprintf("seeking first non-NA; x[%d] %f\n", i, x[i]);
       i++;
     }
-    //Rprintf("poly_start[%d] = %d\n", npoly, i);
     poly_start[npoly] = i;
     // Find last non-NA
     while (!ISNA(x[i]) && (i < (*n))) {
-      //Rprintf("seeking last non-NA; x[%d] %f\n", i, x[i]);
       i++;
     }
-    //Rprintf("poly_end[%d] = %d\n", npoly, i);
     poly_end[npoly] = i-1;
     npoly++;
     i++;
   }
   if (npoly == 0) 
     error("no polygons\n");
+  //
   // 2. Process each polygon individually.
   for (ipoly = 0; ipoly < npoly; ipoly++) {
     Rprintf("\npoly %d @ %d to %d\n", ipoly, poly_start[ipoly], poly_end[ipoly]);
@@ -112,6 +96,7 @@ void polygon_subdivide_vertically(int *n, double *x, double *y, double *x0,
       if (kk > poly_end[ipoly]) // FIXME: probably this is wrong
 	kk = poly_start[ipoly]; // FIXME: probably this is wrong
       double epsilon=0.25;
+      int hidden_points = 0;
 #ifdef DOLEFT
       // Left of x0
       for (i = 0; i <= poly_end[ipoly]-poly_start[ipoly]; i++) {
@@ -149,25 +134,49 @@ void polygon_subdivide_vertically(int *n, double *x, double *y, double *x0,
       kk = imax + 1;
       if (kk > poly_end[ipoly]) // FIXME: probably this is wrong
 	kk = poly_start[ipoly]; // FIXME: probably this is wrong
+      hidden_points = 0;
+      double hidden_y_start = 0;
       for (i = 0; i <= poly_end[ipoly]-poly_start[ipoly]; i++) {
 	Rprintf("  i=%d k=%d kk=%d  x[k]=%7.3f y[k]=%7.3f x[kk]=%7.3f y[kk]=%7.3f\n", i, k, kk, x[k], y[k], x[kk], y[kk]);
 	if (x[k] <= (*x0) && (*x0) <= x[kk]) {
 	  double Y = y[k] + (y[kk]-y[k])*((*x0)+epsilon-x[k])/(x[kk]-x[k]);
-	  Rprintf("CASE A (cross): save k=%d cut=%7.3f Y=%7.3f\n", k, (*x0)+epsilon, Y);
+	  Rprintf("CASE A (leaving hidden zone): k=%d cut=%7.3f Y=%7.3f\n", k, (*x0)+epsilon, Y);
+	  Rprintf("will draw %d hidden points\n", hidden_points);
+	  if (hidden_points > 0) {
+	    double YY = hidden_y_start, dy = (Y-YY)/hidden_points;
+	    for (int p = 0; p < hidden_points; p++) {
+	      SAVE((*x0)+epsilon, YY);
+	      YY += dy;
+	    }
+	  }
 	  SAVE((*x0)+epsilon, Y);
 	  SAVE(x[kk], y[kk]);
+	  hidden_points = 0;
+	  hidden_y_start = Y;
 	} else if (x[kk] <= (*x0) && (*x0) <= x[k]) {
 	  double Y = y[k] + (y[kk]-y[k])*((*x0)+epsilon-x[k])/(x[kk]-x[k]);
-	  Rprintf("CASE B (cross): save k=%d cut=%7.3f Y=%7.3f\n", k, (*x0)+epsilon, Y);
+	  Rprintf("CASE B (entering hidden zone): save k=%d cut=%7.3f Y=%7.3f\n", k, (*x0)+epsilon, Y);
+	  Rprintf("will draw %d hidden points\n", hidden_points);
+	  if (hidden_points > 0) {
+	    double YY = hidden_y_start, dy = (Y-YY)/hidden_points;
+	    for (int p = 0; p < hidden_points; p++) {
+	      SAVE((*x0)+epsilon, YY);
+	      YY += dy;
+	    }
+	  }
 	  SAVE(x[k], y[k]);
 	  SAVE((*x0)+epsilon, Y);
+	  hidden_points = 0;
+	  hidden_y_start = Y;
 	} else {
 	  if (x[k] >= (*x0)) {
 	    Rprintf("CASE C (x[k] >= x0): save x[k=%d]=%7.3f and y[k=%d]=%7.3f\n", k, x[k], k, y[k]);
 	    SAVE(x[k], y[k]);
 	  } else {
 	    Rprintf("CASE D (x[k] <  x0): save x=x0+epsilon=%7.3f and y[k=%d]=%7.3f\n", (*x0)+epsilon, k, y[k]);
-	    SAVE((*x0)+epsilon, y[k]);
+	    //SAVE((*x0)+epsilon, y[k]);
+	    Rprintf("  hidden_points=%d\n", hidden_points);
+	    hidden_points++;
 	  }
 	}
 	if (++k > poly_end[ipoly])
