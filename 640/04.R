9b084ad07e0bc1930efcd6ed25318211817a67a8
Dan Kelley <kelley.dan@gmail.com>
2015-05-06 19:35:26 -0300

    640/04.R shows problem

--- 640/04.R
+++ 640/04.R
@@ -3,7 +3,7 @@ theta <- seq(0, 2*pi, length.out=32)
 lon0 <- 180
 Rx <- 5
 Ry <- 2
-lon <- 180 - Rx + Rx*cos(theta)+Rx*sin(theta/2)*1.5
+lon <- 179.3 - Rx + Rx*cos(theta)+Rx*sin(theta/2)*1.5
 lat <- 0 + Ry * sin(theta)
 par(mfrow=c(1,2), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 plot(lon, lat, asp=1, type='o')
@@ -22,6 +22,6 @@ points(mod$xo, mod$yo, col='red', pch=21);lines(lon, lat)
 plot(mod$xo, mod$yo, xlim=range(lon), ylim=range(lat), asp=1)
 abline(v=lon0, col='blue')
 abline(v=lon0+0.25*c(-1,1), col='blue', lty='dotted')
-polygon(mod$xo, mod$yo, col=rgb(0.2, 0.2, 0.2, alpha=0.1))
+polygon(mod$xo, mod$yo, col=rgb(0.8, 0.8, 0.8))#, alpha=0.1))
 ##text(mod$xo, mod$yo, seq_along(mod$xo)-1, pos=2)
 points(lon, lat, bg='red', col='blue', pch=21)
