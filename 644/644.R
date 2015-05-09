bdc7ca60b8e070f55832365996b87815053a040f
Dan Kelley <kelley.dan@gmail.com>
2015-05-09 14:09:49 -0300

    644

---
+++ 644/644.R
@@ -0,0 +1,9 @@
+library(oce)
+try(source("~/src/oce/R/ctd.R"))
+## test inference of conductivity unit
+ctds <- list.files(".", "*.cnv")
+for (ctd in ctds) {
+    d <- read.oce(ctd)
+    message("File ", ctd, " has conductivity unit: ", d[["conductivityUnit"]])
+}
+
