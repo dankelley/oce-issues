56a65dc3787d668e1599a4a0ce65a660db8bf1f3
Dan Kelley <kelley.dan@gmail.com>
2015-05-16 11:40:03 -0300

    .

--- 649/649b.R
+++ 649/649b.R
@@ -1,7 +1,6 @@
 ## Try reading ODF "adcp" manually. (NOTE: I think it's just one depth.)
 library(oce)
-file <- "MADCPS_hud2013021_1842_3380-69_3600.ODF"
-d <- read.odf(file)
+d <- read.odf("MADCPS_hud2013021_1842_3380-69_3600.ODF")
 if (!interactive()) png("649b.png")
 par(mfrow=c(3,1))
 oce.plot.ts(d[["time"]], d[["u"]])
