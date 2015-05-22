e0486a3b7e94c15c1614209e66e406fe6eb4e738
Dan Kelley <kelley.dan@gmail.com>
2015-05-22 15:07:20 -0300

    653

---
+++ 653/653a.R
@@ -0,0 +1,22 @@
+if (!interactive()) png("653a.png")
+library(oce)
+data(coastlineWorld)
+lon <- coastlineWorld[["longitude"]]
+lat <- coastlineWorld[["latitude"]]
+library(rgdal)
+proj <- "+proj=moll +ellps=sphere"
+xy <- rgdal::project(cbind(lon,lat), proj)
+ll <- rgdal::project(xy, proj, inv=TRUE)
+#plot(xy[,1], xy[,2], asp=1, type='l')
+par(mfrow=c(3,2))
+mapPlot(coastlineWorld, proj=proj)
+XY <- lonlat2map(lon, lat)
+LL <- map2lonlat(XY$x, XY$y)
+lines(XY$x, XY$y, col='red')
+plot(XY$x, XY$y, asp=1, col='red', type='l')
+hist((XY$x-xy[,1])/1e3)
+hist((XY$y-xy[,2])/1e3)
+
+hist((LL$longitude-ll[,1]))
+hist((LL$latitude-ll[,2]))
+if (!interactive()) dev.off()
