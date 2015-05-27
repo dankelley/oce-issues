1fbc704512997790f82ad6a2a63646b1189cf295
Dan Kelley <kelley.dan@gmail.com>
2015-05-27 10:28:09 -0300

    655

--- 655/655a.R
+++ 655/655a.R
@@ -1,5 +1,5 @@
 library(oce)
-source("~/src/oce/R/imagep.R")
+source("~/src/oce/R/map.R")
 library(gsw)
 data(coastlineWorld)
 
@@ -13,13 +13,13 @@ SA <- array(gsw_SA_from_SP(rep(35, length(lon)),
                            rep(100, length(lon)), lon, lat),
             dim=c(length(lo), length(la)))
 
-if (!interactive()) png('gsw_onland.png', width=7*72, height=4*72, type='cairo')
+if (!interactive()) png('655a.png', width=7*72, height=4*72, type='cairo')
 
 par(mfrow=c(2,1), mar=rep(1, 4))
 
 mapPlot(coastlineWorld)
 mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet)
-mtext("Expect: blue land, as in lowerpanel", side=3, line=0, font=2, col="magenta", adj=0)
+mtext("Expect: blue land, as in lower panel", side=3, line=0, font=2, col="magenta", adj=0)
 
 cm <- colormap(SA, missingColor = NA)
 mapPlot(coastlineWorld)
