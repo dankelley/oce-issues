442d8d181bf5a3b230f2c07270e37448208d4667
Dan Kelley <kelley.dan@gmail.com>
2015-06-03 13:56:48 -0300

    657

---
+++ 657/657.R
@@ -0,0 +1,10 @@
+library(oce)
+data(section)
+top <- sectionGrid(section, p=seq(0, 200, 5))
+bottom<- sectionGrid(section, p=seq(200, 6000, 100))
+if (!interactive()) png("657.png")
+layout(matrix(1:2, nrow=2), widths=1, heights=c(0.25, 0.75))
+plot(top, which="temperature", mar=c(1, 3, 1, 1))
+plot(bottom, which="temperature", mar=c(3, 3, 1, 1))
+if (!interactive()) dev.off()
+
