7e9c12d1675f33dc9aa9e1ab5ffeab4571c4779e
Dan Kelley <kelley.dan@gmail.com>
2015-06-06 10:16:38 -0300

    bremen

---
+++ 659/659a.R
@@ -0,0 +1,10 @@
+library(oce)
+ctd <- read.bremen("msm27_012.ctd")
+ladcp <- read.bremen("msm27_012.ladcp")
+
+if (!interactive()) png("659a_1.png")
+plot(ctd, span=5000)
+if (!interactive()) dev.off()
+if (!interactive()) png("659a_2.png")
+plot(ladcp)
+if (!interactive()) dev.off()
