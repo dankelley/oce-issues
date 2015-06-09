2f4a243e8e675ac08dc38161ad1bb7fe3357f03a
Dan Kelley <kelley.dan@gmail.com>
2015-06-09 15:08:51 -0300

    .

--- 640/07_5.R
+++ 640/07_5.R
@@ -14,7 +14,7 @@ dyn.load("polygon4.so")
 cleanAngle <- function(a)
     ifelse(a<(-180), a+360, ifelse(a>180, a-360, a))
 
-if (!interactive()) pdf("07.pdf", width=7, height=7, pointsize=8)
+if (!interactive()) pdf("07_5.pdf", width=7, height=7, pointsize=8)
 ##layout(matrix(c(1,2,3,3), 2, 2, byrow = TRUE), respect = TRUE)
 par(mar=c(2, 2, 1, 1), mgp=c(2, 0.7, 0))
 par(mfrow=c(2,2))
@@ -43,7 +43,9 @@ for (lon_0 in -60) {
     LOOK <- 998:999
     LOOK2 <- 333:334
     if (TRUE) {
-        par(mfrow=c(2,2), mar=c(1.5,1.5,0.5,0.5), mgp=c(2,0.5,0))
+        par(mar=c(1.5,1.5,0.5,0.5), mgp=c(2,0.5,0))
+        ##par(mfrow=c(2,2), mar=c(1.5,1.5,0.5,0.5), mgp=c(2,0.5,0))
+        layout(matrix(1:4,byrow=TRUE, ncol=2), widths=rep(1,2), heights=c(0.2, 0.8))
         plot(lon, lat, xlim=c(-180,180), ylim=c(-90,-63), type='l')
         polygon(lon, lat, col='lightgray')
         points(lon[LOOK], lat[LOOK], pch=20, col=2:3)
@@ -56,21 +58,22 @@ for (lon_0 in -60) {
         polygon(mod$xo, mod$yo, col='lightgray')
         abline(v=lon_0, col='red', lwd=1/2, lty='dotted')
 
-        plot(lon, lat, xlim=c(-75,-50), ylim=c(-85,-63), type='o', pch=20, cex=2/3)
+        plot(lon, lat, xlim=c(-62,-57), ylim=c(-85,-63), type='o', pch=20, cex=2/3)
         points(lon[LOOK], lat[LOOK], pch=20, col=2:3)
-        text(lon[LOOK], lat[LOOK], pos=4, labels=indices[LOOK], cex=2/3, col=2:3)
+        text(lon[LOOK], lat[LOOK], pos=4, labels=indices[LOOK], cex=2/3)
         points(lon[LOOK2], lat[LOOK2], pch=20, col=2:3)
-        text(lon[LOOK2], lat[LOOK2], pos=4, labels=indices[LOOK2], cex=2/3,col=2:3)
+        text(lon[LOOK2], lat[LOOK2], pos=4, labels=indices[LOOK2], cex=2/3)
         inside <- !is.na(lon)&(-68)<lon&lon<(-50)&(-65)<lat&lat<(-60)
         abline(v=lon_0, col='red', lwd=1/2, lty='dotted')
         ## xy <- locator(1)
         # #which.min(abs(lon-xy$x)+abs(lat-xy$y))
         points(lon[445], lat[445], pch=20, col=6)
-        text(lon[445], lat[445], 445, cex=2/3, pos=1,col=6)
+        text(lon[445], lat[445], 445, cex=2/3, pos=1)
 
-        plot(mod$xo, mod$yo, xlim=c(-80,-50), ylim=c(-85,-63), type='o', pch=20, cex=2/3)
-        points(mod$xo[mod$insideo], mod$yo[mod$insideo],pch=20, col='red')
+        plot(mod$xo, mod$yo, xlim=c(-62,-57), ylim=c(-85,-63), type='o', pch=20, cex=2/3)
         polygon(mod$xo, mod$yo, col='lightgray')
+        points(mod$xo, mod$yo, pch=20, col='black')
+        points(mod$xo[mod$insideo], mod$yo[mod$insideo],pch=20, col='red', cex=0.3)
         abline(v=lon_0, col='red', lwd=1/2, lty='dotted')
 
         message("SUSPECT: maybe the problem is with the cut point being at an odd spot, index 999")
