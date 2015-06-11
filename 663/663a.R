829891221e691ba11ff0a15c53829c419b941e73
Dan Kelley <kelley.dan@gmail.com>
2015-06-11 19:21:08 -0300

    663

---
+++ 663/663a.R
@@ -0,0 +1,13 @@
+library(oce)
+data(coastlineWorldFine, package="ocedata")
+proj <- "mercator"
+fill <- 'lightgray'
+latlim <- c(43,54)
+lonlim <- c(-67,-50)
+if (!interactive()) png("663a.png")
+par(mar=c(2,2,1,1), mfrow=c(2,1))
+mapPlot(coastlineWorldFine,latitudelim=latlim, longitudelim=lonlim,
+        proj="mercator", fill=fill, grid=c(2,2))
+mapPlot(coastlineWorldFine,latitudelim=latlim, longitudelim=lonlim,
+        proj="+proj=merc", fill=fill, grid=c(2,2))
+if (!interactive()) dev.off()
