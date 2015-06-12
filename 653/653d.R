eac181ea96534fa82e0aff9b751ba21c17dd59eb
Dan Kelley <kelley.dan@gmail.com>
2015-06-12 15:43:23 -0300

    tweak 653d.R for quirky rawTransform() call

--- 653/653d.R
+++ 653/653d.R
@@ -6,8 +6,11 @@ lat <- coastlineWorld[["latitude"]]
 n <- length(lon)-1
 
 library(rgdal)
-xy <- rawTransform(projfrom="+proj=longlat", projto="+proj=moll", n=n, x=lon[1:n], y=lat[1:n])
+## NOTE: need to cast 'n'; this is not hinted at in the (sparse) docs
+xy <- rawTransform(projfrom="+proj=longlat", projto="+proj=moll",
+                   n=as.integer(n), x=lon[1:n], y=lat[1:n])
 par(mfrow=c(2,1))
 plot(xy[[1]], xy[[2]], asp=1, type='l')
-ll <- rawTransform(projfrom="+proj=moll", projto="+proj=longlat", n=n, x=lon[1:n], y=lat[1:n])
+ll <- rawTransform(projfrom="+proj=moll", projto="+proj=longlat",
+                   n=as.integer(n), x=lon[1:n], y=lat[1:n])
 plot(ll[[1]], ll[[2]], asp=1, type='l')
