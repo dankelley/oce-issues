ccabb466150277e0f0ff36d46cbbbdd1724f0692
Dan Kelley <kelley.dan@gmail.com>
2015-06-13 11:10:58 -0300

    665

---
+++ 665/665a.R
@@ -0,0 +1,11 @@
+library(oce)
+data(coastlineWorld)
+if (!interactive()) png("665a.png", pointsize=9)
+par(mfrow=c(1, 2), mar=c(2, 2, 1, 1))
+lonlim <- c(-90, -70)
+latlim <- c(15, 20)
+mapPlot(coastlineWorld, proj="+proj=lcca +lat_0=20 +lon_0=-75",
+        longitudelim=lonlim, latitudelim=latlim)
+mapPlot(coastlineWorld, proj="+proj=lcca +lat_0=20 +lon_0=-75",
+        grid=5,longitudelim=lonlim, latitudelim=latlim)
+if (!interactive()) dev.off()
