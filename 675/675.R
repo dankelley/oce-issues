b23a365a34fce546a984f3a0fe6fb40bfcad78b3
Dan Kelley <kelley.dan@gmail.com>
2015-06-24 10:05:06 -0300

    675: start work

---
+++ 675/675.R
@@ -0,0 +1,16 @@
+library(oce)
+try(source("~/src/oce/R/landsat.R")) # lets author test changes to the code
+
+if (1 == length(list.files(pattern="^LC.*$"))) {
+    low <- list(longitude=-39.5, latitude=-4.5)
+    up <- list(longitude=-39, latitude=-4)
+    if (1 != length(ls(pattern="^ns$"))) { # buffer for speed of interactive testing
+        ns <- read.landsat("LC82170632015124LGN00", band="tirs1")
+    }
+    trim <- landsatTrim(ns, low, up, debug=3)
+    if (!interactive()) png("675.png")
+    plot(trim)
+    if (!interactive()) dev.off()
+} else {
+    message("675.R needs a directory named LC82170632015124LGN00")
+}
---
+++ 675/README.md
@@ -0,0 +1,3 @@
+The test file relies on LC82170632015124LGN00 being present here. It is
+not part of this git repository, so if you want to test this issue,
+download the data from USGS (or wherever) and put the directory here.
