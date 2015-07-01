dee019561113da8feb26d4b0a1a95a945cf9cbeb
Dan Kelley <kelley.dan@gmail.com>
2015-07-01 13:32:40 -0300

    675: fixed colorscale in two panels

--- 675/675.R
+++ 675/675.R
@@ -1,6 +1,6 @@
 library(oce)
-# try(source("~/src/oce/R/map.R"))       # lets author test changes to the code
-# try(source("~/src/oce/R/landsat.R"))   # lets author test changes to the code
+try(source("~/src/oce/R/map.R"))       # lets author test changes to the code
+try(source("~/src/oce/R/landsat.R"))   # lets author test changes to the code
 if (1 == length(list.files(pattern="^LC.*$"))) {
     low <- list(longitude=-39.5, latitude=-4.5)
     up <- list(longitude=-39, latitude=-4)
@@ -10,10 +10,10 @@ if (1 == length(list.files(pattern="^LC.*$"))) {
     trim <- landsatTrim(ns, low, up)
     if (!interactive()) png("675.png", width=7, height=3, unit="in", res=100, pointsize=9)
     par(mfrow=c(1,2))
-    plot(ns, band="temperature", col=oceColorsJet)
+    plot(ns, band="temperature", col=oceColorsJet, zlim=c(-35, 35))
     abline(h=c(up$latitude, low$latitude))
     abline(v=c(up$longitude, low$longitude))
-    plot(trim, band="temperature", col=oceColorsJet)
+    plot(trim, band="temperature", col=oceColorsJet, zlim=c(-35, 35))
     if (!interactive()) dev.off()
 } else {
     message("675.R needs a directory named LC82170632015124LGN00")
