bedbf26c5a0bd03cac6f6fd90e03a70162fa8f3a
Dan Kelley <kelley.dan@gmail.com>
2015-07-04 19:12:22 -0300

    670 revised a bit

--- 670/670a.R
+++ 670/670a.R
@@ -1,8 +1,18 @@
 library(oce)
 try(source("~/src/oce/R/ctd.R"))
 data(ctdRaw)
+TRIM <- c(200, 300)
+
 if (!interactive()) png("670a.png")
 par(mfrow=c(2,1))
 plotScan(ctdRaw)
-plotScan(ctdTrim(ctdRaw, "scan", parameters=c(200,300)))
+abline(v=TRIM, col='blue')
+mtext("(a) ", side=3, line=0, adj=1)
+mtext("EXPECT: oddness, equilibration, downcast, then upcast",
+      font=2, col='magenta', adj=0, side=3, line=0)
+plotScan(ctdTrim(ctdRaw, "scan", parameters=TRIM))
+abline(v=TRIM, col='red')
+mtext("EXPECT: trimmed to within the blue lines in (a)",
+      font=2, col='magenta', adj=0, side=3, line=0)
+mtext("(b) ", side=3, line=0, adj=1)
 if (!interactive()) dev.off()
