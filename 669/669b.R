14d1adb5b124bdb06e1264fe4330f73cd77f30c8
Dan Kelley <kelley.dan@gmail.com>
2015-07-05 14:02:38 -0300

    668b.R updated

--- 669/669b.R
+++ 669/669b.R
@@ -1,6 +1,7 @@
 library(oce)
-#try(source("~/src/oce/R/ctd.R"))
-try(source("~/src/R-richards/oce/R/ctd.R"))
+# DK to CR: looks like our file structure differs.
+try(source("~/src/oce/R/ctd.R"), silent=TRUE)
+#try(source("~/src/R-richards/oce/R/ctd.R"), silent=TRUE)
 data(ctd)
 set.seed(669)
 n <- length(ctd[["salinity"]])
@@ -9,8 +10,9 @@ lat <- ctd[["latitude"]] + rnorm(n, sd=0.05)
 ## ctdnew <- as.ctd(ctd[['salinity']], ctd[['temperature']], ctd[['pressure']],
 ##                  longitude = lon, latitude = lat)
 ctdnew <- ctd
-ctdnew@metadata$longitude <- lon
-ctdnew@metadata$latitude <- lat
+## DK to CR: I have moved longitude to data, so I've altered next 2 lines.
+ctdnew@data$longitude <- lon
+ctdnew@data$latitude <- lat
 ctdnewSubset <- subset(ctdnew, 200 <= scan & scan <= 300)
 ctdnewTrim <- ctdTrim(ctdnew, method='scan', parameters = c(200, 300))
 if (!interactive()) png("669b_1.png")
@@ -23,3 +25,6 @@ plot(ctdnewSubset, main='Trimmed with subset')
 if (!interactive()) png("669b_4.png")
 plot(ctdnewTrim, main='Trimmed with ctdTrim')
 if (!interactive()) dev.off()
+
+## DK to CR: I added a strong test (better than looking at graphs)
+stopifnot(all.equal(length(ctdnewSubset[['scan']]), length(ctdnewSubset[['longitude']])))
