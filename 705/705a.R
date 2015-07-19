60f644af359a176aa3de34277061b8f46fed4aa0
Dan Kelley <kelley.dan@gmail.com>
2015-07-19 11:22:17 -0300

    .

--- 705/705a.R
+++ 705/705a.R
@@ -1,6 +1,9 @@
 library(oce)
 library(testthat)
-try(source("~/src/oce/R/ctd.R"))
+data(ctd)
+expect_equal(ctd@metadata$waterDepth, NA)
+data(ctdRaw)
+expect_equal(ctdRaw@metadata$waterDepth, NA)
 ctd <- read.ctd(system.file("extdata", "ctd.cnv", package="oce"))
 expect_equal(ctd@metadata$waterDepth, NA)
 
