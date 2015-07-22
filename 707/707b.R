aefe2677c984ac80188c91934a9dba4518f0c6eb
Clark Richards <clark.richards@gmail.com>
2015-07-22 09:16:21 -0400

    707b

--- 707/707b.R
+++ 707/707b.R
@@ -5,23 +5,23 @@ data(coastlineWorldFine)
 
 load('coastline.rda')
 file <- '/data/archive/landsat/LC80100112013172LGN00'
-l <- read.landsat(file, band=1)
+l <- read.landsat(file, band=c('aerosol', 'tirs1'))
 ll <- list(longitude=-50.44143, latitude=68.86987)
 ur <- list(longitude=-50.15551, latitude=68.97754)
 lt <- landsatTrim(l, ll, ur)
 
 if (!interactive()) png('707b-%03d.png')
 
-plot(l)
+plot(l, band='tirs1')
 points(ll$longitude, ll$latitude, pch=19, col=2)
 points(ur$longitude, ur$latitude, pch=19, col=2)
 polygon(c(ll$longitude, ll$longitude, ur$longitude, ur$longitude),
         c(ll$latitude, ur$latitude, ur$latitude, ll$latitude),
         col='purple', lwd=2)
-lines(coastlineWorldFine[['longitude']], coastlineWorldFine[['latitude']])
+lines(coastlineWorldFine[['longitude']], coastlineWorldFine[['latitude']], lwd=2)
 mtext('EXPECT: Coastline aligned with coast, box aligns with trimmed image', col='purple', font=2)
 
-plot(lt, col=grey.colors)
+plot(lt, band='aerosol', col=grey.colors)
 points(ll$longitude, ll$latitude, pch=19)
 points(ur$longitude, ur$latitude, pch=19)
 lines(coastlineWorldFine[['longitude']], coastlineWorldFine[['latitude']])
