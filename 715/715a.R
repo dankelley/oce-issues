7a6cb39c7167cdca5a13eefc7295c4db523d6399
Dan Kelley <kelley.dan@gmail.com>
2015-08-12 15:05:22 -0300

    715

---
+++ 715/715a.R
@@ -0,0 +1,17 @@
+library(testthat)
+library(oce)
+path <- "/data/Barrow Strait Data/2003_2004/MooredCTD"
+file <- "MCTD_PRD2003001_1491_0770_1800.ODF"
+if (1 == length(list.files(path=path, pattern=file))) {
+    try(source('~/src/oce/R/imagep.R'))
+    try(source('~/src/oce/R/ctd.R'))
+    try(source('~/src/oce/R/odf.R'))
+    try(source('~/src/oce/R/oce.R'))
+    d <- read.oce("/data/Barrow Strait Data/2003_2004/MooredCTD/MCTD_PRD2003001_1491_0770_1800.ODF")
+    expect_equal(d@metadata$deploymentType, "moored")
+    dd <- read.odf("/data/Barrow Strait Data/2003_2004/MooredCTD/MCTD_PRD2003001_1491_0770_1800.ODF")
+    expect_equal(dd@metadata$deploymentType, "moored")
+} else {
+    message("715a.R only works if a local file is available")
+}
+
