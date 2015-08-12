091ba36c564c6fd7fe451e4832229f9203c1d726
Clark Richards <clark.richards@gmail.com>
2015-08-11 21:48:58 -0400

    712 testing

--- 712/712a.R
+++ 712/712a.R
@@ -4,8 +4,13 @@ source('~/src/oce/R/oce.R') # for updated oceMagic from rsktxt branch
 source('~/src/oce/R/imagep.R') # to allow oce.plot.ts to work
 source('~/src/oce/R/rsk.R') # for updated read.rsk
 
-d <- read.rsk('060130_20150720_1135.txt', type='txt', debug=10)
+## MH's file (that started this whole thing)
 ## d <- read.rsk('010858_20150225_1139.eng.txt', type='txt', debug=10)
+
+## d <- read.rsk('060130_20150720_1135.txt', type='txt', from=10, by='00:01', debug=10)
+## d <- read.rsk('060130_20150720_1135.txt', type='txt', debug=10)
+## d <- read.rsk('060130_20150720_1135.txt', debug=10)
+d <- read.oce('060130_20150720_1135.txt', debug=10)
 
 if (!interactive()) png('712a.png')
 
