21dc2da9a26fa1798fb30c44a9392236c207373e
Dan Kelley <kelley.dan@gmail.com>
2015-08-14 11:37:06 -0300

    715b

---
+++ 715/715b.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+if (1 == length(list.files(pattern="CTD_DGR2006001_9_1_DN.ODF"))) {
+    library(oce)
+    source("~/src/oce/R/AllClass.R")
+    source("~/src/oce/R/imagep.R")
+    source("~/src/oce/R/oce.R")
+    source("~/src/oce/R/odf.R")
+    source("~/src/oce/R/ctd.R")
+    source("define_ODF_header.R")
+    source("read_ODF.R")
+    if (!interactive()) pdf("715b.pdf")
+
+    ## TEST 1: CTD file in Barrow Strait
+    ODF1 <- read_odf("CTD_DGR2006001_9_1_DN.ODF")
+    odf1 <- ODF2oce(ODF1)
+    plot(odf1, span=1000)
+    summary(odf1)
+
+    ## TEST 2: moored CTD file in Barrow Strait
+    ODF2 <- read_odf("MCTD_DGR2005001_1576_0862_1800.ODF")
+    odf2 <- ODF2oce(ODF2)
+    plot(subset(odf2, time > as.POSIXct("2005-11-01", tz="UTC")))
+    summary(odf2)
+
+    ## TEST 3: another ODF file, used in early tests
+    odf3 <- read.oce("CTD_BCD2010666_01_01_DN.ODF")
+    summary(odf3)
+    plot(odf3)
+
+    ## TEST 4: another ODF file, used in early tests
+    odf4 <- read.oce("MVCTD_HUD2008004_021_001_DN.ODF")
+    summary(odf4)
+    plot(odf4, span=1000)
+    if (!interactive()) dev.off()
+} else {
+    warning("715b.R: only the developers can do tests here, since they have some private files")
+}
---
+++ 715/README.md
@@ -0,0 +1,2 @@
+The tests in this directory require some private .R and .ODF files; only
+developers can do anything meaningful here.
