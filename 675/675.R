63f10550a32d7482a220dfa8ac60ea4838682aca
Dan Kelley <kelley.dan@gmail.com>
2015-08-15 14:54:21 -0300

    debugging 675

--- 675/675.R
+++ 675/675.R
@@ -1,13 +1,16 @@
 library(oce)
-#try(source("~/src/oce/R/map.R"))       # lets author test changes to the code
-#try(source("~/src/oce/R/landsat.R"))   # lets author test changes to the code
+try(source("~/src/oce/R/landsat.R"))   # lets author test changes to the code
 if (1 == length(list.files(pattern="^LC.*$"))) {
     low <- list(longitude=-39.5, latitude=-4.5)
     up <- list(longitude=-39, latitude=-4)
     if (1 != length(ls(pattern="^ns$"))) { # buffer for speed of interactive testing
+        message("reading file...")
         ns <- read.landsat("LC82170632015124LGN00", band="tirs1")
+        message("... done")
+    } else {
+        message("using cached data for speed")
     }
-    trim <- landsatTrim(ns, low, up)
+    trim <- landsatTrim(ns, low, up, debug=3)
     if (!interactive()) png("675.png", width=7, height=3, unit="in", res=100, pointsize=9)
     par(mfrow=c(1,2))
     plot(ns, band="temperature", col=oceColorsJet, zlim=c(-35, 35))
