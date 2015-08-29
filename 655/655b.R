1c2dc7bbd0393d88ae00b5ca4e0f852711faf93d
Dan Kelley <kelley.dan@gmail.com>
2015-08-29 15:04:31 -0300

    655

--- 655/655b.R
+++ 655/655b.R
@@ -11,7 +11,7 @@ par(mfrow=c(2,2), mar=c(2, 2, 1, 1))
 lonlim <- c(-70,-50)
 latlim <- c(40,50)
 topo <- subset(topoWorld, latlim[1] < latitude & latitude < latlim[2])
-topo <- subset(topo, (360+lonlim[1]) < longitude & longitude < (360+lonlim[2]))
+topo <- subset(topo, lonlim[1] < longitude & longitude < lonlim[2])
 breaks <- seq(-2000, 0, 100)
 col <- oceColorsGebco
 cm <- colormap(col=col, breaks=breaks, missingColor="red")
