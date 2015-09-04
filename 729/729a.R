93f5b348cba6c40bc85f3cf1b7e6ac02a9d1cd3f
Dan Kelley <kelley.dan@gmail.com>
2015-09-04 12:03:50 -0300

    ODF2oce() handles NA codes now

--- 729/729a.R
+++ 729/729a.R
@@ -2,11 +2,18 @@ if (length(list.files(pattern="MADCPS_hud2013021_1841_3367-48_3600.ODF"))) {
     library(oce)
     library(testthat)
     try(source("~/src/oce/R/odf.R"))
-    source("read_ODF.R")
+    source("read_odf.R")
     source("define_ODF_header.R")
     d <- read_odf("MADCPS_hud2013021_1841_3367-48_3600.ODF")
     dd <- ODF2oce(d)
     expect_equal(names(dd@data), c("u", "v", "w", "error", "a", "g", "time"))
+    nd <- length(names(dd@data))
+    for (i in 1:nd) {
+        if (is.numeric(dd@data[[i]])) {
+            ## check on 101 because percent-good goes from 0 to 100%
+            expect_less_than(diff(range(dd@data[[i]], na.rm=TRUE)), 101)
+        }
+    }
 } else {
     message("729a.R requires a file named MADCPS_hud2013021_1841_3367-48_3600.ODF to work")
 }
