7a07f71162426c693704f4ce36183c72287a9a2e
Dan Kelley <kelley.dan@gmail.com>
2015-10-19 06:08:11 -0300

    .

--- 756/756a.R
+++ 756/756a.R
@@ -1,3 +1,4 @@
+options(warn=3)
 if (length(list.files(pattern="CTD_HUD2014030_163_1_DN.ODF"))) {
     library(ODF)                           # DK version, patched to work in his locale; not open-source
     library(oce)
@@ -5,7 +6,7 @@ if (length(list.files(pattern="CTD_HUD2014030_163_1_DN.ODF"))) {
     try(source("~/src/oce/R/odf.R"))
     d <- read_odf("CTD_HUD2014030_163_1_DN.ODF")
     dd <- as.oce(d)
-    names(dd@data)
+    str(dd)
 } else {
     message("729a.R requires a file named CTD_HUD2014030_163_1_DN.ODF to work; DK can send to CR if needed")
 }
