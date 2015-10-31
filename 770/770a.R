ea181ee816a74ded6acdee1b77ff382119b6f3e1
Dan Kelley <kelley.dan@gmail.com>
2015-10-30 21:11:53 -0300

    770a

--- 770/770a.R
+++ 770/770a.R
@@ -1,16 +1,31 @@
 library(oce)
+
+rms <- function(x) sqrt(mean(x^2)) # convenience function
+
 data(section)
 lon <- section[["longitude", "byStation"]]
 lat<- section[["latitude", "byStation"]]
-n <- 24
-lon <- lon[1:n]
-lat <- lat[1:n]
+lon <- lon
+lat <- lat
 lonR <- lon[1]
 latR <- lat[1]
+## 1. ellipse
 xy <- geodXy(lon, lat, lonR, latR)
 
-## spherical
-mperdeg <- geodDist(0, 36, 0, 37) * 1e3 # mid-latitude estimate
-LAT <- latR + xy$y / mperdeg
-LON <- lonR + xy$x / mperdeg / cos(pi*LAT / 180)
-data.frame(lon=lon, lat=lat, x=xy$x, y=xy$y, LON=LON, LAT=LAT, lonerr=lon-LON, laterr=lat-LAT)
+## 2. sphere
+mperdeg <- geodDist(0, mean(lat)-0.5, 0, mean(lat)+0.5) * 1e3 # mid-latitude estimate
+X <- (lon - lonR) * mperdeg * cos(lat * pi / 180)
+Y <- (lat - latR) * mperdeg
+XY <- list(x=X, y=Y)
+
+if (!interactive()) png("770a.png")
+par(mfrow=c(2,1), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
+plot(lon, lat, asp=1/cos(median(lat*pi/180)))
+plot(xy$x/1e3, xy$y/1e3, asp=1)
+mtext(sprintf("ellipse-sphere RMS deviation: x %.2f km, y %.2f km",
+              rms(xy$x-XY$x)/1e3, rms(xy$y-XY$y)/1e3),
+      side=3, line=0)
+mtext(sprintf("ellipse-sphere RMS deviation / span: x %.2g, y %.2g",
+              rms(xy$x-XY$x)/1e3/diff(range(xy$x)), rms(xy$y-XY$y)/1e3/diff(range(xy$y))),
+      side=3, line=-1)
+if (!interactive()) dev.off()
