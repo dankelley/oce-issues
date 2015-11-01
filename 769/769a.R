f83f7834cc651002d18831ef20655cb13cd23839
Dan Kelley <kelley.dan@gmail.com>
2015-11-01 15:09:23 -0400

    769

---
+++ 769/769a.R
@@ -0,0 +1,7 @@
+library(oce)
+source("~/src/oce/R/odf.R")
+## system.time(d <- read.odf("../768/MCM_HUD2013021_1841_3508_300.ODF")) user 10.059s (home machine)
+d <- read.odf("../768/MCM_HUD2013021_1841_3508_300.ODF", debug=100) # if > 99, we get timing info
+
+## 10s orig
+## 1.8s if only read 1000 lines
