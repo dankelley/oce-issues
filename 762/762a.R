847a2992d2272ab05346bff5cdd8e2eb78b73500
Clark Richards <clark.richards@gmail.com>
2015-11-09 15:56:05 -0500

    762a breaks for plotProfile

--- 762/762a.R
+++ 762/762a.R
@@ -22,7 +22,7 @@ options(device='x11')
 
 if (!interactive()) png('726a-%03d.png')
 
-d <- read.rsk('065583_20150516_1717.rsk')
+d <- read.oce('065583_20150516_1717.rsk')
 plot(d)
 
 ctd <- ctdTrim(as.ctd(d), parameters=list(pmin=10))
@@ -32,5 +32,11 @@ plotProfile(ctd, xtype='temperature')
 plotProfile(ctd, xtype='temperature2', col=2, add=TRUE)
 legend('bottomright', ctd[['names']][grep('temp', ctd[['names']])],
        lty=1, col=1:2)
+
+plotProfile(ctd, xtype='temperature2')
+
+trimTimes <- as.POSIXct(c(1431751235, 1431753589), origin='1970-01-01', tz='UTC')
+ctd <- ctdTrim(as.ctd(d), 'range', parameters=list(item='time', from=trimTimes[1], to=trimTimes[2]),
+               debug=10)
 
 if (!interactive()) dev.off()
