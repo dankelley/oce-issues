3c6a61eb8e49a85e76f5a50efec8ce1b2dfe9ddb
Dan Kelley <kelley.dan@gmail.com>
2015-11-16 13:46:15 -0400

    777b.R

--- 777/777b.R
+++ 777/777b.R
@@ -6,8 +6,8 @@ file <- "MS2015-150kHz002_000001.ENX"
 if (1 == length(list.files(pattern="*.ENX"))) {
     ## easier to debug without skipping through the file
     d <- read.adp.rdi(file, from=1, to=4, debug=999) ## 999 browses read.adp.rdi()
-    expect_true('br' %in% names(d@data), info="This file should have bottom range, br")
-    expect_true('bv' %in% names(d@data), info="This file should have bottom velocity, bv")
+    ## expect_true('br' %in% names(d@data), info="This file should have bottom range, br")
+    ## expect_true('bv' %in% names(d@data), info="This file should have bottom velocity, bv")
 } else {
     message("cannot run the test in 777a.R because it needs a file named ", file)
 }
