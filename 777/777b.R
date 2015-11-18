5003cc8cc26936add1466db1312af06dc9179313
Dan Kelley <kelley.dan@gmail.com>
2015-11-18 15:14:56 -0400

    777

--- 777/777b.R
+++ 777/777b.R
@@ -1,3 +1,4 @@
+if (!interactive()) png("777b.png")
 library(oce)
 library(testthat)
 try(source("~/src/oce/R/adp.R"))
@@ -5,9 +6,15 @@ try(source("~/src/oce/R/adp.rdi.R"))
 file <- "MS2015-150kHz002_000001.ENX"
 if (1 == length(list.files(pattern="*.ENX"))) {
     ## easier to debug without skipping through the file
-    d <- read.adp.rdi(file, from=1, to=4, debug=999) ## 999 browses read.adp.rdi()
+    d <- read.adp.rdi(file, from=1, to=20, debug=999) ## 999 browses read.adp.rdi()
     ## expect_true('br' %in% names(d@data), info="This file should have bottom range, br")
     ## expect_true('bv' %in% names(d@data), info="This file should have bottom velocity, bv")
+    par(mfrow=c(2,1))
+    plot(d, which="bottomRange")
+    plot(d, which="bottomVelocity")
+    print(head(d[["br"]]))
+    print(head(d[["bv"]]))
 } else {
     message("cannot run the test in 777a.R because it needs a file named ", file)
 }
+if (!interactive()) dev.off()
