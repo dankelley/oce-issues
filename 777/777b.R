dbb44070b32f36120a9bbce6131be3fd96a74cba
Dan Kelley <kelley.dan@gmail.com>
2015-11-19 08:34:18 -0400

    clean debug msg

--- 777/777b.R
+++ 777/777b.R
@@ -6,7 +6,7 @@ try(source("~/src/oce/R/adp.rdi.R"))
 file <- "MS2015-150kHz002_000001.ENX"
 if (1 == length(list.files(pattern="*.ENX"))) {
     ## easier to debug without skipping through the file
-    d <- read.adp.rdi(file, from=1, to=10, debug=999) ## 999 browses read.adp.rdi()
+    d <- read.adp.rdi(file, from=1, to=10) ## 999 browses read.adp.rdi()
     ## expect_true('br' %in% names(d@data), info="This file should have bottom range, br")
     ## expect_true('bv' %in% names(d@data), info="This file should have bottom velocity, bv")
     par(mfrow=c(2,5))
@@ -26,4 +26,3 @@ if (1 == length(list.files(pattern="*.ENX"))) {
     message("cannot run the test in 777a.R because it needs a file named ", file)
 }
 if (!interactive()) dev.off()
-message("\n*** NOTE ***\ndebug=999 slows processing enormously -- but it's temporary to fix 777")
