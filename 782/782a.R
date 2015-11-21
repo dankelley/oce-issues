677e89fd8767d58de6b0851929749ebfb9a4a0ad
Dan Kelley <kelley.dan@gmail.com>
2015-11-21 14:15:10 -0400

    782: use proper names for lon and lat

--- 782/782a.R
+++ 782/782a.R
@@ -5,8 +5,8 @@ try(source("~/src/oce/R/adp.rdi.R"))
 file <- "MS2015-150kHz002_000001.ENX"
 if (1 == length(list.files(pattern="*.ENX"))) {
     d <- read.adp.rdi(file, from=1, to=3, debug=3)
-    expect_true('slongitude' %in% names(d@data), info="This file should have longitude, slongitude")
-    expect_true('slatitude' %in% names(d@data), info="This file should have latitude, slatitude")
+    expect_true('firstLongitude' %in% names(d@data), info="This file should have longitude, firstLongitude")
+    expect_true('firstLatitude' %in% names(d@data), info="This file should have latitude, firstLatitude")
 } else {
     message("cannot run the test in 782a.R because it needs a file named ", file)
 }
