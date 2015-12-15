04638a0a95bf89573363989671be5bf685c3a9f4
Clark Richards <clark.richards@gmail.com>
2015-12-15 10:06:42 -0500

    800; add a test case for a non-standard data column

---
+++ 800/800c.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+library(oce)
+try(source('~/src/oce/R/ctd.R'))
+try(source('~/src/R-richards/oce/R/ctd.R'))
+
+data(ctd)
+ctd <- ctdAddColumn(ctd, rnorm(length(ctd[['time']])), 'stupid', 'St')
+
+if (!interactive()) pdf('800c.pdf')
+
+plotProfile(ctd, lty=2)
+
+plotProfile(ctd, xtype="stupid", lty=2)
+plotProfile(ctd, xtype="salinity", lty=2)
+plotProfile(ctd, xtype="conductivity", lty=2)
+plotProfile(ctd, xtype="temperature", lty=2)
+plotProfile(ctd, xtype="theta", lty=2)
+plotProfile(ctd, xtype="density", lty=2)
+plotProfile(ctd, xtype="index", lty=2)
+plotProfile(ctd, xtype="salinity+temperature", lty=2)
+plotProfile(ctd, xtype="N2", lty=2)
+plotProfile(ctd, xtype="density+N2", lty=2)
+plotProfile(ctd, xtype="density+dpdt", lty=2)
+plotProfile(ctd, xtype="spice", lty=2)
+plotProfile(ctd, xtype="Rrho", lty=2)
+plotProfile(ctd, xtype="RrhoSF", lty=2)
+
+if (!interactive()) dev.off()
+
