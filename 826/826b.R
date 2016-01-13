9943b3ac032188d97c04cdff543d48dd1b837815
Dan Kelley <kelley.dan@gmail.com>
2016-01-13 10:15:31 -0400

    .

--- 826/826b.R
+++ 826/826b.R
@@ -1,6 +1,7 @@
 # Demonstrate RStudio problem with margins.
 #
-# GOAL: create a 1-inch wide plot, near right edge of plot area.
+# GOAL: create a 1-inch wide plot, near right edge of plot area, with a 0.5 inch margin
+# at bottom, right, and top.
 #
 # MACHINE: OSX (macbook pro, retina, 13-inch)
 #
@@ -21,16 +22,23 @@
 # RESULT 2: no plot; instead the error 'figure margins too large'
 #    > print(pin)
 #    [1] 12.121111  4.951667
+#    
 #    > print(mai)
 #    [1]  0.50000 11.12111  0.50000  0.50000
+#    
+#    > par('mai')
+#    [1] 1.02 0.82 0.82 0.42
+#    
 #    > par('csi')
 #    [1] 0.2
+#    
 #    > par('cra')
 #    [1] 10.8 14.4
 
-m <- 0.5
+m <- 0.5 # margin at bottom, top, and right
+w <- 0.5 # width of plot region
 pin <- par('pin')
-mai <- c(m, pin[1]-2*m, m, m)
+mai <- c(m, pin[1] - w - m, m, m)
 print(pin)
 print(mai)
 par('mai')
