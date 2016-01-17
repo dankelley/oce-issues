81416c5d2b31cb524c840cde6cd3db3050d60a6c
Clark Richards <clark.richards@gmail.com>
2016-01-16 22:29:06 -0500

    Issue 830; test argo dataMode

---
+++ 830/830a.R
@@ -0,0 +1,19 @@
+rm(list=ls())
+library(oce)
+try(source("~/src/oce/R/argo.R"))
+try(source("~/src/R-richards/oce/R/argo.R"))
+
+d <- read.argo('~/src/oce/create_data/argo/6900388_prof.nc')
+D <- subset(d, dataMode == "D")
+R <- subset(d, dataMode == "R")
+
+summary(d)
+
+if (!interactive()) png("830a.png")
+plot(d)
+points(D[['longitude']], D[['latitude']], col=2)
+points(R[['longitude']], R[['latitude']], col=1)
+legend('bottomright', c('delayed mode', 'realtime'),
+       pch=1, col=2:1)
+if (!interactive()) dev.off()
+
