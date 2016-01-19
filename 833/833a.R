6454a2a676e0396869aaf25095b6a96d77d8681f
Dan Kelley <kelley.dan@gmail.com>
2016-01-19 06:34:48 -0400

    833

---
+++ 833/833a.R
@@ -0,0 +1,10 @@
+library(oce)
+library(ncdf4)
+f <- nc_open("/Users/kelley/src/oce/create_data/argo/6900388_prof.nc")
+varNames <- names(f$var)
+varNames
+for (name in varNames) {
+    t <- try(v <- ncvar_get(f, name), silent=TRUE)
+    if (!inherits(t, "try-error")) message(paste("name: ", name, " can be read OK"))
+}
+
