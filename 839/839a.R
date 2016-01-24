d842ea576073a9247af7894649309eec028e7910
Dan Kelley <kelley.dan@gmail.com>
2016-01-24 12:55:59 -0400

    amsr

--- 839/839a.R
+++ 839/839a.R
@@ -1,17 +1,33 @@
 library(oce)
-## try(source("~/src/oce/R/amsr.R"))
-d <- read.amsr("f34_20160102v7.2.gz")
+library(testthat)
+if (!length(ls(pattern='^d$')))
+    d <- read.amsr("f34_20160102v7.2.gz")
 ## Test accessors (sensible for temperature?)
 median(d[["SSTDay"]], na.rm=TRUE)
 median(d[["SSTNight"]], na.rm=TRUE)
-## Test summary (are they in right units?)
-summary(d)
 
+## Visual test: units OK?
+summary(d)
 
 ## Plot default and named channels
 if (!interactive()) png("839a.png", pointsize=9)
-par(mfrow=c(2,1))
-plot(d) # default SSTDay
-plot(d, 'SSTNight')
+par(mfrow=c(3,1))
+plot(d, col=oceColorsJet) # default SSTDay
+lon <- d[["longitude"]]
+lat <- d[["latitude"]]
+loni <- 1272
+lati <- 580
+points(lon[loni], lat[lati])
+plot(d, 'SSTNight', col=oceColorsJet)
+points(lon[loni], lat[lati])
+plot(d, 'SST', col=oceColorsJet)
+points(lon[loni], lat[lati])
 if (!interactive()) dev.off()
 
+## Check whether 'raw' access OK
+expect_true(is.numeric(d[['SSTDay']][loni,lati]))
+expect_true(is.raw(d[['SSTDay','raw']][loni,lati]))
+d[['SSTDay']][loni,lati]
+d[['SSTNight']][loni,lati]
+d[['SST']][loni,lati]
+plot(d, 'SST', col=oceColorsJet)
