c8f0c467d282bf8803739334d95fbf02a08e4109
Dan Kelley <kelley.dan@gmail.com>
2016-02-10 12:29:10 -0400

    .

--- 872/872a.R
+++ 872/872a.R
@@ -7,7 +7,8 @@ t <- sealevel[["time"]][1:72]
 eta <- sealevel[["elevation"]][1:72]
 eta <- eta - mean(eta)
 if (!interactive()) png("872a.png")
-oce.plot.ts(t, eta, type='p', pch=20)
+#oce.plot.ts(t, eta, type='p', pch=20)
+oce.plot.ts(t, eta)
 tr <- range(t)
 tw <- seq(tr[1], tr[2], by="15 min")
 e <- webtide("predict",lat=lat,lon=lon,time=tw,plot=FALSE)
