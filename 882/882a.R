9b34f4531e2e0edfed86e205c560b3e2438ae3e8
Clark Richards <clark.richards@gmail.com>
2016-03-02 14:11:05 -0500

    882; changes for RSK version 1.12.2

---
+++ 882/882a.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+library(oce)
+try(source('~/src/oce/R/rsk.R'))
+try(source('~/src/R-richards/oce/R/rsk.R'))
+
+if (!interactive()) png('882a-%03d.png')
+## read the "old" rsk version
+d <- read.rsk('060130_20150720_1135.rsk')
+plot(d)
+title('rskVersion < 1.12.2')
+## read the "upgraded" version (with datasetID in data table)
+d <- read.rsk('060130_20150720_1135_upgraded.rsk')
+plot(d)
+title('rskVersion >= 1.12.2')
+if (!interactive()) dev.off()
