79d02992d2748cfd421a69018c8c5745d268f155
Dan Kelley <kelley.dan@gmail.com>
2016-03-07 15:46:16 -0400

    887

--- 887/887b.R
+++ 887/887b.R
@@ -1,4 +1,9 @@
 library(oce)
 try(source("~/src/oce/R/adv.nortek.R")) # contains some message("TEST..") lines
+message("* ~/src/dolfyn/example_data/vector_data_imu01.VEC")
 d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=1000)
+names(d@data)
+
+message("* ~/src/dolfyn/example_data/vector_data01.VEC")
+d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=1000)
 names(d@data)
