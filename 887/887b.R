974768a407c94a7f491ea9d28d3a163449fd150c
Dan Kelley <kelley.dan@gmail.com>
2016-03-07 19:40:14 -0400

    887

--- 887/887b.R
+++ 887/887b.R
@@ -1,9 +1,21 @@
 library(oce)
+try(source("~/src/oce/R/imagep.R")) # contains some message("TEST..") lines
+try(source("~/src/oce/R/adv.R")) # contains some message("TEST..") lines
 try(source("~/src/oce/R/adv.nortek.R")) # contains some message("TEST..") lines
-message("* ~/src/dolfyn/example_data/vector_data_imu01.VEC")
-d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=1000)
+try(source("~/src/oce/R/oce.R"))
+try(source("~/src/oce/R/misc.R"))
+message("\n* ~/src/dolfyn/example_data/vector_data_imu01.VEC")
+message(oceMagic("~/src/dolfyn/example_data/vector_data_imu01.VEC"))
+d <- read.oce("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=100)
+plot(d)
+d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=100)
+plot(d)
 names(d@data)
 
-message("* ~/src/dolfyn/example_data/vector_data01.VEC")
-d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=1000)
+message("\n* ~/src/dolfyn/example_data/vector_data01.VEC")
+message(oceMagic("~/src/dolfyn/example_data/vector_data01.VEC"))
+d <- read.oce("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=100)
+plot(d)
+d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=100)
+plot(d)
 names(d@data)
