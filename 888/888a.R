9b6792d28117118543a4e55c1dd97fc1ff7219f3
Dan Kelley <kelley.dan@gmail.com>
2016-03-09 13:57:57 -0400

    888

--- 888/888a.R
+++ 888/888a.R
@@ -1,6 +1,7 @@
 library(oce)
-source("~/src/oce/R/adv.nortek.R")
-source("~/src/oce/R/oce.R")
+# try(source("~/src/oce/R/adv.nortek.R"))
+# try(source("~/src/oce/R/oce.R"))
+
 # only works for Kelley and Richards
 if (1 == length(list.files(path="/data/archive/sleiwex/2008/moorings/m05/adv/nortek_1943/raw",
                            pattern="adv_nortek_1943.vec"))) {
