9bd97c99c27a52d5d173d6edc8d25197e9fb4c5c
Dan Kelley <kelley.dan@gmail.com>
2016-03-10 12:45:56 -0400

    887

--- 887/887b.R
+++ 887/887b.R
@@ -1,21 +1,14 @@
+## use dolfyn-supplied test data, one file with IMU, the other without it.
+
 library(oce)
-try(source("~/src/oce/R/imagep.R")) # contains some message("TEST..") lines
-try(source("~/src/oce/R/adv.R")) # contains some message("TEST..") lines
-try(source("~/src/oce/R/adv.nortek.R")) # contains some message("TEST..") lines
-try(source("~/src/oce/R/oce.R"))
-try(source("~/src/oce/R/misc.R"))
+
 message("\n* ~/src/dolfyn/example_data/vector_data_imu01.VEC")
-message(oceMagic("~/src/dolfyn/example_data/vector_data_imu01.VEC"))
-d <- read.oce("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=100)
-plot(d)
-d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data_imu01.VEC", from=1, to=100)
-plot(d)
-names(d@data)
+dimu <- read.oce("~/src/dolfyn/example_data/vector_data_imu01.VEC")
+plot(dimu)
+summary(dimu)
 
 message("\n* ~/src/dolfyn/example_data/vector_data01.VEC")
-message(oceMagic("~/src/dolfyn/example_data/vector_data01.VEC"))
-d <- read.oce("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=100)
+d <- read.oce("~/src/dolfyn/example_data/vector_data01.VEC")
 plot(d)
-d <- read.adv.nortek("~/src/dolfyn/example_data/vector_data01.VEC", from=1, to=100)
-plot(d)
-names(d@data)
+summary(d)
+
