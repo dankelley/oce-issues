5820bd0b0cfb69da3c137005ad75084f053ac2ae
Dan Kelley <kelley.dan@gmail.com>
2016-03-12 11:49:41 -0400

    889

--- 889/889a.R
+++ 889/889a.R
@@ -5,6 +5,8 @@ u <- rep(0.25, 10)
 v <- u
 dy <- 0.5
 
+if (!interactive()) png("889a.png")
+
 drawDirectionField(x, y, u, v, scalex=1, type=1)
 
 drawDirectionField(x, y, u, v, scalex=1, type=1)
@@ -19,4 +21,6 @@ drawDirectionField(x, y, u, v, scalex=1, type=2, add=TRUE, length=0.1, col='red'
 y <- y + dy
 drawDirectionField(x, y, u, v, scalex=1, type=2, add=TRUE, length=0.025, col='blue')
 y <- y + dy
+
+if (!interactive()) dev.off()
 
