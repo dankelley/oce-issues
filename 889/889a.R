eb8672f6f9208bf18e317ef8068585a305574e0d
Dan Kelley <kelley.dan@gmail.com>
2016-03-12 11:44:27 -0400

    889

---
+++ 889/889a.R
@@ -0,0 +1,22 @@
+library(oce)
+x <- 1:10
+y <- 1:10
+u <- rep(0.25, 10)
+v <- u
+dy <- 0.5
+
+drawDirectionField(x, y, u, v, scalex=1, type=1)
+
+drawDirectionField(x, y, u, v, scalex=1, type=1)
+y <- y + dy
+drawDirectionField(x, y, u, v, scalex=1, type=1, add=TRUE, length=0.1, col='red')
+y <- y + dy
+drawDirectionField(x, y, u, v, scalex=1, type=1, add=TRUE, length=0.025, col='blue')
+y <- y + dy
+drawDirectionField(x, y, u, v, scalex=1, type=2, add=TRUE)
+y <- y + dy
+drawDirectionField(x, y, u, v, scalex=1, type=2, add=TRUE, length=0.1, col='red')
+y <- y + dy
+drawDirectionField(x, y, u, v, scalex=1, type=2, add=TRUE, length=0.025, col='blue')
+y <- y + dy
+
