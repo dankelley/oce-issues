8668e1eb31fa4238c6c503032d9233480ee4b2f4
Dan Kelley <kelley.dan@gmail.com>
2016-03-15 10:49:27 -0300

    887a

--- 887/887a.R
+++ 887/887a.R
@@ -1,5 +1,11 @@
 library(oce)
-d1 <- read.adv.nortek("~/Dropbox/OCE-ADV-IMU-Mr2/15080902.vec")
-names(d1@data)
+## d1 <- read.adv.nortek("~/Dropbox/OCE-ADV-IMU-Mr2/15080902.vec")
+## vectorShow(d1[['time']])
+
 dI <- read.adv.nortek("~/Dropbox/OCE-ADV-IMU-Mr2/I5080902.VEC")
-names(dI@data)
+vectorShow(dI[['time']])
+vectorShow(dI[['IMUtime']])
+
+look <- 1:100
+oce.plot.ts(dI[['time']][look], dI[['IMUtime']][look], type='o')
+
