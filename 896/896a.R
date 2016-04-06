9fcb1a5c113a0534a8d783d14418826b239b981b
Dan Kelley <kelley.dan@gmail.com>
2016-04-06 18:15:49 -0300

    896a now shows ctd flag broken

--- 896/896a.R
+++ 896/896a.R
@@ -1,12 +1,21 @@
 library(oce)
+library(testthat)
 data(section)
-stn <- section[["station", 100]]
-message("\n*** SECTION ***")
+ctd <- section[["station", 100]]
 ## this stn has a few points with salinityFlag==3
-STN <- handleFlags(stn, flags=list(salinity=c(1, 3:9)))
-print(data.frame(oldS=stn[['salinity']], flag=stn[['salinityFlag']], new=STN[['salinity']]))
+ctdNew <- handleFlags(ctd, flags=list(salinity=c(1, 3:9)))
+# print(head(data.frame(oldS=ctd[['salinity']], flag=ctd[['salinityFlag']], new=ctdNew[['salinity']])))
+cat("ctd salinity: orig had", sum(is.na(ctd[['salinity']])), "NA values; new has",
+    sum(is.na(ctdNew[['salinity']])), "\n")
+expect_equal(sum(is.na(ctd[["salinity"]])), 0)
+expect_equal(sum(is.na(ctdNew[["salinity"]])), 2)
 
 data(argo)
-message("\n\n*** ARGO ***")
 argoNew <- handleFlags(argo, flags=list(salinity=4:5))
+#print(head(data.frame(oldS=argo[['salinity']], flag=argo[['salinityFlag']], new=argoNew[['salinity']])))
+cat("argo salinity: orig had", sum(is.na(argo[['salinity']])), "NA values; new has",
+    sum(is.na(argoNew[['salinity']])), "\n")
+
+expect_equal(sum(is.na(argo[["salinity"]])), 90)
+expect_equal(sum(is.na(argoNew[["salinity"]])), 110)
 
