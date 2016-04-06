280826424d2e839141b5fa4ffdcf5201a6a0b996
Dan Kelley <kelley.dan@gmail.com>
2016-04-06 15:20:57 -0300

    896a now shows ctd flag broken

--- 896/896a.R
+++ 896/896a.R
@@ -2,7 +2,9 @@ library(oce)
 data(section)
 stn <- section[["station", 100]]
 message("\n*** SECTION ***")
-STN <- handleFlags(stn, flags=list(salinity=c(1, 4:9)))
+## this stn has a few points with salinityFlag==3
+STN <- handleFlags(stn, flags=list(salinity=c(1, 3:9)))
+print(data.frame(oldS=stn[['salinity']], flag=stn[['salinityFlag']], new=STN[['salinity']]))
 
 data(argo)
 message("\n\n*** ARGO ***")
