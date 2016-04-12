5542afde978f97e074c4813c7b9bc057fc239540
Clark Richards <clark.richards@gmail.com>
2016-04-12 09:31:02 -0400

    .

--- 922/922a.R
+++ 922/922a.R
@@ -1,4 +1,10 @@
 rm(list=ls())
 library(oce)
+try(source('~/src/oce/R/ctd.R'))
+try(source('~/src/R-richards/oce/R/ctd.R'))
 
 d <- read.oce('dBdE_1-7.cnv')
+
+if (!interactive()) png('922a.png')
+plot(d)
+if (!interactive()) dev.off()
