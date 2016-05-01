7a637c28071ed37e32c3a066086aa5139b943b7e
Dan Kelley <kelley.dan@gmail.com>
2016-05-01 12:37:26 -0300

    948

---
+++ 948/948a.R
@@ -0,0 +1,11 @@
+library(oce)
+## source("~/src/oce/R/ctd.R")
+## source("~/src/oce/R/odf.R")
+d1 <- read.odf(system.file("extdata", "CTD_BCD2014666_008_1_DN.ODF", package="oce")) 
+summary(d1)
+
+d2 <- read.ctd(system.file("extdata", "CTD_BCD2014666_008_1_DN.ODF", package="oce")) 
+summary(d2)
+
+d3 <- as.ctd(d1)
+summary(d3)
