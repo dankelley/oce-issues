c28a56e6278510a5453fa6183f3485991bc23137
Dan Kelley <kelley.dan@gmail.com>
2016-05-09 15:16:21 -0300

    projeciton

--- 966/966c.R
+++ 966/966c.R
@@ -1,11 +1,11 @@
-library(rgdal)
+library(proj4)
 lon0 <- -180                           # try both 0 and -180
 for (epsilon in seq(10, 0, -1)) {
-    message("epsilon: ", epsilon)
+    message("\nepsilon: ", epsilon)
     #xy <- rgdal::project(cbind(-180+epsilon,-90+epsilon), "+proj=wintri")
-    xy <- rgdal::project(cbind(lon0+epsilon,-90+epsilon), "+proj=wintri")
+    xy <- proj4::project(cbind(lon0+epsilon,-90+epsilon), "+proj=wintri")
     print(xy)
-    lonlat <- rgdal::project(xy, "+proj=wintri", inv=TRUE)
+    lonlat <- proj4::project(xy, "+proj=wintri", inv=TRUE)
     print(lonlat)
 }
 
