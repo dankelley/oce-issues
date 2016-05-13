0595148efe3d1757611da91afbd00dfb8c5d4758
Clark Richards <clark.richards@gmail.com>
2016-05-12 23:57:38 -0400

    956

--- 956/956a.R
+++ 956/956a.R
@@ -1,7 +1,8 @@
 rm(list=ls())
 library(oce)
+try(source('~/src/R-richards/oce/R/rsk.R'))
 
-d <- read.oce('065584_20140816_1633.rsk')
+d <- read.rsk('065584_20140816_1633.rsk')
 ctd <- as.ctd(d)
 
 str(d@metadata, 1)
