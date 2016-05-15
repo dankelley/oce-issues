136d37b056a78678bacc78cc5394c6f3095ae56b
Dan Kelley <kelley.dan@gmail.com>
2016-05-15 09:53:45 -0300

    976

---
+++ 976/976a.R
@@ -0,0 +1,17 @@
+library(oce)
+options(warn=1, width=120)
+rsks <- list.files("~/Dropbox/oce-working-notes/rsk", "*.rsk", full.name=TRUE)
+for (i in seq_along(rsks)) {
+    cat("\n\n")
+    cat(paste(rep("#", 100), collapse=""), "\n")
+    cat("# rsk[", i, "] = ", rsks[i], "\n", sep="")
+    rsk <- read.rsk(rsks[i])
+    cat("\n")
+    summary(rsk)
+    try({
+        ctd <- as.ctd(rsk)
+        cat("\n")
+        summary(ctd)
+    })
+}
+
