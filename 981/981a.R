b4d70f92b4763b14cdf2f434cbe65d2feec9a3c2
Dan Kelley <kelley.dan@gmail.com>
2016-06-08 19:29:54 -0300

    981

--- 981/981a.R
+++ 981/981a.R
@@ -4,7 +4,12 @@ ctd <- section[["station", 1]]
 
 if (!interactive()) png("981.png")
 
+par(mfrow=c(2,1))
 plotProfile(ctd, xtype="nitrite")
 mtext("Expect unit [umol/kg]", side=3, line=2, adj=1, col='magenta', font=2)
+
+ctd[["metadata"]]$units$nitrite <- list(unit=expression(nmol/g), scale="")
+plotProfile(ctd, xtype="nitrite")
+mtext("Expect unit [nmol/g]", side=3, line=2, adj=1, col='magenta', font=2)
 
 if (!interactive()) dev.off()
