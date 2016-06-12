2e1b3b9ca87319898ebd74cc404a21d8fcd6626e
Dan Kelley <kelley.dan@gmail.com>
2016-06-12 19:12:05 -0300

    981b.R

--- 981/981b.R
+++ 981/981b.R
@@ -1,6 +1,7 @@
 library(oce)
-try(source("~/src/oce/R/misc.R"))
+try(source("~/src/oce/R/section.R"))
 try(source("~/src/oce/R/ctd.R"))
+try(source("~/src/oce/R/misc.R"))
 
 data(section)
 
@@ -9,10 +10,12 @@ if (!interactive()) png("981b.png")
 par(mfrow=c(1,2))
 
 ## 1 umol/kg = 1 nmol/g
-unitFake <- list(unit=expression(nmol/g), scale="")
 
 plot(section, which="silicate")
-
+mtext("Note the unit ", side=3, line=0.5, adj=1, col='magenta', font=2, cex=2/3)
+for (i in seq_along(section@data$station)) {
+    section@data$station[[i]]@metadata$units$silicate <- list(unit=expression(nmol/g), scale="")
+}
 plot(section, which="silicate")
-mtext("Expect nmol/g ", side=3, line=-1, adj=1, col='magenta', font=2, cex=2/3)
+mtext("The unit should now be nmol/g ", side=3, line=0.5, adj=1, col='magenta', font=2, cex=2/3)
 if (!interactive()) dev.off()
