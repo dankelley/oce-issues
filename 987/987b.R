7902a5df00f3e6518c04acc1cbed936f04f1d889
Dan Kelley <kelley.dan@gmail.com>
2016-06-21 12:32:29 -0300

    dk twiddling

--- 987/987b.R
+++ 987/987b.R
@@ -1,3 +1,4 @@
+## detailed check on ctdFindProfiles; plots reveal if OK
 rm(list=ls())
 library(oce)
 library(testthat)
@@ -5,7 +6,7 @@ try(source('~/src/oce/R/ctd.R'))
 ## try(source('~/src/R-richards/oce/R/ctd.R'))
 
 data(ctd)
-N <- 200
+N <- 3
 ctd <- oceSetData(ctd, 'pressure', rep(c(ctd[['pressure']], rev(ctd[['pressure']])), N))
 ctd <- oceDeleteData(ctd, 'depth')
 ctd <- oceSetData(ctd, 'temperature', rep(c(ctd[['temperature']], rev(ctd[['temperature']])), N))
@@ -20,30 +21,25 @@ smoother <- function (x, n = 11, ...)
     xf <- as.numeric(stats::filter(x, f, ...))
 }
 
-if (FALSE){
+DEBUG <- 110 # if >100, ctdFindProfiles() plots debugging info (secret feature that may disappear)
+par(mfrow=c(2,1))
 pr <- ctdFindProfiles(ctd)
-npr <- length(pr)
-print('smoother = smooth.spline')
-print(paste0('Number of profiles found (of ', N, '): ', npr, " (expect disagreement with smooth.spline)"))
 
-pr <- ctdFindProfiles(ctd, smoother=smooth, cutoff=0.1)
+## If salinities are equal, probably everything else is, also.
 npr <- length(pr)
-print('smoother = smooth')
-print(paste0('Number of profiles found (of ', N, '): ', npr))
-expect_equal(N, npr)
+for (i in 2:npr) {
+  expect_equal(pr[[i-1]][["salinity"]], pr[[i]][["salinity"]])
 }
 
-pr <- ctdFindProfiles(ctd, smoother=smoother)
-npr <- length(pr)
-print('smoother = custom moving average')
-print(paste0('Number of profiles found (of ', N, '): ', npr))
-expect_equal(N, npr)
-
+pr <- ctdFindProfiles(ctd, direction='ascending')
 ## If salinities are equal, probably everything else is, also.
 ## NOTE: the very first profile differs slightly from the others (it has 2 more points) so
 ## NOTE: we do not insist that it be correct.
 if (npr > 1) {
-    for (i in 3:length(pr)) {
+    npr <- length(pr)
+    for (i in 3:npr) {
         expect_equal(pr[[i-1]][["salinity"]], pr[[i]][["salinity"]], info=paste("salinities in profiles", i-1, "and", i, "do not match"))
     }
 }
+
+
