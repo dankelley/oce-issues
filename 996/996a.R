c163fa1ef198daf02c0a6d7d74d72e8d296ce5bf
Dan Kelley <kelley.dan@gmail.com>
2016-06-28 17:41:15 -0300

    996

---
+++ 996/996a.R
@@ -0,0 +1,8 @@
+library(oce)
+try(source("~/src/oce/R/section.R"))
+data(section)
+if (!interactive()) png("996a.png")
+plot(section, which="temperature")
+plot(section, which="salinity", col='red', add=TRUE)
+if (!interactive()) dev.off()
+
