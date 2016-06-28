b5f1e471ad33b508f3c33b5d46924978c4da20b0
Dan Kelley <kelley.dan@gmail.com>
2016-06-28 18:17:53 -0300

    list

--- 1001/1001a.R
+++ 1001/1001a.R
@@ -2,14 +2,15 @@ library(oce)
 toread <- c("042.cnv","041.cnv","040.cnv")
 ctds <- vector("list", length(toread))
 for ( i in 1:length(toread))
-    ctds[i] <- ctdDecimate(ctdTrim(read.ctd.sbe(file.path(toread[i]))),p=1)
+    ctds[[i]] <- ctdDecimate(ctdTrim(read.ctd.sbe(file.path(toread[i]))),p=1)
+
 sec1 <- as.section(ctds)
 summary(sec1)
 if (!interactive())
     png("1001a.png", type="cairo")
 plot(sec1,
      which="temperature",
-     #which=c("temperature","salinity2","oxygen2","map"),
+     ##which=c("temperature","salinity2","oxygen2","map"),
      xtype='distance',ztype='image', showStations=TRUE)
 if (!interactive())
     dev.off()
