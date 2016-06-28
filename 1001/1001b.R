33e678b71bb052445de14fecd6ec604187b389f7
Dan Kelley <kelley.dan@gmail.com>
2016-06-28 18:36:29 -0300

    1001b.R

---
+++ 1001/1001b.R
@@ -0,0 +1,18 @@
+library(oce)
+toread <- c("042.cnv","041.cnv","040.cnv")
+ctds <- vector("list", length(toread))
+for ( i in 1:length(toread)) {
+    ctds[[i]] <- ctdDecimate(ctdTrim(read.ctd.sbe(file.path(toread[i]))),p=1)
+    ctds[[i]][["waterDepth"]] <- max(ctds[[i]][["pressure"]]) + 10 # FAKE a bottom
+}
+
+sec1 <- as.section(ctds)
+summary(sec1)
+if (!interactive())
+    png("1001b.png", type="cairo")
+plot(sec1,
+     which="temperature",
+     ##which=c("temperature","salinity2","oxygen2","map"),
+     xtype='distance',ztype='image', showStations=TRUE)
+if (!interactive())
+    dev.off()
