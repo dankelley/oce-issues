1bf8dfd52c73e440f82eb6f9a7686aa363de5ba5
Dan Kelley <kelley.dan@gmail.com>
2016-06-29 18:28:31 -0300

    1007

--- 1006/1006a.R
+++ 1006/1006a.R
@@ -1,4 +1,5 @@
 library(oce)
+source("~/src/oce/R/section.R")
 ## a <- read.oce("/Users/kelley/MEOP-CTD_2015-07-01/CANADA/ncARGO/ct55-9575-10_prof.nc")
 a <- read.oce("ct55-9575-10_prof.nc")
 s <- as.section(a)
---
+++ 1007/1007a.R
@@ -0,0 +1,15 @@
+library(oce)
+options(warn=3)
+try(source("~/src/oce/R/section.R"))
+data(section)
+if (!interactive()) png("1007a1.png")
+plot(section)
+if (!interactive()) dev.off()
+for(i in seq_along(section@data$station)) {
+    section@data$station[[i]]@data$salinity <- rep(NA,
+          length(section@data$station[[i]]@data$salinity))
+}
+if (!interactive()) png("1007a2.png")
+plot(section)
+if (!interactive()) dev.off()
+
