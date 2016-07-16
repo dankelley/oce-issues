bf8edee1173be4b45ddc8f39e03945204e462d11
Dan Kelley <kelley.dan@gmail.com>
2016-07-16 11:14:07 -0300

    1019

---
+++ 1019/1019a.R
@@ -0,0 +1,13 @@
+library(oce)
+try(source("~/src/oce/R/map.R"))
+data(coastlineWorld)
+if (!interactive()) png("1019a.png")
+par(mfrow=c(2,1), mar=c(2, 2, 1, 1))
+mapPlot(coastlineWorld, longitudelim=c(-130,-55), latitudelim=c(40,60),
+         projection="+proj=lcc +lat_0=30 +lat_1=60 +lon_0=-100", col='gray')
+mapPlot(coastlineWorld, longitudelim=c(-130,-55), latitudelim=c(40,60),
+         projection="+proj=lcc +lat_0=30 +lat_1=60 +lon_0=-100", col='gray',
+         cex.axis=1.4, mgp=c(0,0.7,0))
+
+if (!interactive()) dev.off()
+
