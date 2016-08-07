a75e0f15c4d18b14b2d193f31a6affc0f3884316
Dan Kelley <kelley.dan@gmail.com>
2016-08-07 10:06:26 -0300

    .

--- 1046/1046a.R
+++ 1046/1046a.R
@@ -4,7 +4,7 @@ imagep(volcano)
 x <- seq(0, 20, 0.5)
 y <- seq(0, 20, 0.5)
 z <- outer(x, y, function(x,y) cos(2*pi*x/20) * sin(2*pi*y/20))
-imagep(40+x, 20+y, z, breaks=seq(-1,1,0.01), col=oceColorsJet, debug=3, add=TRUE)
+imagep(40+x, 20+y, z, breaks=seq(-1,1,0.01), col=oceColorsJet, add=TRUE)
 abline(v=40) # check that it's in right place
 abline(h=20)
 if (!interactive()) dev.off()
