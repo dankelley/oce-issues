6d238946c49bcd02d0d1fd1182e04c344afd4877
Dan Kelley <kelley.dan@gmail.com>
2016-09-03 15:01:13 -0300

    .

---
+++ 1075/1075b.R
@@ -0,0 +1,16 @@
+library(oce)
+d1 <- read.oce("M39_5_phys_oce.tab.tsv") # in this github repository
+summary(d1)
+
+d2 <- read.oce("Schneider-etal_2015.tab.tsv") # a (large) private file
+summary(d2)
+
+d2ctd <- as.ctd(d2)
+profiles <- ctdFindProfiles(d2ctd, by="location")
+if (!interactive()) png("1075b_%02d.png")
+for (profile in profiles)
+    plot(profile)
+if (!interactive()) dev.off()
+
+
+
---
+++ 1075/README.md
@@ -0,0 +1,2 @@
+* M39_5_phys_oce.tab.tsv      https://doi.pangaea.de/10.1594/PANGAEA.836816?format=textfile
+* Schneider-etal_2015.tab.tsv https://doi.pangaea.de/10.1594/PANGAEA.854055?format=textfile
