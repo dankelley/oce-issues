b53b88d32b740e0998e311e9cf8fcbb446ccaaeb
Dan Kelley <kelley.dan@gmail.com>
2016-09-04 15:16:42 -0300

    .

--- 784/784a.R
+++ 784/784a.R
@@ -38,6 +38,7 @@ read.owhl <- function(file, tz="UTC")
 
     pushBack(header, file)
     data <- read.csv(file, skip=nheader, header=FALSE, col.names=names, stringsAsFactors=FALSE)
+    data <- as.list(data) # preferred in oce
     ## I am not sure what 'frac.seconds' means, e.g. does "25" mean 0.25s? I'm guessing that
     time <- numberAsPOSIXct(data$POSIXt, tz=tz) + data$frac.seconds / 100
     data$time <- time
@@ -58,6 +59,42 @@ read.owhl <- function(file, tz="UTC")
 d <- read.owhl("~/git/testing_OWHL/sample_data/15032600_halfday.CSV")
 summary(d)
 if (!interactive()) png("784a.png")
+par(mfrow=c(2,2), mar=c(3, 3, 2, 1), mgp=c(2, 0.7, 0))
 oce.plot.ts(d[["time"]], d[["pressure"]], ylab="Pressure [dbar]")
+## Chop out the descent phase; time determined by locator()
+dd <- subset(d, time>1427371658)
+t <- dd[["time"]]
+p <- dd[["pressure"]]
+oce.plot.ts(t, p, ylab="Pressure [dbar]")
+## low-pass to prewhiten spectrum; lp=lowpass; hp=highpass
+plp <- predict(smooth.spline(t, p, df=10)) 
+lines(plp$x, plp$y, col='red', lwd=3)
+php <- p - plp$y
+phpts <- ts(php, deltat=diff(as.numeric(t[1:2])))
+
+par(mar=c(3.5, 3.5, 2, 1), mgp=c(2, 0.7, 0))
+
+s <- spectrum(phpts, spans=c(31,11,5), plot=FALSE)
+plot(s$freq, s$spec, type='l', xlab="Frequency [Hz]", ylab="Power [dbar^2/Hz]")
+## plot(log10(s$freq), (s$spec), type='l', xlab="log10 Frequency", ylab="log10 Power")
+
+## Variance conserving plot with log(freq). Crude period
+## axis on bottom of plot.
+
+
+
+plot(log10(s$freq), s$spec*s$freq, type='l',
+     xlim=c(-1.5,-0.6), xlab="Frequency [Hz]", ylab=expression("Freq*Power [dbar^2]"), axes=FALSE)
+xaxp <- par("xaxp")
+l <- seq(xaxp[1], xaxp[2], length.out=xaxp[3]+1)
+axis(side=2)
+axis(1)
+period <- c(5, 10, 15, 20, 25)
+rug(log10(1/period), side=3, tick=-0.02, lwd=1)
+rug(log10(1/(1:100)), side=3, tick=-0.01, lwd=1)
+mtext(period, side=3, line=0.7, at=log10(1/period), cex=par('cex'))
+mtext("Period [s]", side=3, line=2)
+box()
+
 if (!interactive()) dev.off()
 
