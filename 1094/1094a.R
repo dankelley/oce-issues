bf2146240064d72b90fd06f0c632da0d494b2835
Dan Kelley <kelley.dan@gmail.com>
2016-10-01 10:05:08 -0300

    1094a.R makes png, not pdf (easier to put on GH)

--- 1094/1094a.R
+++ 1094/1094a.R
@@ -3,6 +3,7 @@ library(oce)
 
 start <- as.POSIXct('2016-09-28 00:00:00', tz='UTC')
 deltat <- c(5, 6, 11)
+if (!interactive()) png("1094a.png")
 par(mfrow=c(length(deltat), 1))
 for (dt in deltat) { # hours
     t <- seq(start, start + dt*3600, by=60)
@@ -10,3 +11,5 @@ for (dt in deltat) { # hours
     oce.plot.ts(t, yy)
     mtext(paste("dt:", dt, "hours"), side=3, line=0, adj=1)
 }
+if (!interactive()) dev.off()
+
