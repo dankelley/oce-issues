ba9d3b3ed5b4bbde122f26785cf24c88d30a6f2e
Dan Kelley <kelley.dan@gmail.com>
2016-12-28 11:31:22 -0400

    1128

---
+++ 1128/1128a.R
@@ -0,0 +1,24 @@
+library(oce)
+library(testthat)
+data(adp)
+adp[['orientation']] <- 'downward'
+
+if (!interactive()) png("1128a.png")
+
+par(mfrow=c(1, 2))
+
+plot(adp, which=24, ytype="profile")
+mtext("EXPECT: y axis from 40 to 10", col="magenta", adj=1)
+mtext(" ytype='profile'", line=-1.2, adj=0)
+usr1 <- par('usr')[3:4]
+
+plot(adp, which=24, ytype="distance")
+mtext("EXPECT: y axis from 10 to 40", col="magenta", adj=1)
+mtext(" ytype='distance'", line=-1.2, adj=0)
+usr2 <- par('usr')[3:4]
+options(digits=15)
+
+expect_equal(usr1, c(0.57, 45.39))
+expect_equal(usr2, rev(c(0.57, 45.39)))
+
+if (!interactive()) dev.off()
