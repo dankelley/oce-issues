47fb5d9bee038d6ff88aa09fffe22aca8026f83f
Dan Kelley <kelley.dan@gmail.com>
2016-12-28 13:13:09 -0400

    1128a.R

--- 11xx/1128/1128a.R
+++ 11xx/1128/1128a.R
@@ -1,23 +1,44 @@
 library(oce)
 library(testthat)
 data(adp)
-adp[['orientation']] <- 'downward'
+source("~/git/oce/R/adp.R")
 
 if (!interactive()) png("1128a.png")
 
-par(mfrow=c(1, 2))
-
-plot(adp, which=24, ytype="profile")
-mtext("EXPECT: y axis from 40 to 10", col="magenta", adj=1)
-mtext(" ytype='profile'", line=-1.2, adj=0)
-usr1 <- par('usr')[3:4]
+par(mfrow=c(2, 2))
 
 plot(adp, which=24, ytype="distance")
-mtext("EXPECT: y axis from 10 to 40", col="magenta", adj=1)
+mtext("EXPECT: y 10 to 40, fastest @ top", col="magenta", adj=1, cex=0.9)
 mtext(" ytype='distance'", line=-1.2, adj=0)
+mtext("data(adp), 'upward' ", side=1, line=-8, adj=1)
+usr1 <- par('usr')[3:4]
+
+plot(adp, which=24, ytype="profile")
+mtext("EXPECT: y 10 to 40, fastest @ top", col="magenta", adj=1, cex=0.9)
+mtext(" ytype='profile'", line=-1.2, adj=0)
+mtext("data(adp), 'upward' ", side=1, line=-8, adj=1)
 usr2 <- par('usr')[3:4]
 
-expect_equal(usr1, c(0.57, 45.39))
-expect_equal(usr2, rev(c(0.57, 45.39)))
+## Now change the dataset to get the other case
+adp[['orientation']] <- 'downward'
+
+plot(adp, which=24, ytype="distance")
+mtext("EXPECT: y 10 to 40, fastest @ top", col="magenta", adj=1, cex=0.9)
+mtext(" ytype='distance'", line=-1.2, adj=0)
+mtext("'downward' ", side=1, line=-8, adj=1)
+usr3 <- par('usr')[3:4]
+
+plot(adp, which=24, ytype="profile")
+mtext("EXPECT: y 10 to 40, fastest @ bottom", col="magenta", adj=1, cex=0.9)
+mtext(" ytype='profile'", line=-1.2, adj=0)
+mtext("'downward' ", side=1, line=-8, adj=1)
+usr4 <- par('usr')[3:4]
 
 if (!interactive()) dev.off()
+
+## put these tests at the end so we get the graph for debugging
+expect_equal(usr1, c(0.57, 45.39))
+expect_equal(usr2, c(0.57, 45.39))
+expect_equal(usr3, c(0.57, 45.39))
+expect_equal(usr4, rev(c(0.57, 45.39)))
+
