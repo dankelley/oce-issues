8870d1bf3622df852ba8d50656b6711178d3d00e
Dan Kelley <kelley.dan@gmail.com>
2016-12-30 16:22:04 -0400

    1161

---
+++ 11xx/1161/1161a.R
@@ -0,0 +1,23 @@
+library(oce)
+library(testthat)
+## source("~/git/oce/R/ctd.R")
+## source("~/git/oce/R/sw.R")
+
+d <- read.oce("CTD_98911_1P_1_DN.txt")
+
+if (!interactive()) png("1161a.png")
+par(mfrow=c(1,2))
+
+## 1. test access
+expect_equal(length(d[["theta"]]), 127)
+expect_equal(head(d[['theta']]), c(0.0346, 0.1563, 0.2153, 0.1970, 0.1916, 0.2141))
+plotProfile(d, xtype='theta')
+
+## 2. test assignment (fake signal)
+d[["theta"]] <- rep(10, length.out=length(d[["pressure"]]))
+expect_equal(length(d[["theta"]]), 127)
+expect_equal(head(d[['theta']]), rep(10, 6))
+plotProfile(d, xtype='theta')
+mtext("EXPECT: vertical line", col="magenta", font=2, line=-1)
+
+if (!interactive()) dev.off()
---
+++ 11xx/README.md
@@ -0,0 +1,6 @@
+Use 
+    make clean
+to clean all subdirectories, then
+    make
+to run all the tests. If anything fails, examine the code in the relevant subdirectory. Also, be sure to check all the graphs, using
+    open */*png
