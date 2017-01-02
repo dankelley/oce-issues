7b6f6e436404e8cb745c166abf71497a3df1c1a1
Dan Kelley <kelley.dan@gmail.com>
2017-01-02 10:31:52 -0400

    .

--- 10xx/1093/1093a.R
+++ 10xx/1093/1093a.R
@@ -5,7 +5,6 @@
 
 if (FALSE) {
     library(oce)
-    file <- "/data/archive/sleiwex/2008/moorings/m09/adp/rdi_2615/raw/adp_rdi_2615.000"
-    adp01 <- read.oce(file, from=10, to=20)
-    save(adp01, file="1093a.rdi")
+    adp01 <- read.oce("/data/archive/sleiwex/2008/moorings/m09/adp/rdi_2615/raw/adp_rdi_2615.000", from=10, to=20)
+    save(adp01, file="1093a.rda")
 } 
