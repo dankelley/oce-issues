9ad96cc631ab2e84731e245a0057495fb362767f
Dan Kelley <kelley.dan@gmail.com>
2017-01-08 10:17:01 -0400

    .

--- 10xx/1093/1093e.R
+++ 10xx/1093/1093e.R
@@ -2,7 +2,7 @@ library(oce)
 f <- "~/sentinelADCP/NLIW.pd0"
 from <- 200000                         # skip 200K profiles
 system.time({
-    d <- read.oce(f, from=from, to=from+200, testing=TRUE)
+    d <- read.oce(f, from=from, to=from+200, by=10, testing=TRUE)
 })
 summary(d)
 if (!interactive()) png("1093d.png")
