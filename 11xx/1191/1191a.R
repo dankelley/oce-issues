a57f345e07192ccb20e35b4aedb8352b69555f45
Dan Kelley <kelley.dan@gmail.com>
2017-02-06 13:42:34 -0400

    1191

---
+++ 11xx/1191/1191a.R
@@ -0,0 +1,23 @@
+library(oce)
+if (1 == length(list.files(path=".", pattern="^ct21-36-07_prof.nc$"))) {
+    d <- read.argo("~/Desktop/ct21-36-07_prof.nc")
+    ## learn what sort of flats we have
+    for(flag in 0:5) {
+        cat("flag", flag, "\n") 
+        for (name in names(d[["flags"]])) {
+            cat("  ", name, "has", sum(d[["flags"]][[name]]==flag),"flagged\n")
+        }
+    }
+    ## NOTE: the above reveals that the only flags in this file are 1 (good) and 4 (bad).
+    d1 <- handleFlags(d)               # 1=good 4=bad
+    d2 <- handleFlags(d, flags=list(all=c(0, 3:9)), action=list("NA"))
+    d3 <- handleFlags(d, flags=list(all=c(3:9)), action=list("NA"))
+    d4 <- handleFlags(d, flags=list(all=c(1,2)), action=list("NA")) # should differ from d1
+    summary(d)
+    summary(d1)
+    summary(d2)
+    summary(d3)
+    summary(d4)
+} else {
+    message("cannot run this test without a (private) test file\n")
+}
---
+++ 11xx/1191/README.md
@@ -0,0 +1,5 @@
+From the first part of the output we learn that the only flag values are 1 and
+4.
+
+Therefore I think summary(d) should be the same as summary(d3)
+
