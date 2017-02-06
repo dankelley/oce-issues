beb853293058249e4637c71f0ff7c4a8969a37df
Dan Kelley <kelley.dan@gmail.com>
2017-02-06 15:47:37 -0400

    .

--- 11xx/1191/1191a.R
+++ 11xx/1191/1191a.R
@@ -2,7 +2,7 @@ library(oce)
 if (1 == length(list.files(path=".", pattern="^ct21-36-07_prof.nc$"))) {
     d <- read.argo("~/Desktop/ct21-36-07_prof.nc")
     ## learn what sort of flats we have
-    for(flag in 0:5) {
+    for(flag in 0:4) {
         cat("flag", flag, "\n") 
         for (name in names(d[["flags"]])) {
             cat("  ", name, "has", sum(d[["flags"]][[name]]==flag, na.rm=TRUE),"flagged\n")
@@ -10,13 +10,21 @@ if (1 == length(list.files(path=".", pattern="^ct21-36-07_prof.nc$"))) {
     }
     ## NOTE: the above reveals that the only flags in this file are 1 (good) and 4 (bad).
     d1 <- handleFlags(d)               # 1=good 4=bad
-    d2 <- handleFlags(d, flags=list(all=c(0, 3:9)), action=list("NA"))
+    d2 <- handleFlags(d, flags=list(all=c(0, 3:9)), action=list("NA")) # expect same output as d1
     d3 <- handleFlags(d, flags=list(all=c(3:9)), action=list("NA"))
     d4 <- handleFlags(d, flags=list(all=c(1,2)), action=list("NA")) # should differ from d1
+    cat("d  (built-in data)\n")
     summary(d)
+    cat("d1 (default flag handling; expect different from d)\n")
     summary(d1)
+    cat("d2 (equivalent to default flag handling; expect same as d1)\n")
     summary(d2)
+    cat("d3 (equivalent to default flag handling; expect same as d1)\n")
     summary(d3)
+    cat("d4 (sets flag=1,2 to NA ... but we don't have such cases; expect same as d1)\n")
+    cat("here is why I say no such cases...\n")
+    cat("   flag=1 has", sum(1==d[["salinityFlag"]]), "cases\n")
+    cat("   flag=2 has", sum(2==d[["salinityFlag"]]), "cases\n")
     summary(d4)
 } else {
     message("cannot run this test without a (private) test file\n")
--- 11xx/1191/README.md
+++ 11xx/1191/README.md
@@ -1,5 +1,12 @@
-From the first part of the output we learn that the only flag values are 1 and
-4.
+The first part of the output of 1191a.R indicates that the only flag values are
+1 and 4. The argo convention is that 1=good and 4=bad, and it is that
+convention that is used in creating d1.
 
-Therefore I think summary(d) should be the same as summary(d3)
+Predicted output from the summary() calls:
+
+* d1: whatever it is
+* d2: identical to d1 (FAILS. WHY?)
+* d3: identical to d1
+* d4: good data set to NA, bad data retained
+
 
