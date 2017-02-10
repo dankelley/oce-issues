a53f8467ff5b729250f4b5d76b2aac8083bc4959
Dan Kelley <kelley.dan@gmail.com>
2017-02-10 18:58:58 -0400

    1191

--- 11xx/1191/1191a.R
+++ 11xx/1191/1191a.R
@@ -1,32 +1,40 @@
 library(oce)
 if (1 == length(list.files(path=".", pattern="^ct21-36-07_prof.nc$"))) {
     d <- read.argo("~/Desktop/ct21-36-07_prof.nc")
-    ## learn what sort of flats we have
-    for(flag in 0:4) {
-        cat("flag", flag, "\n") 
-        for (name in names(d[["flags"]])) {
-            cat("  ", name, "has", sum(d[["flags"]][[name]]==flag, na.rm=TRUE),"flagged\n")
+    ## learn about the flags in the data; I plan to make 'summary()' tell this
+    if (FALSE) { # ugly
+        for(flag in 0:9) {
+            cat("flag", flag, "\n") 
+            for (name in names(d[["flags"]])) {
+                cat("  ", name, "has", sum(d[["flags"]][[name]]==flag, na.rm=TRUE),"flagged\n")
+            }
         }
     }
-    ## NOTE: the above reveals that the only flags in this file are 1 (good) and 4 (bad).
+
+    for (item in c("salinity", "temperature", "pressure")) {
+        cat(item, "flags:")
+        print(table(d[[paste(item, "Flag", sep="")]]))
+    }
+
+    ## NOTE: the above reveals that the only salinity flags in this file are 1 (good) and 4 (bad).
     d1 <- handleFlags(d, debug=2)               # default: 1=good, 0=bad, 2:9=bad
     d2 <- handleFlags(d, flags=list(c(0, 3:9)), action=list("NA"), debug=2)
     d3 <- handleFlags(d, flags=list(c(3:9)), action=list("NA"), debug=2)
-    d4 <- handleFlags(d, flags=list(c(1,2)), action=list("NA"), debug=2)
+    d4 <- handleFlags(d, flags=list(salinity=c(1,2)), debug=2)
     cat("\n\n\n")
     cat("d: with no flag handling\n")
     summary(d)
     cat("\n\n\n")
-    cat("d1: Default flag handling, should be different to d. [result: OK]\n")
+    cat("d1: Default flag handling, should be different to d. Should be no S remaining [result: OK]\n")
     summary(d1)
     cat("\n\n\n")
-    cat("d2: should be same as d1. [result: ]\n")
+    cat("d2: should be same as d1. [result: OK]\n")
     summary(d2)
     cat("\n\n\n")
-    cat("d3: expect same as d2, since no flag=0 cases. [result: ]\n")
+    cat("d3: expect same as d2, since no flag=0 cases. [result: OK]\n")
     summary(d3)
     cat("\n\n\n")
-    cat("d4: expect same as d, since no flag=1,2 cases. [result: ]\n")
+    cat("d4: expect the same as d, since salinity has no flag=1,2 cases. [result: PASS]\n")
     cat("here is why I say no such cases...\n")
     cat("   flag=1 has", sum(1==d[["salinityFlag"]]), "cases\n")
     cat("   flag=2 has", sum(2==d[["salinityFlag"]]), "cases\n")
