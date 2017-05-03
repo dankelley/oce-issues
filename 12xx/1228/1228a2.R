417ead2ba5f44380388c5c3209ef7b4ede902bf5
Dan Kelley <kelley.dan@gmail.com>
2017-05-03 18:23:09 -0300

    1229 more tests

--- 12xx/1228/1228a2.R
+++ 12xx/1228/1228a2.R
@@ -7,14 +7,14 @@ options(digits.secs=4)
 
 m <- readMat("adcp.mat")
 cat("reading d1\n")
-d1 <- read.oce("adcp.000", from=1, to=29)
+d1 <- read.oce("adcp.000", from=1, to=9)
 cat("d1: diff(ensembleStart)= ", paste(diff(ldc$ensembleStart), collapse=" "), "\n")
 
 cat("reading d2\n")
-d2 <- read.oce("adcp.000", from=1, to=29, by=2)
+d2 <- read.oce("adcp.000", from=1, to=9, by=2)
 cat("d2: diff(ensembleStart)= ", paste(diff(ldc$ensembleStart), collapse=" "), "\n")
-expect_equal(d1[["time"]][seq.int(1, 29, 2)], d2[["time"]])
-expect_equal(d1[["pressure"]][seq.int(1, 29, 2)], d2[["pressure"]])
-expect_equal(d1[["v"]][seq.int(1, 29, 2),,], d2[["v"]])
+expect_equal(d1[["time"]][seq.int(1, 9, 2)], d2[["time"]])
+expect_equal(d1[["pressure"]][seq.int(1, 9, 2)], d2[["pressure"]])
+expect_equal(d1[["v"]][seq.int(1, 9, 2),,], d2[["v"]])
 
 
