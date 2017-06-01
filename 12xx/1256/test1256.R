eb5261ae7eff829996c97af7a7beca509d03c8fe
Dan Kelley <kelley.dan@gmail.com>
2017-06-01 10:18:11 -0300

    1256 graphs results

--- 12xx/1256/test1256.R
+++ 12xx/1256/test1256.R
@@ -4,6 +4,7 @@ data(section)
 stn <- section[["station", 3]]
 options(oceDeveloper=NULL)
 options(digits=10, width=100)
+par(mfrow=c(3,1), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 for (i in 1:3) {
     if (i == 1) options(oceDeveloper=NULL) else options(oceDeveloper=i-1)
     cat("LOOP i=", i, ", oceDeveloper=", getOption("oceDeveloper"), "\n")
@@ -32,6 +33,10 @@ for (i in 1:3) {
                  gsw_SA_from_SP(SP=S, p=p, longitude=longitude, latitude=latitude))
     expect_equal(CT, gsw_CT_from_t(SA=SA, t=T, p=p))
 
-    print(data.frame(S=S, T=T, p=p, SA=SA, CT=CT, st_gsw=AA, st_unesco=BB, diffPercent=200*(AA-BB)/(AA+BB)))
+    df <- data.frame(S=S, T=T, p=p, SA=SA, CT=CT, st_gsw=AA, st_unesco=BB, diffPercent=200*(AA-BB)/(AA+BB))
+    print(df)
+    plot(df$st_unesco, df$st_unesco - df$st_gsw, ylim=c(-0.006, -0.002), type='o')
+    mtext(paste("oceDeveloper=", getOption("oceDeveloper")), side=3, line=-1.4)
+    grid()
 }
 
