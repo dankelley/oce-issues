696beac59927f62314427b6f8c57aa831c6b6c7f
Dan Kelley <kelley.dan@gmail.com>
2017-07-23 11:04:07 -0300

    1274a

--- 12xx/1274/1274a.R
+++ 12xx/1274/1274a.R
@@ -1,21 +1,14 @@
 library(oce)
-options(digits=10, width=150)
 data(section)
 section <- handleFlags(section)
 dhu <- swDynamicHeight(section, eos="unesco")
-dhg <- swDynamicHeight(section, eos="g")
+dhg <- swDynamicHeight(section, eos="gsw")
 bad <- which(is.na(dhg$height))
 if (length(bad)) {
     cat("The second of the following three stations gives NA for dyn height. Why?\n")
     for (i in bad[1] + c(-1, 1)) {
         cat("\n\n", rep("=", 100), "\n\n", sep="")
         s <- section[["station", i]]
-        if (i == bad[1]) {
-            SA <- s[["SA"]]
-            CT <- s[["CT"]]
-            p <- s[["pressure"]]
-            save(SA, CT, p, file="broken_profile.rda") # used by 1232b.R
-        }
         ##summary(s)
         ##plot(s, span=5000)
         cat("s dyn height (gsw): ", swDynamicHeight(s, eos="gsw"), "\n")
@@ -28,10 +21,11 @@ if (length(bad)) {
     }
 }
 
+if (!interactive()) png("1274a.png")
 par(mfrow=c(2,1), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 plot(dhg$distance, dhg$height)
 points(dhu$distance,dhu$height,col=2)
-plot(dhg$distance, dhg$height, ylim=c(-5,5))
+plot(dhg$distance, dhg$height, ylim=c(-1, 3.5))
 points(dhu$distance,dhu$height,col=2,pch=20)
-
+if (!interactive()) dev.off()
 
