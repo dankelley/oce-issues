3cdb095fa15fa28220394ee4717c42a5419d9aa0
Dan Kelley <kelley.dan@gmail.com>
2017-08-14 16:26:00 -0300

    1281

--- 12xx/1282/1282a.R
+++ 12xx/1282/1282a.R
@@ -8,7 +8,6 @@ if (!interactive()) png("1282a_%d.png")
 ## test data: coarse PEI coastline
 peilon <- c(-63.6645, -62.9393, -62.01208, -62.50391, -62.87433,
             -64.1427999999999, -64.39261, -64.01486, -63.6645)
-
 peilat <- c(46.55001, 46.41587, 46.4431400000001, 46.03339, 45.96818,
             46.39265, 46.72747, 47.03601, 46.55001)
 peiFillable<- as.coastline(peilon, peilat, fillable=TRUE)
@@ -18,31 +17,39 @@ expect_false(peiUnfillable[["fillable"]])
 
 par(mfrow=c(2,2))
 
-## TEST 1: fillable data
-plot(peiFillable)
-mtext("expect: polygon", col="magenta", font=2, side=3, adj=1, line=0)
+clongitude <- -63.2
+clatitude <- 46.5
+span <- 350
 
-plot(peiFillable, type="l")
-mtext("expect: gray lines", col="magenta", font=2, side=3, adj=1, line=0)
+for (i in 1:2) {
+    projection <- if (i == 1) NULL else "+proj=aea +lon_0=-63"
 
-plot(peiFillable, type="p", col="red", pch=2)
-mtext("expect: red triangles", col="magenta", font=2, side=3, adj=1, line=0)
+    ## TEST 1: fillable data
+    plot(peiFillable, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: gray/black polygon", col="magenta", side=3, adj=1, line=0)
 
-plot(peiFillable, type="o", col="red",pch=20)
-mtext("expect: red lines and pch=20 points", col="magenta", side=3, adj=1, line=0)
+    plot(peiFillable, type="l", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: black lines", col="magenta", side=3, adj=1, line=0)
 
-## TEST 2: unfillable data
-plot(peiUnfillable, debug=4)
-mtext("expect: polygon", col="magenta", font=2, side=3, adj=1, line=0)
+    plot(peiFillable, type="p", col="red", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: red circles", col="magenta", side=3, adj=1, line=0)
 
-plot(peiUnfillable, type="l")
-mtext("expect: gray lines", col="magenta", font=2, side=3, adj=1, line=0)
+    plot(peiFillable, type="o", col="blue", pch=20, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: blue lines & dots", col="magenta", side=3, adj=1, line=0)
 
-plot(peiUnfillable, type="p")
-mtext("expect: red points", col="magenta", font=2, side=3, adj=1, line=0)
+    ## TEST 2: unfillable data
+    plot(peiUnfillable, debug=4, border="red", col="lightblue", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: blue/red polygon", col="magenta", side=3, adj=1, line=0)
 
-plot(peiUnfillable, type="o", col="red", pch=20)
-mtext("expect: red lines and pch=20 points", col="magenta", font=2, side=3, adj=1, line=0)
+    plot(peiUnfillable, type="l", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: lines w/ gap", col="magenta", side=3, adj=1, line=0)
+
+    plot(peiUnfillable, type="p", col="red", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: red circles", col="magenta", side=3, adj=1, line=0)
+
+    plot(peiUnfillable, type="o", col="blue", pch=20, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
+    mtext("expect: blue lines w/ gap & dots", col="magenta", side=3, adj=1, line=0)
+}
 
 if (!interactive()) dev.off()
 
