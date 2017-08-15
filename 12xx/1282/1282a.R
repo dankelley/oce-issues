d27e53ff45bbc23de0dde4963074222ed500dd2f
Dan Kelley <kelley.dan@gmail.com>
2017-08-15 10:11:27 -0300

    1282 test improvements

--- 12xx/1282/1282a.R
+++ 12xx/1282/1282a.R
@@ -1,7 +1,10 @@
 library(oce)
 source("~/git/oce/R/coastline.R")
+source("~/git/oce/R/map.R")
 library(testthat)
 data(coastlineWorld)
+
+options(oceDebug=3)
 
 if (!interactive()) png("1282a_%d.png")
 
@@ -25,30 +28,48 @@ for (i in 1:2) {
     projection <- if (i == 1) NULL else "+proj=aea +lon_0=-63"
 
     ## TEST 1: fillable data
+    lab <- paste("fillable", if(i==1) "/linear " else "/map ", sep="")
     plot(peiFillable, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: gray/black polygon", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='polygon' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiFillable, type="l", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: black lines", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='l' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiFillable, type="p", col="red", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: red circles", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='p' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiFillable, type="o", col="blue", pch=20, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
-    mtext("expect: blue lines & dots", col="magenta", side=3, adj=1, line=0)
+    mtext("expect: blue circles+lines", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='o' ", col=2, side=3, line=-2, adj=1)
 
     ## TEST 2: unfillable data
+    lab <- paste("unfillable", if(i==1) "/linear " else "/map ", sep="")
     plot(peiUnfillable, debug=4, border="red", col="lightblue", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: blue/red polygon", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='polygon' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiUnfillable, type="l", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: lines w/ gap", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='l' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiUnfillable, type="p", col="red", projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: red circles", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='p' ", col=2, side=3, line=-2, adj=1)
 
     plot(peiUnfillable, type="o", col="blue", pch=20, projection=projection, clatitude=clatitude, clongitude=clongitude, span=span)
     mtext("expect: blue lines w/ gap & dots", col="magenta", side=3, adj=1, line=0)
+    mtext(lab, col=2, side=3, line=-1, adj=1)
+    mtext("type='o' ", col=2, side=3, line=-2, adj=1)
 }
 
 if (!interactive()) dev.off()
