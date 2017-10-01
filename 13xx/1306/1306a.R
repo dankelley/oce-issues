54be1c8d0cd71ed1c1dedcff448eadf0d056ac8f
dankelley <kelley.dan@gmail.com>
2017-10-01 11:06:20 -0300

    1306

---
+++ 13xx/1306/1306a.R
@@ -0,0 +1,9 @@
+library(oce)
+data(coastlineWorld)
+if (!interactive()) png("1306a.png", width=4, height=7, unit="in", res=150, pointsize=10)
+par(mar=c(3, 3, 1, 1))
+mapPlot(coastlineCut(coastlineWorld, -50), debug=5,
+         longitudelim=c(-100,5), latitudelim=c(35, 120),
+         projection="+proj=lcc +lat_0=60 +lat_1=80 +lon_0=-50", col='gray')
+if (!interactive()) dev.off()
+
