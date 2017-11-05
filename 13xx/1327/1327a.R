62f2b28d87f8bffcde44ce159b3bae0c45bc832c
dankelley <kelley.dan@gmail.com>
2017-11-05 07:43:11 -0400

    1327

---
+++ 13xx/1327/1327a.R
@@ -0,0 +1,29 @@
+library(oce)
+header <- function(msg) {
+    message(rep("=", 50), sep="")
+    message(msg)
+    message(rep("=", 50), sep="")
+}
+
+header("Case 1")
+u2 <- "ftp://ftp.ifremer.fr/ifremer/argo/dac/bodc/6900388/6900388_prof.nc"
+f2 <-  gsub("^.*/(.*)$", "\\1", u2)
+if (0 == length(list.files(pattern=f2)))
+    download.file(u2, f2)
+a2 <- read.oce(f2)
+str(a2[['data']])
+plot(a2, which=4)
+a2s <- subset(argoGrid(a2), pressure > 500 & pressure < 1000)
+str(a2s[['data']])
+
+header("Case 1")
+u1 <- "http://www.usgodae.org/ftp/outgoing/argo/dac/meds/4901783/4901783_prof.nc"
+f1 <-  gsub("^.*/(.*)$", "\\1", u1)
+if (0 == length(list.files(pattern=f1)))
+    download.file(u1, f1)
+a1 <- read.oce(f1)
+str(a1[['data']])
+plot(a1, which=4)
+as1s <- subset(argoGrid(a1), pressure > 500 & pressure < 1000)
+str(a1s[['data']])
+
