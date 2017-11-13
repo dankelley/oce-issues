9d107839d6f29896b29e2b9b72ad4357bf0827c6
dankelley <kelley.dan@gmail.com>
2017-11-13 19:51:47 -0400

    1320 fixed on both aeqd and aitoff projections

--- 13xx/1320/1320a.R
+++ 13xx/1320/1320a.R
@@ -1,10 +1,32 @@
 library(oce)
 data(coastlineWorld)
-p <- "+proj=aitoff +lon_0=-45"
+coastlineWorld <- coastlineCut(coastlineWorld, -45)
+p <- "+proj=aeqd +lon_0=-45"
 if (!interactive()) png("1320a.png")
 par(mar=c(2, 2, 1, 1))
-par(xpd=NA)
+## par(xpd=NA)
 mapPlot(coastlineWorld, projection=p, col="lightgray",
         longitudelim=c(-80,0), latitudelim=c(0,79))
+
+## EXPERIMENT looking at a point that is cut off (for some plot
+## geometries). "i" is just a point I found by clicking with locator(),
+## and imin:imax is the coastline sequence holding this point.
+lon <- coastlineWorld[["longitude"]]
+lat <- coastlineWorld[["latitude"]]
+xy <- rgdal::project(cbind(lon, lat), proj=p)
+i <- which.min((xy[,1]-4081795)*(xy[,1]-4081795) + (xy[,2]-9461998)*(xy[,2]-9461998))
+na <- which(is.na(lon))
+imin <-  na[max(which(na<i))]
+imax <- na[min(which(na>i))]
+##message("i=", i, ", imin=", imin, ", imax=", imax)
+ilook <- imin:imax
+if (2 != sum(is.na(lon[ilook])))
+    stop("ilook is messed up")
+lines(xy[ilook, 1], xy[ilook, 2], col=2)
+points(xy[ilook, 1], xy[ilook, 2], col=2, pch=20, cex=1/2)
+
+
+
+
 if (!interactive()) dev.off()
 
