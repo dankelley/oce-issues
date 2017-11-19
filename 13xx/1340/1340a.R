47110482cca294d5d99803bec0f3b05cad00e1c7
dankelley <kelley.dan@gmail.com>
2017-11-19 16:22:25 -0400

    fiddling with 1340

--- 13xx/1340/1340a.R
+++ 13xx/1340/1340a.R
@@ -1,5 +1,5 @@
 library(oce)
-library(gsw)
+## source("~/git/oce/R/map.R")
 data(coastlineWorld)
 
 ## generate a vector for a grid of points on the Earth
@@ -12,17 +12,24 @@ SA <- array(gsw_SA_from_SP(rep(35, length(lon)),
                            rep(100, length(lon)), lon, lat),
             dim=c(length(lo), length(la)))
 
-if (!interactive()) png('1340a.png', height=300, type='cairo')
+if (!interactive()) png('1340a_%d.png', width=5.5, height=3, unit="in", res=150, type='cairo')
 
-par(mar=rep(1, 4), mfrow=c(1, 2))
+par(mar=rep(1, 4))
+p <- "+proj=moll"
+p <- "+proj=lonlat"
 
-mapPlot(coastlineWorld, projection="+proj=moll")
-mapImage(lo, la, SA, missingColor=NA)
-mapLines(coastlineWorld, col='red')
+mapPlot(coastlineWorld, projection=p)
+mapImage(lo, la, SA, missingColor=NA, debug=0)
+mapLines(coastlineWorld, col='gray')
 
-mapPlot(coastlineWorld, projection="+proj=moll")
-mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet)
-mapLines(coastlineWorld, col='red')
+mapPlot(coastlineWorld, projection=p)
+mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet, debug=0) # bad
+mapLines(coastlineWorld, col='gray')
+
+## does it help to specify breaks?
+mapPlot(coastlineWorld, projection=p)
+mapImage(lo, la, SA, missingColor=NA, breaks=128/4, col=oceColorsJet, debug=0) # ok
+mapLines(coastlineWorld, col='gray')
 
 
 if (!interactive()) dev.off()
