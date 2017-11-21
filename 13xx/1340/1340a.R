53b424fea8d18148692add7153dda82e8bf7ff4b
dankelley <kelley.dan@gmail.com>
2017-11-21 18:38:31 -0400

    1340 more work (now see problem)

--- 13xx/1340/1340a.R
+++ 13xx/1340/1340a.R
@@ -15,6 +15,8 @@ SA <- array(gsw_SA_from_SP(rep(35, length(lon)),
 if (!interactive()) png('1340a_%d.png', width=5.5, height=3, unit="in", res=150, type='cairo')
 
 par(mar=rep(1, 4))
+par(mfrow=c(1, 1))
+
 p <- "+proj=moll"
 p <- "+proj=lonlat"
 
@@ -25,22 +27,30 @@ mapLines(coastlineWorld, col='gray')
 par(mfrow=c(2, 1))
 mapPlot(coastlineWorld, projection=p)
 
-options("dan"=2)
+options("dan"=1)
 mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet, debug=0) # bad
-dan2 <- dan
+if (exists("dan")) dan1 <- dan
 mapLines(coastlineWorld, col='gray')
 
-options("dan"=1)
+options("dan"=2)
 mapPlot(coastlineWorld, projection=p)
 mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet, debug=0) # bad
-dan1 <- dan
+if (exists("dan")) dan2 <- dan
 mapLines(coastlineWorld, col='gray')
 par(mfrow=c(1, 1))
 
+stopifnot(all.equal(dan1$xy, dan2$xy))
+stopifnot(all.equal(dan1$r, dan2$r))
+stopifnot(all.equal(dan1$longitude, dan2$longitude))
+stopifnot(all.equal(dan1$z, dan2$z))
+stopifnot(all.equal(dan1$col, dan2$col))
+
+if (FALSE) {
 ## does it help to specify breaks?
 mapPlot(coastlineWorld, projection=p)
 mapImage(lo, la, SA, missingColor=NA, breaks=128/4, col=oceColorsJet, debug=0) # ok
 mapLines(coastlineWorld, col='gray')
+}
 
 
 if (!interactive()) dev.off()
