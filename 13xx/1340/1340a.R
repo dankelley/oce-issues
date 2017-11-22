e277dde06050c019df20dab118c87eeaa20c8505
dankelley <kelley.dan@gmail.com>
2017-11-22 13:01:49 -0400

    1340

--- 13xx/1340/1340a.R
+++ 13xx/1340/1340a.R
@@ -1,5 +1,4 @@
 library(oce)
-## source("~/git/oce/R/map.R")
 data(coastlineWorld)
 
 ## generate a vector for a grid of points on the Earth
@@ -14,43 +13,41 @@ SA <- array(gsw_SA_from_SP(rep(35, length(lon)),
 
 if (!interactive()) png('1340a_%d.png', width=5.5, height=3, unit="in", res=150, type='cairo')
 
-par(mar=rep(1, 4))
-par(mfrow=c(1, 1))
+par(mar=c(2, 2, 4, 1))
 
-p <- "+proj=moll"
-p <- "+proj=lonlat"
 
-mapPlot(coastlineWorld, projection=p)
-mapImage(lo, la, SA, missingColor=NA, debug=0)
-mapLines(coastlineWorld, col='gray')
+for (mc in c(NA, "lightgray")) {
+    for (proj in c("+proj=moll", "+proj=lonlat")) {
 
-par(mfrow=c(2, 1))
-mapPlot(coastlineWorld, projection=p)
+        mapPlot(coastlineWorld, projection=proj)
+        mapImage(lo, la, SA, missingColor=mc)
+        mtext("(lo, la, SA, missingColor=mc)", side=3)
+        mapLines(coastlineWorld, col='gray')
+        mtext(proj, side=3, line=2)
+        mtext(paste("mc=", mc), side=3, line=3)
 
-options("dan"=1)
-mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet, debug=0) # bad
-if (exists("dan")) dan1 <- dan
-mapLines(coastlineWorld, col='gray')
+        mapPlot(coastlineWorld, projection=proj)
+        mapImage(lo, la, SA, missingColor=mc, col=oceColorsJet)
+        mtext("(lo, la, SA, missingColor=mc, col=oceColorsJet)", side=3)
+        mapLines(coastlineWorld, col='gray')
+        mtext(proj, side=3, line=2)
+        mtext(paste("mc=", mc), side=3, line=3)
 
-options("dan"=2)
-mapPlot(coastlineWorld, projection=p)
-mapImage(lo, la, SA, missingColor=NA, col=oceColorsJet, debug=0) # bad
-if (exists("dan")) dan2 <- dan
-mapLines(coastlineWorld, col='gray')
-par(mfrow=c(1, 1))
+        mapPlot(coastlineWorld, projection=proj)
+        mapImage(lo, la, SA, missingColor=mc, col=oceColorsJet)
+        mtext("(lo, la, SA, missingColor=mc, col=oceColorsJet)", side=3)
+        mapLines(coastlineWorld, col='gray')
+        mtext(proj, side=3, line=2)
+        mtext(paste("mc=", mc), side=3, line=3)
 
-stopifnot(all.equal(dan1$xy, dan2$xy))
-stopifnot(all.equal(dan1$r, dan2$r))
-stopifnot(all.equal(dan1$longitude, dan2$longitude))
-stopifnot(all.equal(dan1$z, dan2$z))
-stopifnot(all.equal(dan1$col, dan2$col))
+        mapPlot(coastlineWorld, projection=proj)
+        mapImage(lo, la, SA, missingColor=mc, breaks=128, col=oceColorsJet)
+        mtext("(lo, la, SA, missingColor=mc, breaks=128, col=oceColorsJet)", side=3)
+        mapLines(coastlineWorld, col='gray')
+        mtext(proj, side=3, line=2)
+        mtext(paste("mc=", mc), side=3, line=3)
 
-if (FALSE) {
-## does it help to specify breaks?
-mapPlot(coastlineWorld, projection=p)
-mapImage(lo, la, SA, missingColor=NA, breaks=128/4, col=oceColorsJet, debug=0) # ok
-mapLines(coastlineWorld, col='gray')
+    }
 }
-
 
 if (!interactive()) dev.off()
