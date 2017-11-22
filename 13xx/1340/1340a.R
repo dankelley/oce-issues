10b082c5c61b64bcba5087c63df9b1d90cdf8ce3
dankelley <kelley.dan@gmail.com>
2017-11-22 18:23:31 -0400

    1340

--- 13xx/1340/1340a.R
+++ 13xx/1340/1340a.R
@@ -11,6 +11,8 @@ SA <- array(gsw_SA_from_SP(rep(35, length(lon)),
                            rep(100, length(lon)), lon, lat),
             dim=c(length(lo), length(la)))
 
+SA[100:150, 100:150] <- NA
+
 if (!interactive()) png('1340a_%d.png', width=5.5, height=3, unit="in", res=150, type='cairo')
 
 par(mar=c(2, 2, 4, 1))
@@ -18,35 +20,27 @@ par(mar=c(2, 2, 4, 1))
 
 for (mc in c(NA, "lightgray")) {
     for (proj in c("+proj=moll", "+proj=lonlat")) {
+        lab <- paste("missingColor=", mc, ", projection=\"", proj, "\"", sep="")
 
         mapPlot(coastlineWorld, projection=proj)
         mapImage(lo, la, SA, missingColor=mc)
-        mtext("(lo, la, SA, missingColor=mc)", side=3)
         mapLines(coastlineWorld, col='gray')
-        mtext(proj, side=3, line=2)
-        mtext(paste("mc=", mc), side=3, line=3)
+        mtext(lab, side=3)
 
         mapPlot(coastlineWorld, projection=proj)
         mapImage(lo, la, SA, missingColor=mc, col=oceColorsJet)
-        mtext("(lo, la, SA, missingColor=mc, col=oceColorsJet)", side=3)
         mapLines(coastlineWorld, col='gray')
-        mtext(proj, side=3, line=2)
-        mtext(paste("mc=", mc), side=3, line=3)
+        mtext(lab, side=3)
 
         mapPlot(coastlineWorld, projection=proj)
         mapImage(lo, la, SA, missingColor=mc, col=oceColorsJet)
-        mtext("(lo, la, SA, missingColor=mc, col=oceColorsJet)", side=3)
         mapLines(coastlineWorld, col='gray')
-        mtext(proj, side=3, line=2)
-        mtext(paste("mc=", mc), side=3, line=3)
+        mtext(lab, side=3)
 
         mapPlot(coastlineWorld, projection=proj)
         mapImage(lo, la, SA, missingColor=mc, breaks=128, col=oceColorsJet)
-        mtext("(lo, la, SA, missingColor=mc, breaks=128, col=oceColorsJet)", side=3)
         mapLines(coastlineWorld, col='gray')
-        mtext(proj, side=3, line=2)
-        mtext(paste("mc=", mc), side=3, line=3)
-
+        mtext(lab, side=3)
     }
 }
 
