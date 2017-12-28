a5f764e2116efb1e883cb0b96d1b0b94673af6fb
dankelley <kelley.dan@gmail.com>
2017-12-28 14:46:26 -0400

    1351a.R added

--- 13xx/1351/1351a.R
+++ 13xx/1351/1351a.R
@@ -1,3 +1,17 @@
-ttide <- read.table("ttide.dat", header=TRUE, skip=6, stringsAsFactors=FALSE)
-ttide
+library(testthat)
+
+ttide <- read.table("ttide.dat", header=TRUE, skip=8, stringsAsFactors=FALSE)
+## t_demo uses
+## infername=['P1';'K2'];
+## inferfrom=['K1';'S2'];
+## infamp=[.33093;.27215];
+## infphase=[-7.07;-22.40]
+
+## Check amplitudes (using the infamp field)
+expect_equal(ttide[ttide$Tide=="P1", "Amp"], .33093*ttide[ttide$Tide=="K1", "Amp"], tol=1e-5) # amp P1 from K1
+expect_equal(ttide[ttide$Tide=="K2", "Amp"], .27215*ttide[ttide$Tide=="S2", "Amp"], tol=1e-5) # amp P1 from K1
+## Check phases (using the infphase field)
+expect_equal(ttide[ttide$Tide=="P1", "Pha"], ttide[ttide$Tide=="K1", "Pha"] - (-7.07), tol=1e-5) # amp P1 from K1
+expect_equal(ttide[ttide$Tide=="K2", "Pha"], ttide[ttide$Tide=="S2", "Pha"] - (-22.40), tol=1e-5) # amp P1 from K1
+
 
