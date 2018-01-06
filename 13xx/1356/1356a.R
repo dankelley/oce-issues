96a713e9d55e6828e275838f4a142587189537e1
dankelley <kelley.dan@gmail.com>
2018-01-06 16:52:44 -0400

    1356

--- 13xx/1356/1356a.R
+++ 13xx/1356/1356a.R
@@ -1,20 +1,45 @@
-rm(list=ls())
+## rm(list=ls())
 library(oce)
-source("~/git/oce/R/tides.R")
+
+## below from t_demo.m
+## >> datestr(tuk_time(1))
+##
+## ans =
+##
+##     '06-Jul-1975 01:00:00'
 
 foreman <- read.table("../1351/foreman.dat", header=TRUE, stringsAsFactors=FALSE)
+ttide <- read.table("../1351/ttide.dat", header=TRUE, stringsAsFactors=FALSE, skip=8)
 
 data(sealevelTuktoyaktuk)
+
+## Next block is fiddling around with something I just realized on 2017-01-06,
+## namely that T_TIDE doesn't use the central time, but rather such a time
+## rounded down to the nearest hour. That explained some problems in phase matching.
+t <- sealevelTuktoyaktuk[["time"]]
+expect_equal(t[1], as.POSIXct("1975-07-06 01:00:00", tz="UTC"))
+expect_equal(t[2], as.POSIXct("1975-07-06 02:00:00", tz="UTC"))
+tn <- as.numeric(t)
+tnmean <- mean(tn)
+tnmean
+numberAsPOSIXct(tnmean, tz="UTC")
+## get centre time, rounded to be on an hour mark
+tnmean0 <- 3600 * round(tnmean / 3600)
+numberAsPOSIXct(tnmean0, tz="UTC")
+head((tn - tnmean) / 3600)
+head((tn - tnmean0) / 3600)
+tt <- tn - tnmean0
+expect_equal(tt/3600, -791:792)
+
+par(mar=rep(0, 4), mgp=c(2, 0.7, 0))
 m <- tidem(sealevelTuktoyaktuk,
            constituents=c("standard", "M10"),
-           greenwich=TRUE,
            infer=list(name=c("P1", "K2"), # 0.0415525871 0.0835614924
                       from=c("K1", "S2"), # 0.0417807462 0.0833333333
                       amp=c(0.33093, 0.27215),
-                      phase=c(-7.07, -22.40)), debug=0)
+                      phase=c(-7.07, -22.40)))
 
 if (!interactive()) png("1356a.png", height=5, width=5, unit="in", res=100, pointsize=10)
-par(mar=c(2, 2, 2, 2), mgp=c(2, 0.7, 0))
 rpd <- pi / 180
 Soce <- sin(m[["phase"]] * rpd)
 Coce <- cos(m[["phase"]] * rpd)
@@ -29,7 +54,7 @@ for (deg in seq(0, 360, 5)) {
 for (deg in seq(0, 360, 15)) {
     text(0.75*cos(deg*rpd), 0.75*sin(deg*rpd), deg, col="gray")
 }
-look <- 1:9
+look <- seq_along(m[["name"]])
 delta <- 0.02
 Delta <- 5 * delta
 points((1+delta)*Cforeman[look], (1+delta)*Sforeman[look], pch=20)
@@ -37,5 +62,27 @@ text((1+Delta)*Cforeman[look], (1+Delta)*Sforeman[look], look)
 points((1-delta)*Coce[look], (1-delta)*Soce[look], col=2, pch=20)
 text((1-Delta)*Coce[look], (1-Delta)*Soce[look], look, col=2)
 legend("topright", col=1:2, pch=20, legend=c("Foreman", "tidem"))
+
+
 if (!interactive()) dev.off()
 
+cat("Phase comparison:\n")
+print(data.frame(name=m[["name"]],
+                 phaseTidem=sprintf("%.2f", m[["phase"]]),
+                 phaseTTIDE=sprintf("%.2f", ttide$phase),
+                 phaseForeman=sprintf("%.2f", foreman$G),
+                 phaseErrForeman=sprintf("%.2f", m[["phase"]] - foreman$G),
+                 phaseErrTTIDE=sprintf("%.2f", m[["phase"]] - ttide$phase)))
+
+cat("Amplitude comparison:\n")
+print(data.frame(name=m[["name"]],
+                 ampTidem=sprintf("%.4f", m[["amplitude"]]),
+                 ampTTIDE=sprintf("%.4f", ttide$amplitude),
+                 ampForeman=sprintf("%.4f", foreman$A),
+                 ampErrForeman=sprintf("%.4f", m[["amplitude"]] - foreman$A),
+                 ampErrTTIDE=sprintf("%.4f", m[["amplitude"]] - ttide$amplitude)))
+
+max(abs(m[["amplitude"]] - foreman$A))
+max(abs(m[["amplitude"]] - ttide$amplitude))
+max(abs(m[["phase"]] - foreman$G))
+max(abs(m[["phase"]] - ttide$phase))
