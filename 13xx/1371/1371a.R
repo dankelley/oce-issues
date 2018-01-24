b26a90e8f7132dbadc3de3444b3a84bb14bf0af2
dankelley <kelley.dan@gmail.com>
2018-01-24 14:29:23 -0400

    1371a.R: use 1 page per plot (easier to read labels)

--- 13xx/1371/1371a.R
+++ 13xx/1371/1371a.R
@@ -1,18 +1,19 @@
 library(oce)
 data(ctd)
+set.seed(1371) # for reproducibility
 ctd <- oceSetData(ctd, 'oxygen', rnorm(ctd[['pressure']]), unit ='none')
 ctd <- oceSetData(ctd, 'stupid', rnorm(ctd[['pressure']]), unit ='none')
 
 plim <- c(100,0)
 xlim <- c(0,3)
-if (!interactive()) png("1371a.png")
+if (!interactive()) png("1371a_%02d.png")
 
-par(mfrow = c(2,6))
 plotProfile(ctd, xtype = 'stupid')
-mtext(side = 1, text = 'no limits', line = 1)
+mtext(side = 1, text = 'EXPECT: 0<p<46; -2.9<x<3.4 {OK}', line = 0, col='magenta')
 
 plotProfile(ctd, xtype = 'stupid', plim = plim)
-mtext(side = 1, text = 'plim', line = 1)
+mtext(side = 1, text = 'EXPECT: 0<p<46; -2.9<x<3.4 {FAIL}', line = 0, col='magenta')
+
 plotProfile(ctd, xtype = 'stupid', ylim = plim)
 mtext(side = 1, text = 'ylim', line = 1)
 
@@ -32,7 +33,7 @@ plotProfile(ctd, xtype = 'oxygen', ylim = plim)
 plotProfile(ctd, xtype = 'oxygen', xlim = xlim)
 
 plotProfile(ctd, xtype = 'oxygen', plim = plim, xlim = xlim)
-plotProfile(ctd, xtype = 'oxygen', ylim = plim, xlim = xlim)
+##plotProfile(ctd, xtype = 'oxygen', ylim = plim, xlim = xlim, debug=3) # dies
 
 if (!interactive()) dev.off()
 
