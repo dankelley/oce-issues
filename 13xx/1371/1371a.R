3dc22e11372f45af01d94402a7ad630fe5658674
dankelley <kelley.dan@gmail.com>
2018-01-24 18:58:07 -0400

    1371 improved

--- 13xx/1371/1371a.R
+++ 13xx/1371/1371a.R
@@ -7,7 +7,6 @@ library(oce)
 ## more clearly whether x autoscales well, if y is narrowed.
 
 data(ctd)
-
 ctd <- subset(ctd, pressure < 20)
 p <- ctd[["pressure"]]
 ctd <- oceSetData(ctd, 'oxygen', p, unit ='none')
@@ -15,7 +14,8 @@ ctd <- oceSetData(ctd, 'fake', -p, unit ='none')
 
 plim <- c(100, 0)
 xlim <- c(-15, -10)
-#if (!interactive()) png("1371a_%02d.png")
+
+if (!interactive()) png("1371a_%02d.png")
 
 plotProfile(ctd, xtype='fake')
 mtext(side=1, text="Test 1", line=0, col='magenta')
@@ -49,17 +49,17 @@ plotProfile(ctd, xtype='oxygen', ylim=plim)
 mtext(side=1, text="Test 8", line=0, col='magenta')
 expect_equal(par("usr"), c(0.74796, 20.51304, 104, -4))
 
-plotProfile(ctd, xtype='oxygen', xlim=xlim)
+plotProfile(ctd, xtype='oxygen', xlim=-rev(xlim))
 mtext(side=1, text="Test 9", line=0, col='magenta')
-expect_equal(par("usr"), c(-15.2, -9.8, 20.51304, 0.74796))
+expect_equal(par("usr"), c(9.8, 15.2, 20.51304, 0.74796))
 
-plotProfile(ctd, xtype='oxygen', plim=plim, xlim=xlim)
+plotProfile(ctd, xtype='oxygen', plim=plim, xlim=-rev(xlim))
 mtext(side=1, text="Test 10", line=0, col='magenta')
-expect_equal(par("usr"), c(-15.2, -9.8, 104, -4))
+expect_equal(par("usr"), c(9.8, 15.2, 104, -4))
 
-plotProfile(ctd, xtype='oxygen', ylim=plim, xlim=xlim)
+plotProfile(ctd, xtype='oxygen', ylim=plim, xlim=-rev(xlim))
 mtext(side=1, text="Test 11", line=0, col='magenta')
-expect_equal(par("usr"), c(-15.2, -9.8, 104, -4))
+expect_equal(par("usr"), c(9.8, 15.2, 104, -4))
 
 ## Salinity has Slim. See how this interacts with xlim.
 plotProfile(ctd, xtype='salinity', ylim=plim)
@@ -77,8 +77,5 @@ expect_equal(par("usr"), c(29.99855641, 31.61705416, 104, -4))
 message("FIXME: check that narrowing the y limit (or plim etc) also narrows the
         range on the x axis, as appropriate.")
 
-
-
-
-#if (!interactive()) dev.off()
+if (!interactive()) dev.off()
 
