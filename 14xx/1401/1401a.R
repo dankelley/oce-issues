d71cdcc036efc1edae93c9fba58e933d4567d94d
dankelley <kelley.dan@gmail.com>
2018-04-23 16:42:24 -0300

    1401

--- 14xx/1401/1401a.R
+++ 14xx/1401/1401a.R
@@ -2,8 +2,14 @@ library(oce)
 options(warn=1)
 f <- '/users/kelley/Dropbox/oce-working-notes/tests/adp-rdi/winriver_vesselmounted_1200kHz/DATA_20080701123035_000r.000'
 if (file.exists(f)) {
-    d <- read.adp.rdi(f, debug=100)
+    d <- read.adp.rdi(f, debug=0)
     ldc <- oce:::do_ldc_rdi_in_file(f, from=1, to=0, by=1, 0L)
+}
+
+if (!interactive()) {
+    png("1401a1.png", width=7, height=7, unit="in", res=150, pointsize=9)
+    plot(d)
+    dev.off()
 }
 
 ## I don't see a pattern to the timing, but the fact that
@@ -11,9 +17,10 @@ if (file.exists(f)) {
 print(table(diff(ldc$ensembleStart)))
 d <- diff(ldc$ensembleStart)
 
+
 ## Plot some data, to see if there's a pattern
 if (!interactive())
-    png("1401.png", width=7, height=7, unit="in", res=150, pointsize=9)
+    png("1401a2.png", width=7, height=7, unit="in", res=150, pointsize=9)
 par(mfrow=c(4, 1), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 plot(1:200, d[1:200], type='s', col='lightgray', ylab="diff(ensembleStart)")
 points(1:200, d[1:200], pch=20, cex=0.7, col=2)
@@ -26,25 +33,27 @@ points(401:600, d[401:600], pch=20, cex=0.7, col=2)
 
 plot(601:800, d[601:800], type='s', col='lightgray', ylab="diff(ensembleStart)")
 points(601:800, d[601:800], pch=20, cex=0.7, col=2)
-dev.off()
 
 df <- data.frame(d=d, buf3=as.numeric(head(ldc$outbuf[ldc$ensembleStart+3], -1)))
 print(table(df))
-stem(df$d)
-stem(df$buf3)
-cat("df$buf3 values: ", paste(unique(df$buf3), collapse=" "), "\n")
-stem(subset(df, buf3==3)$d)
-stem(subset(df, buf3==4)$d)
+##stem(df$d)
+##stem(df$buf3)
+##cat("df$buf3 values: ", paste(unique(df$buf3), collapse=" "), "\n")
+##stem(subset(df, buf3==3)$d)
+##stem(subset(df, buf3==4)$d)
 
 ## experiment on the 7f7f sequences
 cat(f, "\n")
-for (i in 1:10) {
-    offsets <- readBin(ldc$outbuf[ldc$ensembleStart[i]+seq(6, 2*
-    cat(sprintf("i=%3d ensemble_len=%5d num_data_types=%2d diff_start=%5d\n",
+for (i in 1:5) {
+    ensembleLength <- readBin(ldc$outbuf[ldc$ensembleStart[i]+2:3], "integer", n=1, size=2)
+    ntypes <- readBin(ldc$outbuf[ldc$ensembleStart[i]+5], "integer", n=1, size=1)
+    offsets <- readBin(ldc$outbuf[ldc$ensembleStart[i]+6+seq(0, 2*ntypes)], "integer", n=ntypes, size=2)
+    cat(sprintf("i=%3d ensembleLength=%5d ntypes=%2d diff_start=%5d",
                 i,
-                readBin(ldc$outbuf[ldc$ensembleStart[i]+2:3], "integer", n=1, size=2),
-                readBin(ldc$outbuf[ldc$ensembleStart[i]+5], "integer", n=1, size=1),
+                ensembleLength,
+                ntypes,
                 ldc$ensembleStart[i+1]-ldc$ensembleStart[i]))
+    cat(" offsets=", paste(offsets, collapse=" "), "\n")
 }
 
-
+if (!interactive()) dev.off()
--- 14xx/1401/1401b.R
+++ 14xx/1401/1401b.R
@@ -1,10 +1,12 @@
-message("read file from data(adp) to see if any unknown codes\n")
 library(oce)
 f <- "/data/archive/sleiwex/2008/moorings/m09/adp/rdi_2615/raw/adp_rdi_2615.000"
 if (file.exists(f)) {
-    beam <- read.adp.rdi(f, debug=100)
+    beam <- read.adp.rdi(f, debug=0)
     ldc <- oce:::do_ldc_rdi_in_file(f, from=1, to=0, by=1, 0L)
 }
+if (!interactive()) png("1401b.png")
+plot(beam)
+if (!interactive()) dev.off()
 
 ## experiment on the 7f7f sequences
 for (i in 1:10) {
