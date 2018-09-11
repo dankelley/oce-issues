20a0941daed6c9acb8afbdd735d364f296992562
dankelley <kelley.dan@gmail.com>
2018-09-11 15:47:38 -0300

    1440

---
+++ 14xx/1440/1440a.R
@@ -0,0 +1,29 @@
+library(oce)
+library(testthat)
+for (n in c(1, 20)) {
+    cat("n=", n, "\n")
+    T <- seq(2,3, length.out=n)
+    S <- seq(31.4, 31.6, length.out=n)
+    p <- seq(0, n, length.out=n)
+    lat <- rep(44.5, n)
+    lon <- rep(-63, n)
+    stn <- 'fake'
+    time <- seq(from=as.POSIXct("2012-01-01 00:00", tz='UTC'), by='min', length.out=20)
+    ctd <- as.ctd(salinity=S,
+                  temperature=T,
+                  pressure=p,
+                  time=time,
+                  longitude=lon,
+                  latitude=lat,
+                  station=stn,
+                  startTime=min(time))
+    expect_true("longitude" %in% names(ctd[["metadata"]]))
+    expect_true("latitude" %in% names(ctd[["metadata"]]))
+    if (n > 1) {
+        expect_true("longitude" %in% names(ctd[["data"]]))
+        expect_equal(length(ctd[["data"]][["longitude"]]), n)
+        expect_true("latitude" %in% names(ctd[["data"]]))
+        expect_equal(length(ctd[["data"]][["latitude"]]), n)
+    }
+}
+
