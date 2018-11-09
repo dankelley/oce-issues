6237be88436bf59def3a1e91b59afc6014a5c9e3
dankelley <kelley.dan@gmail.com>
2018-11-09 16:06:02 -0400

    1457

--- 14xx/1457/1457a.R
+++ 14xx/1457/1457a.R
@@ -1,6 +1,5 @@
 library(oce)
-debug <- 10
 old <- read.oce("old.cnv")
 new1 <- read.oce("new.cnv")
-new2 <- read.oce("new.cnv", columns=list(conductivity=list(name="cond0S/m", unit=list(unit=expression(S/m), scale=""))), debug=debug)
+new2 <- read.oce("new.cnv", columns=list(conductivity=list(name="cond0S/m", unit=list(unit=expression(S/m), scale=""))))
 
