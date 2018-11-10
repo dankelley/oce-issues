727962dc14759c24a41476c71c1e99a3bfcd1de8
dankelley <kelley.dan@gmail.com>
2018-11-10 07:59:26 -0400

    1457

--- 14xx/1457/1457a.R
+++ 14xx/1457/1457a.R
@@ -2,4 +2,3 @@ library(oce)
 old <- read.oce("old.cnv")
 new1 <- read.oce("new.cnv")
 new2 <- read.oce("new.cnv", columns=list(conductivity=list(name="cond0S/m", unit=list(unit=expression(S/m), scale=""))))
-
---
+++ 14xx/1457/1457b.R
@@ -0,0 +1,24 @@
+library(oce)
+microcat <- read.oce("2062a.cnv")
+microcat <- oceSetMetadata(microcat, "deploymentType", "moored")
+## Get system upload time, which should give us the year of the recovery
+## time. We'll need that because we are going to construct time from
+## the timeJV2 column.
+microcat[["metadata"]][["time"]]
+## Now, we ought to check on how many days the machine has been
+## recording, because it could be multiple years, and if so we'll
+## need to set our start time accordingly
+range(microcat[["timeJV2"]])
+## OK, under a year. Assuming no wrap-around (which would give
+## an upper range of 365 or 366), we can now make a time column
+t0 <- as.POSIXct("2018-01-01 00:00:00", tz="UTC")
+t <- t0 + microcat[["timeJV2"]] * 86400
+## Let's insert this into the data
+microcat <- oceSetData(microcat, "time", t)
+## Finally, to avoid confusion (whew), we destroy the time
+## entry from metadata
+microcat <- oceDeleteMetadata(microcat, "time")
+
+if (!interactive()) png("1456b.png")
+plot(microcat)
+if (!interactive()) dev.off()
