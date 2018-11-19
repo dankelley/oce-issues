1d3e6f73290c5f313481e6fc3e4b8f4ea42e1f46
dankelley <kelley.dan@gmail.com>
2018-11-19 15:57:29 -0400

    .

--- 14xx/1463/1463b.R
+++ 14xx/1463/1463b.R
@@ -1,12 +1,14 @@
 library(oce)
 library(shiny)
-ui <- fluidPage(fluidRow(column=2, sliderInput("l", "log10(time interval [sec])", min=0, max=6, value=1, step=0.01)),
-                fluidRow(column(10, plotOutput("timeSeries"))))
+ui <- fluidPage(fluidRow(column=1,
+                         sliderInput("l", "log10(time interval [sec])",
+                                     min=0, max=7, value=log10(50), step=0.01)),
+                fluidRow(column(12, plotOutput("timeSeries"))))
 server <- function(input, output) {
     output$timeSeries <- renderPlot({
         t0 <- as.POSIXct("2018-11-19 00:00:00", tz="UTC")
         t <- c(t0, t0+10^input$l)
-        oce.plot.ts(t, seq_along(t), debug=10)
+        oce.plot.ts(t, seq_along(t))
         mtext(sprintf("l=%.2g, 10^l=%.2g [sec]", input$l, 10^input$l), side=3, line=-1)
     })
 }
