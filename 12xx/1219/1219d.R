ddc66eb62e65fa3c78e9b45ba0182410fd164529
dankelley <kelley.dan@gmail.com>
2018-11-20 16:27:01 -0400

    ad2cp handles updated 'ad2cp' oce branch

--- 12xx/1219/1219d.R
+++ 12xx/1219/1219d.R
@@ -7,50 +7,45 @@ library(testthat)
 ## that has been merged.
 f <- "~/Dropbox/oce_ad2cp/labtestsig3.ad2cp" # DK only tests
 if (file.exists(f)) {
-    N <- 100
-    d <- read.ad2cp(f, 1, N, 1)
+    d <- read.ad2cp(f, 1, 100, 1)
     if (inherits(d, "oce") && "AD2CP" == d[["instrumentType"]]) {
         if (!interactive()) png("1219d_1.png")
-        look <- which(d[["id"]] == 22)
         par(mfrow=c(2,3), mar=c(2.5,2.5,1,1), mgp=c(1.5,0.5,0)) # 2x3 enough for head()
-        for (l in head(look)) {
-            v <- d[["v"]][[l]]
-            dist <- d[["blanking"]][l] + d[["cellSize"]][l][1] + 1:dim(v)[1]
-            plot(v[,1], dist, xlim=c(min(v), max(v)), type='l', xlab="velo?", ylab="Distance [m] ???")
+        firstVelo <- which(d[["id"]] == 22)[1]
+        v <- d[["v"]]
+        dist <- d[["blanking"]][firstVelo] + d[["cellSize"]][firstVelo]*seq(1, dim(v)[2])
+        for (l in 1:6) {
+            plot(v[l,,1], dist, xlim=c(min(v), max(v)), type='l',
+                 xlab="velo beams", ylab="Distance [m] ???")
             mtext(sprintf("d$v[[%d]]", l), side=3, cex=0.6)
             for (i in 2:4) {
-                lines(v[,i], dist, type='l', col=i)
+                lines(v[l,,i], dist, type='l', col=i)
             }
         }
         if (!interactive()) dev.off()
-        ## test ideas for a velo image plot
-        ## fill up an array for velocity
-        ncell <- dim(d[["v"]][[look[1]]])[1]
-        nbeam <- dim(d[["v"]][[look[1]]])[2]
-        V <- array(double(), dim=c(length(look), ncell, nbeam))
-        for (i in seq_along(look)) {
-            V[i, ,] <- d[["v"]][[look[i]]]
-        }
+        v <- d[["v"]]
         ## Plot as images, one panel per component
         if (!interactive()) png("1219d_2.png")
-        zlim <- max(abs(d[["v"]][[look[1]]])) * c(-1, 1)
-        par(mfrow=c(nbeam, 2))
-        ntime <- dim(V[,,1])[1]
+        zlim <- max(abs(v)) * c(-1, 1)
+        par(mfrow=c(dim(v)[3], 2))
+        ntime <- dim(v)[1]
+        nbeam <- dim(v)[3]
+        time <- d[["time"]][d[["id"]]==22]
         for (i in seq_len(nbeam)) {
-            imagep(d[['time']][look], dist, V[,,i], zlim=zlim, ylab="Distance [m] ???")
-            hist(V[,,i])
+            imagep(time, dist, v[,,i], zlim=zlim, ylab="Distance [m] ???")
+            hist(v[,,i])
         }
         if (!interactive()) dev.off()
         ## Plot crude u and v-like quantities to check if hist() peaks near 0
         if (!interactive()) png("1219d_3.png")
-        u13 <- V[,,1] - V[,,3]
-        u24 <- V[,,2] - V[,,4]
-        zlim <- max(abs(c(u13,u24))) * c(-1, 1)
+        v13 <- v[,,1] - v[,,3]
+        v24 <- v[,,2] - v[,,4]
+        zlim <- max(abs(c(v13, v24))) * c(-1, 1)
         par(mfrow=c(2, 2))
-        imagep(d[['time']][look], dist, u13, zlim=zlim, ylab="Distance [m] ???")
-        hist(u13)
-        imagep(d[['time']][look], dist, u24, zlim=zlim, ylab="Distance [m] ???")
-        hist(u24)
+        imagep(time, dist, v13, zlim=zlim, ylab="Distance [m] ???")
+        hist(v13)
+        imagep(time, dist, v24, zlim=zlim, ylab="Distance [m] ???")
+        hist(v24)
         if (!interactive()) dev.off()
     }
 }
