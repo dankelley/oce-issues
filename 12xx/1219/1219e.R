f35314a5c0143c496983d6683952c7cab9ea95df
dankelley <kelley.dan@gmail.com>
2018-12-07 11:17:39 -0400

    .

--- 12xx/1219/1219e.R
+++ 12xx/1219/1219e.R
@@ -1,10 +1,13 @@
 library(oce)
 f <- "~/Dropbox/quipp1.ad2cp"
 if (file.exists(f)) {
-    d <- read.ad2cp(f, from=1, to=100, by=1, debug=10)
+    d <- read.ad2cp(f, from=1, to=1000, by=1)#, debug=10)
     summary(d)
-    if (!interactive()) png("1219e.png")
+    if (!interactive()) png("1219e1.png", unit="in", res=150, width=7, height=7, pointsize=10)
     plot(d)
+    if (!interactive()) dev.off()
+    if (!interactive()) png("1219e2.png", unit="in", res=150, width=7, height=7, pointsize=10)
+    plot(d, which="amplitude")
     if (!interactive()) dev.off()
 }
 
