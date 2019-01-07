c11d0934895546c32e7fae1ce86b5895631a5f17
dankelley <kelley.dan@gmail.com>
2019-01-07 16:36:07 -0400

    .

--- 14xx/1472/1472a.R
+++ 14xx/1472/1472a.R
@@ -1,6 +1,7 @@
 library(oce)
 data(section)
-sg <- sectionGrid(section)
+GS <- subset(section, 109<=stationId&stationId<=129)
+GSg <- sectionGrid(GS, p=seq(0, 2000, 100), debug=3)
 stations <- GSg[["station"]]
 
 for (istation in seq_along(stations)) {
@@ -13,13 +14,8 @@ for (istation in seq_along(stations)) {
     message("  salinity is OK")
 }
 
-## 1. start of section, default fields.
-plot(head(section))
-## 2. Gulf Stream
-GS <- subset(section, 109<=stationId&stationId<=129)
-GSg <- sectionGrid(GS, p=seq(0, 2000, 100))
-plot(GSg, which=c(1, 99), map.ylim=c(34, 42))
 par(mfrow=c(2, 1))
 plot(GS, which=1, ylim=c(2000, 0), ztype='points',
      zbreaks=seq(0,30,2), pch=20, cex=3)
 plot(GSg, which=1, ztype='image', zbreaks=seq(0,30,2))
+
