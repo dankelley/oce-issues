0b254d34b301d70abd57b3f147c5381cacb0a70e
dankelley <kelley.dan@gmail.com>
2019-01-08 12:57:17 -0400

    better tests for 1472

--- 14xx/1472/1472a.R
+++ 14xx/1472/1472a.R
@@ -1,21 +1,20 @@
 library(oce)
 data(section)
 GS <- subset(section, 109<=stationId&stationId<=129)
-GSg <- sectionGrid(GS, p=seq(0, 2000, 100), debug=3)
+GSg <- sectionGrid(GS, p=seq(0, 2000, 100), debug=3, method="approxML")
 stations <- GSg[["station"]]
 
 for (istation in seq_along(stations)) {
-    message("station ", istation)
+    cat("station ", istation, "\n")
     station <- stations[[istation]]
-    print(station)
     expect_true(is.finite(station[["temperature"]][1]))
-    message("  temperature is OK")
     expect_true(is.finite(station[["salinity"]][1]))
-    message("  salinity is OK")
+    cat("  ... top level is non-NA, so this station passes the test\n")
 }
 
+if (!interactive()) png("1472a.png")
 par(mfrow=c(2, 1))
 plot(GS, which=1, ylim=c(2000, 0), ztype='points',
-     zbreaks=seq(0,30,2), pch=20, cex=3)
+     zbreaks=seq(0,30,2), pch=20, cex=2)
 plot(GSg, which=1, ztype='image', zbreaks=seq(0,30,2))
-
+if (!interactive()) dev.off()
