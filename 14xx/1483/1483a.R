496a5e39785809c6eb1c52a61d0025274526874e
dankelley <kelley.dan@gmail.com>
2019-01-20 09:56:52 -0400

    1483 tested a bit more

--- 14xx/1483/1483a.R
+++ 14xx/1483/1483a.R
@@ -2,6 +2,8 @@ library(oce)
 d <- read.oce("~/Dropbox/oce_secret_data/01.rsk")
 summary(d)
 if (!interactive()) png("1483a.png", height=250)
+par(mfrow=c(2, 1))
+hist(d[["conductivity"]])
 hist(d[["cond12"]])
 if (!interactive()) dev.off()
 
