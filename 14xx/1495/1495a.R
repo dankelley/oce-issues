60e412c0c94a2706849acf3ed8771f24213225a7
dankelley <kelley.dan@gmail.com>
2019-02-20 14:47:11 -0400

    grid

--- 14xx/1495/1495a.R
+++ 14xx/1495/1495a.R
@@ -5,12 +5,10 @@ n <- 100
 y <- 1:n + 10 ^ rnorm(n)
 t <- as.POSIXct(seq(as.Date('2017-01-01'), by = 'day', length.out = n))
 
-## source("~/git/oce/R/oce.R");
-## source("~/git/oce/R/misc.R");
-## source("~/git/oce/R/imagep.R");
 if (!interactive()) png("1485a.png")
 par(mfrow=c(2, 2))
 oce.plot.ts(x = t, y = y)
+grid() # lets us see if base grid() has same colour etc
 oce.plot.ts(x = t, y = y, log = 'y')
 oce.plot.ts(x = t, y = y, grid = TRUE)
 oce.plot.ts(x = t, y = y, log = 'y', grid = TRUE)
