11c55ef47321214f5cbdec0151a75bee8f1f76d9
dankelley <kelley.dan@gmail.com>
2019-03-16 15:36:05 -0300

    further tests (on two subsets)

--- 15xx/1508/1508.R
+++ 15xx/1508/1508.R
@@ -1,19 +1,20 @@
 library(oceanglider)
-if (!exists("g")) {
+if (!exists("g0")) {
     file <- "~/Dropbox/sea021m49/netcdf/GLI2018_SEA021_049DM_L1.nc"
     ## file <- "/data/archive/glider/2019/sx/sea021m49/netcdf/GLI2018_SEA021_049DM_L1.nc"
-    g <- read.glider(file)
-    head(g[["time"]], 10)
+    g0 <- read.glider(file)
+    head(g0[["time"]], 10)
     ## The above shows that we have a few (4) points from 2018-07-12 at the start,
     ## followed by data on 2019-02-22, so we subset for just the later data.
     start <- as.POSIXct("2019-02-21")
-    g <- subset(g, time > start)
-    g  <- subset(g, salinity < 40)
+    g1 <- subset(g0, time > start)
+    g2 <- subset(g1, salinity < 40)
 }
 
-par(mfrow=c(2,1))
 focus <- as.POSIXct(c('2019-02-27', '2019-03-05'), tz='UTC')
-oce.plot.ts(g[['time']], g[['pressure']], xlim=focus)
-focus <- as.POSIXct(c('2019-02-27', '2019-02-28'), tz='UTC')
-oce.plot.ts(g[['time']], g[['pressure']], xlim=focus)
+par(mfrow=c(2,2))
+plot(g0[['time']], g0[['pressure']], xlim=focus) # OK
+plot(g1[['time']], g1[['pressure']], xlim=focus) # OK
+plot(g2[['time']], g2[['pressure']], xlim=focus) # OK
+oce.plot.ts(g2[['time']], g2[['pressure']], xlim=focus) # FAILS
 
