da05e16a71293a17ea640f009bd6dc16d6e9a6c9
dankelley <kelley.dan@gmail.com>
2019-03-16 13:27:35 -0300

    .

---
+++ 15xx/1508/1508.R
@@ -0,0 +1,17 @@
+library(oceanglider)
+if (!exists("g")) {
+    file <- "~/Dropbox/sea021m49/netcdf/GLI2018_SEA021_049DM_L1.nc"
+    g <- read.glider(file)
+    head(g[["time"]], 10)
+    ## The above shows that we have a few (4) points from 2018-07-12 at the start,
+    ## followed by data on 2019-02-22, so we subset for just the later data.
+    start <- as.POSIXct("2019-02-21")
+    g <- subset(g, time > start)
+}
+
+par(mfrow=c(2,1))
+focus <- as.POSIXct(c('2019-02-27', '2019-03-05'), tz='UTC')
+oce.plot.ts(g[['time']], g[['pressure']], xlim=focus)
+focus <- as.POSIXct(c('2019-02-27', '2019-02-28'), tz='UTC')
+oce.plot.ts(g[['time']], g[['pressure']], xlim=focus)
+
