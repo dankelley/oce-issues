d2d24e5f96074e2882a668f269d77329e2283d09
dankelley <kelley.dan@gmail.com>
2019-07-17 16:47:50 -0300

    test code for issue 1583

--- 15xx/1583/1583.R
+++ 15xx/1583/1583.R
@@ -2,8 +2,15 @@ library(oce)
 data(section)
 gs <- subset(section, 115<=stationId&stationId<=125)
 
-xgrid <- seq(0, ceiling(max(gs[['distance', 'byStation']])), by = 1)
-ygrid <- seq(5, ceiling(max(gs[['pressure']])), by = 5)
-# Barnes
-source("~/git/oce/R/section.R")
-gsBarnes <- sectionSmooth(gs, "barnes", xg = xgrid, yg = ygrid, debug = 3)
+## NOTE: by=1 produces all NA values in the resultant gsBarnes. But if
+## we also set xr=10, then things work. Or, we can set by=2 and it works.
+## xgrid <- seq(0, ceiling(max(gs[['distance', 'byStation']])), by = 1) # fails to plot
+xgrid <- seq(0, ceiling(max(gs[['distance', 'byStation']])), by = 1) # fails to plot
+##xgrid <- seq(0, ceiling(max(gs[['distance', 'byStation']])), by = 2) # plots OK
+ygrid <- seq(5, ceiling(max(gs[['pressure']])), by = 50) # c. 100 points
+gsBarnes <- sectionSmooth(gs, "barnes", xg = xgrid, yg = ygrid, xr=10, debug = 3)
+
+par(mfrow=c(2,1))
+plot(gs, eos='unesco', which="temperature")
+plot(gsBarnes, eos='unesco', which="temperature")
+
