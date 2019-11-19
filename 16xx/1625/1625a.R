78bcac67aefd52976285001eced9f2f12a62fd69
dankelley <kelley.dan@gmail.com>
2019-11-19 19:15:14 -0400

    issue 1625

---
+++ 16xx/1625/1625a.R
@@ -0,0 +1,32 @@
+## Test new functionality for issue 1625
+library(oce)
+data(coastlineWorld)
+
+if (!interactive()) png("1625a.png")
+par(mfrow=c(2, 2), mar=c(3,3,2,1))
+col <- "lightgray"
+lonlim <- c(-20, 20)
+latlim <- c(20, 50)
+proj <- "+proj=lcc +lat_1=30 +lat_2=40"
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj)
+mtext("Expect ticks & labels on bottom & left", pch=0.4)
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        latlabels=30)
+mtext("Expect tick & label at 30N", pch=0.4)
+
+mapPlot(coastlineWorld, ,col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        latlabels=FALSE)
+mtext("Expect ticks but no numbers at left", pch=0.4)
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        latlabels=NULL)
+mtext("Expect no ticks or numbers at left", pch=0.4)
+
+if (!interactive()) dev.off()
+
---
+++ 16xx/1625/1625b.R
@@ -0,0 +1,33 @@
+## Test new functionality for issue 1625
+library(oce)
+data(coastlineWorld)
+
+if (!interactive()) png("1625b.png")
+par(mfrow=c(2, 2))
+par(mar=c(3,3,2,1))
+col <- "lightgray"
+lonlim <- c(-20, 20)
+latlim <- c(20, 50)
+proj <- "+proj=lcc +lat_1=30 +lat_2=40"
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj)
+mtext("Expect ticks & labels on bottom & left", pch=0.4)
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        lonlabels=c(-5,5))
+mtext("FAIL Expect tick & label at 5W and 5E", pch=0.4)
+
+mapPlot(coastlineWorld, ,col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        lonlabels=FALSE)
+mtext("Expect ticks but no numbers at bottom", pch=0.4)
+
+mapPlot(coastlineWorld, col=col,
+        longitudelim=lonlim, latitudelim=latlim, projection=proj,
+        lonlabels=NULL)
+mtext("Expect no ticks or numbers at bottom", pch=0.4)
+
+if (!interactive()) dev.off()
+
