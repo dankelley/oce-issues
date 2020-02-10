c71b6a95e78fd1284b8d083dbd100e57c9a0bc4f
dankelley <kelley.dan@gmail.com>
2020-02-10 11:33:36 -0400

    .

--- 16xx/1656/1656.R
+++ 16xx/1656/1656.R
@@ -1,12 +1,12 @@
-## Show that plot()'s TS plots obey col, pch, and cex.
+## Show that plot() passes col (etc) to plotTS() and plotProfile()
 library(oce)
 load("testCTD.RData")
 cm <- colormap(haul05.trim[["pressure"]])
 if (!interactive()) png("issue1657.png")
-par(mfrow=c(2, 2))
-plot(haul05.trim, which=3)
+par(mfrow=c(2, 1))
 plot(haul05.trim, which=3, col=cm$zcol, pch=20, cex=2)
-plotProfile(haul05.trim, xtype="fluorescence", type="p")
+plotTS(haul05.trim, col=cm$zcol, pch=20, cex=2)
+plotProfile(haul05.trim, xtype="fluorescence", type="p", col=cm$zcol, pch=20, cex=2)
 plot(haul05.trim, which="fluorescence", type="p", col=cm$zcol, pch=20, cex=2)
 if (!interactive()) dev.off()
 
