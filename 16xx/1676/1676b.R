5803713c30e6bcf36381ffb37f6b6e0509dcd270
dankelley <kelley.dan@gmail.com>
2020-04-13 19:40:46 -0300

    clean up test code

--- 16xx/1676/1676b.R
+++ 16xx/1676/1676b.R
@@ -1,10 +1,19 @@
 library(oce)
 file <- "~/Dropbox/S101135A001_Ronald.ad2cp"
-d <- read.oce(file, debug=5)
-if (!interactive()) png("1676b_image.png")
+d <- read.oce(file) # , debug=5)
+if (!interactive()) png("1676b_velocity.png")
 plot(d, zlim=c(-1.5, 1.5))
 summary(d)
 if (!interactive()) dev.off()
+
+if (!interactive()) png("1676b_backscatter.png")
+par(mfrow=c(4,1), mar=c(3,3,2,1), mgp=c(2,0.7,0))
+plot(d, which="a1")
+plot(d, which="a2")
+plot(d, which="a3")
+plot(d, which="a4")
+if (!interactive()) dev.off()
+
 if (!interactive()) png("1676b_hist.png")
 par(mfrow=c(3,1), mar=c(3,3,2,1), mgp=c(2,0.7,0))
 v <- d[["v"]]
@@ -12,4 +21,4 @@ hist(v[,,1], xlab="u [m/s]", main="")
 hist(v[,,2], xlab="v [m/s]", main="")
 hist(v[,,3], xlab="z [m/s]", main="")
 if (!interactive()) dev.off()
-## 
+
--- 16xx/1676/1676c.R
+++ 16xx/1676/1676c.R
@@ -1,30 +1,49 @@
-##  at cindex=166769238 (17.755% through file) chunk=20624: size=12 id=0x23 dataSize=16032 dataChecksum=1155 headerChecksum=5495
-##  at cindex=166785282 (17.756% through file) chunk=20625: size=10 id=0x1c dataSize=952 dataChecksum=13270 headerChecksum=2011
-##  at cindex=166786244 (17.757% through file) chunk=20626: size=12 id=0x23 dataSize=16032 dataChecksum=62255 headerChecksum=1059
-## warning: ldc_ad2cp_in_file() at cindex=166786256 (17.757% through file), cindex_last_good=166785282, data checksum is 63402 but it should be 62255
-## warning: expected header.sync to be 0xa5 but it was 0x00 at cindex=166802288 (17.758% through file) ... skipping to next 0xa5 chacter...
-## ... got a sync character (0xa5) at cindex=166821940 (17.76% through file) so trying to continue from there
-## Error in do_ldc_ad2cp_in_file(filename, from, to, by, debug - 1) : 
-##   header_size is 165, but it must be 10 or 12 at cindex=166821940 (17.76% through file)
-## Calls: read.oce -> read.adp.ad2cp -> do_ldc_ad2cp_in_file
+## Next is pretty handy, so putting it here to find it later
+plotRaw <- function(buf, i, events, highlight)
+{
+    omar <- par("mar")
+    omgp <- par("mgp")
+    par(mar=c(3, 3, 2.5, 1), mgp=c(2, 0.7, 0))
+    plot(i, as.integer(buf[i]), pch=".", yaxs="i",
+         xlab="Buffer Index", ylab="Buffer Value (base 10)")
+    n <- length(events[[1]])
+    for (j in seq_len(n)) {
+        rug(events$i[j], side=1, col=events$col[j], lwd=2, ticksize=-0.02)
+        rug(events$i[j], side=1, col=events$col[j], lwd=2, ticksize=0.02)
+        rug(events$i[j], side=3, col=events$col[j], lwd=2, ticksize=0.02)
+        rug(events$i[j], side=3, col=events$col[j], lwd=2, ticksize=-0.02)
+        mtext(events$label[j], side=3, at=events$i[j], col=events$col[j], line=0.5)
+    }
+    ih <- which(buf[i] == highlight)
+    abline(v=i[ih], col="magenta", lwd=1/2)
+    mtext(paste0("Highlighting where buffer is 0x", highlight), side=4, col="magenta")
+    par(mar=omar, mgp=omgp)
+}
+
 library(oce)
 fileSize <- 939294720
 o <- seq(0, 12)
-buf <- readBin("~/Dropbox/S101135A001_Ronald.ad2cp", what="raw", n=fileSize)
+## Cache
+if (!exists("buf"))
+    buf <- readBin("~/Dropbox/S101135A001_Ronald.ad2cp", what="raw", n=fileSize)
 
-cindex_last_good <- 166785282
-data.frame(o, buf=buf[cindex_last_good + o + 1]) # the +1 is to go from C to R
 dataSize <- 16032
-O <- seq(-20, 20)
-i <- seq(-dataSize, 5*dataSize)
-B <- buf[cindex_last_good + i]
+
 if (!interactive()) png("1676c.png", width=7, height=3, unit="in", res=150, pointsize=9)
 par(mar=c(3,3,1,1), mgp=c(2,0.7,0))
-plot(i, as.integer(B), pch=".",
-     xlab="Byte index past start of bad chunk",
-     ylab="Integer value of byte")
-abline(v=0, col=2, lwd=2)
-abline(v=dataSize, col=4, lwd=2)
-mtext("Red: start of first invalid data chunk", adj=0, col=2)
-mtext("Blue: expected end of invalid data chunk", adj=1, col=4)
+
+i <- 166786244 + seq(-2*dataSize, 5*dataSize)
+plotRaw(buf, i=i, events=list(i=166786244+c(0,16032),
+                              label=c("Start", "End"),
+                              col=c("darkgreen", "red")),
+                              highlight=as.raw(0xa5))
+
 if (!interactive()) dev.off()
+
+d <- function(i, msg="", o=seq(-3,3))
+{
+    cat(msg, "\n")
+    data.frame(index=i+o, offset=o, buf=buf[i+o+1])
+}
+d(166786244, "ok")
+d(166805910, "bad")
