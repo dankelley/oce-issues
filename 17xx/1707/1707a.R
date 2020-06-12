1a98bd8c4ec065f0d40ac985d9c5c03b264d4e9d
dankelley <kelley.dan@gmail.com>
2020-06-12 10:32:49 -0300

    .

--- 17xx/1707/1707a.R
+++ 17xx/1707/1707a.R
@@ -4,11 +4,14 @@ proj <- "+proj=merc"
 lonlim <- c(-20, 20)
 latlim <- c(-20, 20)
 if (!interactive()) png("1707a.png")
-par(mfrow=c(2, 2), mar=c(2,2,1,1))
-for (axisStyle in 1:4) {
+par(mfrow=c(2, 3), mar=c(2,2,1,1))
+for (axisStyle in 1:5) {
     mapPlot(coastlineWorld, longitudelim=lonlim, latitudelim=latlim,
             col="tan", proj=proj, axisStyle=axisStyle, grid=c(5,5))
     mtext(paste0("axisStyle=", axisStyle), cex=par("cex"))
 }
+plot(0:1,0:1,xlab="",ylab="",axes=FALSE,type="n")
+box()
+text(0.5,0.5,"Test of\nmapPlot(...,axisType)\n(github issue 1707)")
 if (!interactive()) dev.off()
 
