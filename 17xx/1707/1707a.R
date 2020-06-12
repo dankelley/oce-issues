313d4b2a565bed0f064f2ee7ba5534a772b8b438
dankelley <kelley.dan@gmail.com>
2020-06-12 11:13:11 -0300

    .

--- 17xx/1707/1707a.R
+++ 17xx/1707/1707a.R
@@ -3,7 +3,7 @@ data("coastlineWorld")
 proj <- "+proj=merc"
 lonlim <- c(-20, 20)
 latlim <- c(-20, 20)
-if (!interactive()) png("1707a.png")
+if (!interactive()) png("1707a.png", width=7, height=5, res=120, unit="in")
 par(mfrow=c(2, 3), mar=c(2,2,1,1))
 for (axisStyle in 1:5) {
     mapPlot(coastlineWorld, longitudelim=lonlim, latitudelim=latlim,
