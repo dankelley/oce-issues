7bea50e25681d1a7290ad21abb08990c6faa3d78
dankelley <kelley.dan@gmail.com>
2020-11-14 10:46:44 -0400

    fix 1625 (need reporter check)

--- 16xx/1625/1625a.R
+++ 16xx/1625/1625a.R
@@ -2,35 +2,36 @@
 library(oce)
 data(coastlineWorld)
 
-source("~/git/oce/R/map.R")
+## source("~/git/oce/R/map.R")
 
 col <- "lightgray"
 lonlim <- c(-20, 20)
 latlim <- c(20, 50)
 proj <- "+proj=lcc +lat_1=30 +lat_2=40"
+debug <- 0
 for (geographical in 0:4) {
     if (!interactive()) png(paste0("1625a_", geographical, ".png"))
+    cex <- 0.85 * par("cex")
     par(mfrow=c(2, 2), mar=c(3,3,2,1))
     mapPlot(coastlineWorld, col=col, geographical=geographical,
-            longitudelim=lonlim, latitudelim=latlim, projection=proj)
-    mtext("Expect ticks & labels on bottom & left", cex=par("cex"))
-    mtext(paste0("geographical=", geographical), side=3, line=1.2, col=2, font=2, cex=0.9)
+            longitudelim=lonlim, latitudelim=latlim, projection=proj, debug=debug)
+    mtext("a) default: expect ticks & labels", cex=cex)
+    mtext(paste0("geographical=", geographical, ": examine LEFT axis"), side=3, line=1.2, col=2, font=2, cex=cex)
 
     mapPlot(coastlineWorld, col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             latlabels=c(25, 35))
-    mtext("Expect tick & label at 25N and 30N", cex=par("cex"))
+    mtext("b) latlabels=#: expect ticks & labels at 25N,30N", cex=cex)
 
     mapPlot(coastlineWorld, ,col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             latlabels=FALSE)
-    mtext("Expect ticks but no numbers at left", cex=par("cex"))
+    mtext("c) latlabels=FALSE: expect ticks without labels", cex=cex)
 
     mapPlot(coastlineWorld, col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             latlabels=NULL)
-    mtext("Expect no ticks or numbers at left", cex=par("cex"))
+    mtext("d) latlabels=NULL: expect no ticks or labels", cex=cex)
     if (!interactive()) dev.off()
 }
-
 
--- 16xx/1625/1625b.R
+++ 16xx/1625/1625b.R
@@ -2,35 +2,36 @@
 library(oce)
 data(coastlineWorld)
 
-source("~/git/oce/R/map.R")
+## source("~/git/oce/R/map.R")
 
 col <- "lightgray"
 lonlim <- c(-20, 20)
 latlim <- c(20, 50)
 proj <- "+proj=lcc +lat_1=30 +lat_2=40"
-debug <- TRUE
+debug <- 0
 for (geographical in 0:4) {
     if (!interactive()) png(paste0("1625b_", geographical, ".png"))
+    cex <- 0.85 * par("cex")
     par(mfrow=c(2, 2), mar=c(3,3,2,1))
     mapPlot(coastlineWorld, col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj, debug=debug)
-    mtext("Expect ticks & labels on bottom & left", cex=par("cex"))
+    mtext("a) default: expect ticks & labels", cex=par("cex"))
+    mtext(paste0("geographical=", geographical, ": examine BOTTOM axis"), side=3, line=1.2, col=2, font=2, cex=cex)
 
     mapPlot(coastlineWorld, col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             lonlabels=c(-5,5), debug=debug)
-    mtext("Expect tick & label at 5W and 5E", cex=par("cex"))
+    mtext("b) lonlabels=#: expect ticks & labels at 5W and 5E", cex=cex)
 
     mapPlot(coastlineWorld, ,col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             lonlabels=FALSE, debug=debug)
-    mtext("Expect ticks but no numbers at bottom", cex=par("cex"))
+    mtext("c) lonlabels=FALSE: expect ticks but no labels", cex=cex)
 
     mapPlot(coastlineWorld, col=col, geographical=geographical,
             longitudelim=lonlim, latitudelim=latlim, projection=proj,
             lonlabels=NULL, debug=debug)
-    mtext("Expect no ticks or numbers at bottom", cex=par("cex"))
+    mtext("d) lonlabels=NULL: expect no ticks or labels", cex=cex)
     if (!interactive()) dev.off()
 }
-
 
