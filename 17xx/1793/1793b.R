011ce3fc6232ee9bc12035facceeed9eedc97150
dankelley <kelley.dan@gmail.com>
2021-03-21 13:51:56 -0300

    cleanup

--- 17xx/1793/1793b.R
+++ 17xx/1793/1793b.R
@@ -4,6 +4,8 @@ for (lon in seq(50, 51, 0.1)) {
     cat("lon=", lon, " ... next (with keep=TRUE,warn=FALSE) works:\n")
     print(sf_project("+proj=longlat", "+proj=ortho +lon_0=-40", cbind(lon,0), keep=TRUE, warn=FALSE))
     cat("lon=", lon, " ... next (with default keep,warn) fails at lon=50.1:\n")
+    try({
     print(sf_project("+proj=longlat", "+proj=ortho +lon_0=-40", cbind(lon,0)))
+    })
 }
 
