77ded8f1ca686282bf198720287bdd3364853e84
dankelley <kelley.dan@gmail.com>
2021-04-17 16:49:22 -0300

    refine benchmark grid

--- 18xx/1805/1805d.R
+++ 18xx/1805/1805d.R
@@ -1,12 +1,12 @@
 # is findInterval() faster than cut()?
 t0 <- as.POSIXct("2021-01-01", tz="UTC")
-Ns <- 10^(4:8)
+Ns <- 10^seq(3, 8, 0.25)
 As <- rep(NA, length(Ns))
 Bs <- rep(NA, length(Ns))
 
 for (i in seq_along(Ns)) {
     t <- t0 + seq(1, Ns[i])
-    ninterval <- 2560
+    ninterval <- 2560L
     y <- rnorm(Ns[i])
     df <- data.frame(t, y)
     b <- seq(min(t), max(t), length.out=ninterval)
@@ -15,10 +15,10 @@ for (i in seq_along(Ns)) {
 }
 
 par(mfrow=c(2,1), mar=c(3,3,1,1), mgp=c(2,0.7,0))
-plot(Ns, As, type="b", log="x", ylim=range(c(As,Bs)),
+plot(Ns, As, type="o", log="x", ylim=range(c(As,Bs)),
      xlab="Number of Data Points", ylab="User Time [s]")
-lines(Ns, Bs, type="b", col=2)
+lines(Ns, Bs, type="o", col=2)
 legend("topleft", col=1:2, lwd=1, pch=1, legend=c("cut", "findInterval"))
 plot(Ns, As/Bs, type="b", log="x",
      xlab="Number of Data Points", ylab="Time Reduction Factor")
-
+data.frame(N=Ns, cutTime=As, findIntervalTime=Bs)
