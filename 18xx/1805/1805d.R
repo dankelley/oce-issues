61d5d6800b2f61196947915073f7b8222a4cfd2c
dankelley <kelley.dan@gmail.com>
2021-04-17 14:45:48 -0300

    test findInterval() vs cut() speed

---
+++ 18xx/1805/1805d.R
@@ -0,0 +1,24 @@
+# is findInterval() faster than cut()?
+t0 <- as.POSIXct("2021-01-01", tz="UTC")
+Ns <- 10^(4:8)
+As <- rep(NA, length(Ns))
+Bs <- rep(NA, length(Ns))
+
+for (i in seq_along(Ns)) {
+    t <- t0 + seq(1, Ns[i])
+    ninterval <- 2560
+    y <- rnorm(Ns[i])
+    df <- data.frame(t, y)
+    b <- seq(min(t), max(t), length.out=ninterval)
+    As[i] <- system.time(A <- split(df, cut(t, b)))[1]
+    Bs[i] <- system.time(B <- split(df, findInterval(t, b)))[1]
+}
+
+par(mfrow=c(2,1), mar=c(3,3,1,1), mgp=c(2,0.7,0))
+plot(Ns, As, type="b", log="x", ylim=range(c(As,Bs)),
+     xlab="Number of Data Points", ylab="User Time [s]")
+lines(Ns, Bs, type="b", col=2)
+legend("topleft", col=1:2, lwd=1, pch=1, legend=c("cut", "findInterval"))
+plot(Ns, As/Bs, type="b", log="x",
+     xlab="Number of Data Points", ylab="Time Reduction Factor")
+
