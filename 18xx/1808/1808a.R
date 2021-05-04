344a4cf4273c017e3b57b4af0e14bc557abe42eb
dankelley <kelley.dan@gmail.com>
2021-05-04 15:46:58 -0300

    cleanup

--- 18xx/1808/1808a.R
+++ 18xx/1808/1808a.R
@@ -1,18 +1,29 @@
-# vim: tw=140
+# Test a few local ODF files.  This is Dan's first test suite, since it is
+# faster than 1808b.R and it covers three institutions.
+
+# git clone https://github.com/dankelley/oce-issues.git
+
 
 library(oce)
 options(width=200)
 
-source("~/git/oce/R/misc.R")
-source("~/git/oce/R/odf.R")
-source("~/git/oce/R/oce.R")
+# Uncomment the next lines for testing, saving the time it takes to build oce.
 
+# source("~/git/oce/R/oce.R")
+# source("~/git/oce/R/misc.R")
+# source("~/git/oce/R/odf.R")
+# source("~/git/oce/R/ctd.R")
+# source("~/git/oce/R/ctd.odf.R")
+
+# IML lab
 diml <- read.oce("CTD_1994038_147_1_DN.ODF")
 summary(diml)
 
+# BIO lab
 dbio <- read.oce("CTD_BCD2014666_008_1_DN.ODF")
 summary(dbio)
 
+# UNB? lab
 dunb <- read.oce("CTD_2020003_004_1_DN.odf")
 summary(dunb)
 
--- 18xx/1808/1808b.R
+++ 18xx/1808/1808b.R
@@ -1,14 +1,21 @@
-# Test against a variety of ODF files, including some from the "odf_transform"
-# branch of https://github.com/cioos-siooc/cioos-siooc_data_transform.git
+# Test against a fairly wide variety of ODF files, including some from the
+# "odf_transform" branch of
+# https://github.com/cioos-siooc/cioos-siooc_data_transform.git
+
+# git clone https://github.com/dankelley/oce-issues.git
 
 library(oce)
-
-source("~/git/oce/R/oce.R")
-source("~/git/oce/R/misc.R")
-source("~/git/oce/R/odf.R")
-
 options(width=200)
-files <- list.files(".", "*.(ODF)|(odf)$", full.names=TRUE)[6]
+
+# Uncomment the next lines for testing, saving the time it takes to build oce.
+
+# source("~/git/oce/R/oce.R")
+# source("~/git/oce/R/misc.R")
+# source("~/git/oce/R/odf.R")
+# source("~/git/oce/R/ctd.R")
+# source("~/git/oce/R/ctd.odf.R")
+
+files <- list.files(".", "*.(ODF)|(odf)$", full.names=TRUE)
 n <- length(files)
 for (i in seq_along(files)) {
     cat(sprintf("\n\n~~~~~~~~~~~~~~~~~~~~~~~\n~~~ TEST %3d of %3d ~~~\n~~~~~~~~~~~~~~~~~~~~~~~\n\n", i, n), sep="")
