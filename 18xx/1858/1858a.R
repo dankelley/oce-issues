bee0deab6d7a3f0e82d28309f43b72d092325226
dankelley <kelley.dan@gmail.com>
2021-08-07 10:45:38 -0300

    .

--- 18xx/1858/1858a.R
+++ 18xx/1858/1858a.R
@@ -1,8 +1,10 @@
-# Not recognizing codes
-# 1. 0x00 0x30 binary fixed attitude (Surveyor Table 35, page 150)
+# Recognize code 0x00 0x30 binary fixed attitude (Surveyor Table 35, page 150),
+# to address github issue #1858.
+message(getwd())
+
 library(oce)
 for (f in list.files(pattern="*.ENS")) {
-    d <- read.adp.rdi(f, debug=1)
+    d <- read.adp.rdi(f, debug=0)
     cat(f, "\n  [[\"binaryFixedAttitudeHeaderRaw\"]]: ",
         paste(d[["binaryFixedAttitudeHeaderRaw"]],collapse=""), "\n", sep="")
 }
