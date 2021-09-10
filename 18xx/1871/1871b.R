8894f1df4d56d3af0ed5b34c22b0d8031ee5d6fa
dankelley <kelley.dan@gmail.com>
2021-09-10 18:20:44 -0300

    1871

--- 18xx/1871/1871b.R
+++ 18xx/1871/1871b.R
@@ -5,9 +5,19 @@ p0 <- "+proj=longlat +datum=WGS84 +no_defs"
 
 lon <- seq(-180, 180, 15)
 lat <- rep(85, 25)
+plot(lon, lat)
 
 xy <- sf::sf_project(p0, p, cbind(lon, lat))
+S <- 1e3
+if (!interactive()) png("1871b.png")
+plot(xy[,1]/S, xy[,2]/S, asp=1, xlab="easting [km]", ylab="northing [km]")
+grid()
 LONLAT <- sf::sf_project(p, p0, xy, keep=TRUE)
 df <- data.frame(lon=lon, lat=lat, x=xy[,1], y=xy[,2], LON=LONLAT[,1], LAT=LONLAT[,2])
 print(df)
+ok <- is.finite(df$LON)
+points(xy[ok,1]/S, xy[ok,2]/S, pch=20)
+mtext("filled symbols indicate invertible points")
+if (!interactive()) dev.off()
+
 
---
+++ 18xx/1871/1871c.R
@@ -0,0 +1,28 @@
+# Check: in the data frame shown at the end, do LON and LAT equal lon and lat?
+library(oce)
+library(sf)
+data(coastlineWorld)
+p <- "+proj=ortho +lon_0=-63 +lat_0=44"
+p0 <- "+proj=longlat +datum=WGS84 +no_defs"
+
+if (!interactive()) png("1871c.png")
+par(mar=c(0.5, 0.5, 1, 0.5))
+mapPlot(coastlineWorld, projection="+proj=ortho +lon_0=-63 +lat_0=44", type="p", pch=20, cex=0.2, col="forestgreen")
+
+lon <- seq(-180, 180, 15)
+for (lat0 in seq(-85, 85, 5)) {
+    lat <- rep(lat0, 25)
+    t <- try(xy <- sf::sf_project(p0, p, cbind(lon, lat), keep=TRUE))
+    if (!inherits(t, "try-error")) {
+        S <- 1e3
+        LONLAT <- sf::sf_project(p, p0, xy, keep=TRUE)
+        df <- data.frame(lon=lon, lat=lat, x=xy[,1], y=xy[,2], LON=LONLAT[,1], LAT=LONLAT[,2])
+        print(df)
+        ok <- is.finite(df$LON)
+        mapPoints(lon, lat, pch=20, col=ifelse(ok, "red", "black"), cex=1)
+    }
+}
+legend("topright", col=c("red","black"), pch=20, legend=c("invertible", "non-invertible"))
+if (!interactive()) dev.off()
+
+
