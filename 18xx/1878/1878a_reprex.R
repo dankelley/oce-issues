746f2289f2c157791280f3a0a31f8838f886d54c
dankelley <kelley.dan@gmail.com>
2022-01-01 14:46:43 -0400

    1878

--- 18xx/1878/1878a_reprex.R
+++ 18xx/1878/1878a_reprex.R
@@ -1,15 +1,11 @@
 library(oce)
 data(section)
-section <- handleFlags(section)
-# focus on the eastern region
-section <- subset(section, longitude > (-40))
-if (!interactive())
-    png("1878a_%02d.png", unit="in", width=7, height=7, res=200)
+section <- subset(handleFlags(section), longitude > (-40))
 
 par(mfrow=c(3,2))
 for (which in c("salinity", "temperature", "SA", "CT")) {
     plot(section, which=which)
-    mtext(paste0("which=\"",which,"\""), font=2)
+    mtext(paste0("which=\"",which,"\""), cex=par("cex"))
 }
 hist(section[["SA"]] - section[["salinity"]],
     main="SA-salinity contrast")
@@ -20,9 +16,6 @@ available <- section[["?"]]
 names <- sort(c(available$data, available$dataDerived))
 for (name in names) {
     plot(section, which=name)
-    mtext(paste0("which=\"", name, "\""), line=0.5, font=2)
+    mtext(paste0("which=\"", name, "\""), line=0.5, cex=par("cex"))
 }
-
-if (!interactive())
-    dev.off()
 
