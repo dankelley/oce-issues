974775bcf2c1cbec080f66547073be54e6d29820
dankelley <kelley.dan@gmail.com>
2022-01-02 18:08:20 -0400

    1906

---
+++ 19xx/1906/1906a.R
@@ -0,0 +1,37 @@
+library(oce)
+source("~/git/oce/R/ctd.R")
+source("~/git/oce/R/misc.R")
+data(section)
+stn <- section[["station", 100]]
+q <- stn[["?"]]
+p <- stn[["pressure"]]
+T <- stn[["temperature"]]
+S <- stn[["salinity"]]
+
+png("1906a_%02d.png", width=700, height=300)
+par(mfrow=c(1, 2))
+# 1. Check temperature
+plotProfile(stn, xtype="temperature",type="p") # docs say in-situ
+lines(T,p,col=2)
+text(6,2500,"Line should go\nthrough circles;\npanels should be\nidentical.",pos=4)
+plot(stn,which="temperature")
+lines(T,p,col=2)
+text(6,2500,"Line should go\nthrough circles;\npanels should be\nidentical.",pos=4)
+
+# 2. Check salinity
+plotProfile(stn, xtype="salinity", type="p")
+lines(S,p,col=2)
+text(35.2,2500,"Line should go\nthrough circles;\npanels should be\nidentical.",pos=4)
+plot(stn,which="salinity")
+lines(S,p,col=2)
+text(35.2,2500,"Line should go\nthrough circles;\npanels should be\nidentical.",pos=4)
+
+# 3. Examine all possibilities
+for (item in sort(c(q$data, q$dataDerived))) {
+    message(item)
+    plot(stn, which=item)
+    mtext(paste0(" which=",item),side=1,line=-1.0,col=2,font=2,cex=par("cex"))
+}
+
+dev.off()
+
