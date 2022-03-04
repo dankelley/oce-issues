f007a3c829b9feafc8c84bae75b0ac817d4032a4
dankelley <kelley.dan@gmail.com>
2022-03-04 09:10:55 -0400

    rough draft of read.ctd.aml()

--- 19xx/1924/02dk.R
+++ 19xx/1924/02dk.R
@@ -3,6 +3,7 @@ library(oce)
 file <- "Custom.export.026043_2021-07-21_17-36-45.txt"
 read.ctd.aml <- function(file, debug=getOption("oceDebug"))
 {
+    oceDebug(debug, "read.ctd.aml() {\n", unindent=1, style="bold")
     if (!missing(file) && is.character(file) && 0 == file.info(file)$size)
         stop("empty file")
     filename <- ""
@@ -17,38 +18,50 @@ read.ctd.aml <- function(file, debug=getOption("oceDebug"))
         open(file, "r")
         on.exit(close(file))
     }
-    res <- new("ctd")
     getMetadataItem <- function(lines, name, numeric=TRUE)
     {
-        l <- grep(paste0("^",name), lines)
+        l <- grep(paste0("^",name,"="), lines)
         if (length(l) > 0L) {
             # NOTE: we take first definition, ignoring others
             item <- lines[l[1]]
             res <- strsplit(lines[l], "=")[[1]][2]
             if (numeric)
                 res <- as.numeric(res)
+            else
+                res <- trimws(res)
         } else {
             NULL
         }
         res
     }
     lines <- readLines(file, encoding="UTF-8-BOM", warn=FALSE)
+    # FIXME: add other relevant metadata here.  This will require some
+    # familiarity with the typical contents of the metadata.  For example,
+    # I see 'SN' and 'BoardSN', and am inferring that we want to save
+    # the first, but maybe it's the second...
     longitude <- getMetadataItem(lines, "Longitude")
     latitude <- getMetadataItem(lines, "Latitude")
+    # I 
+    serialNumber <- getMetadataItem(lines, "SN")
+    Date <- getMetadataItem(lines, "Date", numeric=FALSE)
+    Time <- getMetadataItem(lines, "Time", numeric=FALSE)
+    time <- as.POSIXct(paste(Date, Time), tz="UTC")
     col.names <- strsplit(lines[1], ",")[[1]]
-    print(col.names)
     CommentsLine <- grep("^Comments=", lines)
     oceDebug(debug, "CommentsLine=", CommentsLine, "\n")
     data <- read.csv(text=lines, skip=CommentsLine+1, header=FALSE, col.names=col.names)
-    print(head(data))
     T <- data[["Temperature..C."]]
     C <- data[["Conductivity..mS.cm."]]
     p <- swPressure(data[["Depth..m."]])
     S <- swSCTp(conductivity=C, temperature=T, pressure=p, conductivityUnit="mS/cm", eos="unesco")
-    as.ctd(S, T, p, longitude=longitude, latitude=latitude)
+    rval <- as.ctd(S, T, p, longitude=longitude, latitude=latitude,
+        time=time, serialNumber=serialNumber)
+    rval@metadata$filename <- filename
+    oceDebug(debug, "} # read.ctd.aml() {\n", unindent=1, style="bold")
+    rval
 }
 
-ctd <- read.ctd.aml(file, debug=3)
+ctd <- read.ctd.aml(file)
 summary(ctd)
 if (!interactive()) png("02dk.png")
 plot(ctd)
