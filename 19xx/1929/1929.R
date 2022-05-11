f21f0bf842553df28ce74bed37916f448ca5eb94
dankelley <kelley.dan@gmail.com>
2022-05-11 13:00:09 -0300

    .

--- 19xx/1929/1929.R
+++ 19xx/1929/1929.R
@@ -2,7 +2,6 @@ library(oce)
 d <- read.ctd.ssda("14190549.csv")
 summary(d)
 plot(d, span=3000)
-
 # Finally, print some values for checking against the file.
 head(data.frame(S=d[["salinity"]], T=d[["temperature"]], p=d[["pressure"]]))
 
