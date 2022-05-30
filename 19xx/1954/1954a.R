75ced903baee38cf85ae85f3176ad0a033e72fed
dankelley <kelley.dan@gmail.com>
2022-05-30 11:25:58 -0300

    .

--- 19xx/1954/1954a.R
+++ 19xx/1954/1954a.R
@@ -1,7 +1,7 @@
 library(oce)
 f1 <- "S102791A002_Barrow_v2.ad2cp"
 #d1 <- read.adp.ad2cp(f1, debug=3)
-d1 <- read.adp.ad2cp(f1)
+d1 <- read.adp.ad2cp(f1, debug=3)
 str(d1@data$average,1)
 if (!interactive())
     png("1954a.png")
--- 19xx/1954/1954b.R
+++ 19xx/1954/1954b.R
@@ -1,3 +1,5 @@
+library(oce)
+
 f <- "S102791A002_Barrow_v2_avgd.ad2cp"
 
 # Detect headers by 3 byte match (to see if read.adp.ad2cp() is just haywire)
@@ -8,7 +10,6 @@ check3 <- sapply(seq_len(n-2),
         b[i]==as.raw(0xa5) && b[i+1]==as.raw(0x0a) && b[i+3]==as.raw(0x10))
 cat("Crude check suggests chunks start at cindex=", paste(which(check3)-1, collapse=" "), "\n")
 
-library(oce)
 f2 <- "S102791A002_Barrow_v2_avgd.ad2cp"
 try(d2 <- read.adp.ad2cp(f2, debug=3))
 
