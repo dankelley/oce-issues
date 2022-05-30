d935e7ee418054ae56fb31cd4a2f25a5d470f922
dankelley <kelley.dan@gmail.com>
2022-05-30 10:06:24 -0300

    plot magnetometer timeseries as a test

--- 19xx/1954/1954a.R
+++ 19xx/1954/1954a.R
@@ -1,4 +1,17 @@
 library(oce)
 f1 <- "S102791A002_Barrow_v2.ad2cp"
-try(d1 <- read.adp.ad2cp(f1, debug=3))
+#d1 <- read.adp.ad2cp(f1, debug=3)
+d1 <- read.adp.ad2cp(f1)
+str(d1@data$average,1)
+if (!interactive())
+    png("1954a.png")
+par(mfrow=c(3,1))
+with(d1@data$average,
+    {
+        oce.plot.ts(time, magnetometerx)
+        oce.plot.ts(time, magnetometery)
+        oce.plot.ts(time, magnetometerz)
+    })
+if (!interactive())
+    dev.off()
 
