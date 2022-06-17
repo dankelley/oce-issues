3ad2589b300eb102c4972b7eea08ecb650bd9431
dankelley <kelley.dan@gmail.com>
2022-06-17 13:02:16 -0300

    .

--- 19xx/1954/1954d.R
+++ 19xx/1954/1954d.R
@@ -5,13 +5,13 @@ A <- d1@data$burstAltimeterRaw
 z <- seq(0, by=A$altimeterRawSampleDistance, length=A$altimeterRawNumberOfSamples)
 
 if (!interactive())
-    png("1954d_%d.png")
+    png("1954d_%d.png", width=7, height=7, unit="in", res=250)
 I <- d1@data$burstAltimeterRaw$altimeterRawSamples
-imagep(A$time, z, I, zlim=c(4000,7000), ylab="Distance [m]")
+imagep(A$time, z, I, zlim=c(4000,7000), ylab="Distance [m]", decimate=FALSE)
 I2 <- apply(I, 2, median)
 peaks <- z[I2>6000]
 plot(z, I2, type="s", xlim=c(0,2), xlab="Distance [m]", ylab="Amplitude")
-mtext(paste0("Peaks at ", paste(peaks, collapse="m and"), "m"))
+mtext(paste0("Peaks at ", paste(peaks, collapse="m and "), "m"))
 grid()
 
 if (!interactive())
