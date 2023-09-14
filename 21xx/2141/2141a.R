cce6809b8c47b9a178985e21f6c0d032894793ef
dankelley <kelley.dan@gmail.com>
2023-09-14 14:25:24 -0300

    test that original-name lookup works

--- 21xx/2141/2141a.R
+++ 21xx/2141/2141a.R
@@ -1,4 +1,6 @@
 library(oce)
-d <- read.ctd.saiv("Tr1_St2_Graveneset.txt", debug=2)
+debug <- 0
+d <- read.ctd.saiv("Tr1_St2_Graveneset.txt", debug=debug)
+# ensure that original name worked correctly
+stopifnot(identical(d[["fluorescence"]], d[["F (µg/l)"]]))
 summary(d)
-
