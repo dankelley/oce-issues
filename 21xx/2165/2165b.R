c3434359eabf8fea545a859a790c26533cde18cb
dankelley <kelley.dan@gmail.com>
2023-10-31 08:15:12 -0300

    draw a guiding line; label y

--- 21xx/2165/2165b.R
+++ 21xx/2165/2165b.R
@@ -20,24 +20,54 @@ dd[["orientation"]] <- "downward"
 summary(dd)
 cat(vectorShow(dd[["cellSize"]]))
 cat(vectorShow(dd[["blankingDistance"]]))
-png("01_aqd_dk_%d.png")
+if (!interactive()) {
+    png("2165b_%d.png", unit = "in", res = 200, width = 7, height = 5)
+}
 par(mfrow = c(3, 1))
-oce.plot.ts(d[["time"]], d[["pressure"]])
-mtext(sprintf("cellSize=%.4fm", d[["cellSize"]]), adj = 1, col = 2)
-oce.plot.ts(d[["time"]], d[["temperature"]])
-oce.plot.ts(d[["time"]], d[["heading"]])
+oce.plot.ts(d[["time"]], d[["pressure"]], ylab = "Pressure [dbar]")
+label <- numberAsPOSIXct(1561170000) # chosen by eye
+abline(v = label, col = "magenta")
+oce.plot.ts(d[["time"]], d[["temperature"]], ylab = "Temperature [degC]")
+abline(v = label, col = "magenta")
+oce.plot.ts(d[["time"]], d[["heading"]], ylab = "Heading")
+abline(v = label, col = "magenta")
 ## plot(dd, which=1:3, zlim=c(-1, 1), col=oceColorsVelocity)
-par(mfrow = c(3, 1))
-imagep(d[["time"]], d[["distance"]], d[["v"]][, , 1], zlim = c(-1, 1), col = oceColorsVelocity)
-mtext(sprintf("cellSize=%.4fm", d[["cellSize"]]), adj = 1, col = 2)
-imagep(d[["time"]], d[["distance"]], d[["v"]][, , 2], zlim = c(-1, 1), col = oceColorsVelocity)
-imagep(d[["time"]], d[["distance"]], d[["v"]][, , 3], zlim = c(-1, 1), col = oceColorsVelocity)
 
 par(mfrow = c(3, 1))
-imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 1], col = oceColorsTurbo)
-mtext(sprintf("cellSize=%.4fm", d[["cellSize"]]), adj = 1, col = 2)
-imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 2], col = oceColorsTurbo)
-imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 3], col = oceColorsTurbo)
+
+imagep(d[["time"]], d[["distance"]], d[["v"]][, , 1],
+    ylab = "Distance [m]", zlim = c(-1, 1), col = oceColorsVelocity)
+abline(v = label, col = "magenta")
+mtext("v[, , 1]", adj = 0.5, cex = par("cex"))
+
+imagep(d[["time"]], d[["distance"]], d[["v"]][, , 2],
+    ylab = "Distance [m]", zlim = c(-1, 1), col = oceColorsVelocity)
+
+abline(v = label, col = "magenta")
+mtext("v[, , 2]", adj = 0.5, cex = par("cex"))
+
+imagep(d[["time"]], d[["distance"]], d[["v"]][, , 3],
+    ylab = "Distance [m]", zlim = c(-1, 1), col = oceColorsVelocity)
+abline(v = label, col = "magenta")
+mtext("v[, , 3]", adj = 0.5, cex = par("cex"))
+
+par(mfrow = c(3, 1))
+imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 1], col = oceColorsTurbo,
+    ylab="Distance [m]")
+mtext("a[, , 1]", adj = 0.5, cex = par("cex"))
+abline(v = label, col = "magenta")
+
+imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 2], col = oceColorsTurbo,
+    ylab="Distance [m]")
+mtext("a[, , 2]", adj = 0.5, cex = par("cex"))
+abline(v = label, col = "magenta")
+
+imagep(d[["time"]], d[["distance"]], d[["a", "numeric"]][, , 3], col = oceColorsTurbo,
+    ylab="Distance [m]")
+mtext("a[, , 3]", adj = 0.5, cex = par("cex"))
+abline(v = label, col = "magenta")
 
 ## plot(dd, which=5:7)
-dev.off()
+if (!interactive()) {
+    dev.off()
+}
