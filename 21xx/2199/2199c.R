a6b7fa110b5eb6e903faf1e35cf5bd43640e26e8
dankelley <kelley.dan@gmail.com>
2024-03-06 16:00:45 -0400

    demo gridCoarseness

--- 21xx/2199/2199c.R
+++ 21xx/2199/2199c.R
@@ -4,7 +4,7 @@ PNG <- function(name) {
         # https://codedocean.wordpress.com/2014/02/03/anti-aliasing-and-image-plots/
         png(name,
             type = "cairo", antialias = "none", family = "Arial",
-            unit = "in", width = 7, height = 3.5, res = 400, pointsize = 8
+            unit = "in", width = 7, height = 3.5, res = 200, pointsize = 8
         )
         par(mar = c(2, 2, 1, 1), mgp = c(2, 0.7, 0))
     }
@@ -21,19 +21,21 @@ lon <- seq(-130, -90, int)
 lat <- seq(50, 89.95, int)
 m <- matrix(lat, byrow = TRUE, nrow = length(lon), ncol = length(lat))
 
-PNG("2199c_default.png")
+PNG("2199c_%d.png")
 
-mapPlot(coastlineWorldMedium,
-    projection = "+proj=lcc +lat_1=30 +lat_2=45 +lon_0=-110",
-    longitudelim = c(-130, -100), latitudelim = c(70, 75)
-)
-g <- function(...) binMean2D(..., fill = TRUE)
-mapImage(
-    longitude = lon, latitude = lat, z = m,
-    filledContour = TRUE,
-    breaks = 256,
-    # col = function(n) paste0(oceColorsJet(n), "aa")
-    gridCoefficient = 20,
-    col = oceColorsJet
-)
+for (gridCoarseness in seq(5, 25, 5)) {
+    mapPlot(coastlineWorldMedium,
+        projection = "+proj=lcc +lat_1=30 +lat_2=45 +lon_0=-110",
+        longitudelim = c(-130, -100), latitudelim = c(60, 75)
+    )
+    g <- function(...) binMean2D(..., fill = TRUE)
+    mapImage(
+        longitude = lon, latitude = lat, z = m,
+        filledContour = TRUE,
+        breaks = 256,
+        gridCoarseness = gridCoarseness,
+        col = oceColorsJet
+    )
+    mtext(sprintf("gridCoarseness = %f", gridCoarseness))
+}
 GNP()
