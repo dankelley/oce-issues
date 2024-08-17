ad5864b7880cca4520ca4fbe65d99053975fb92b
dankelley <kelley.dan@gmail.com>
2024-08-17 09:59:18 -0300

    reformat

--- 05xx/562/562.R
+++ 05xx/562/562.R
@@ -1,7 +1,11 @@
 library(oce)
-d <- read.oce("~/Downloads/RSKtools/sample.rsk")
+f <- "/Users/kelley/git/oar_book/data/sample.rsk"
+d <- read.oce(f)
+summary(d)
+ctd <- as.ctd(salinity = d[["salinity"]], temperature = d[["temperature"]], pressure = d[["pressure"]])
+plot(ctd, which = 1, eos = "unesco")
+summary(d)
 if (!interactive()) png("562.png")
-plot(d, which=1)
-mtext(" EXPECT: plot with levels, maybe a calibration", font=2, col="purple", line=-1, adj=0)
+plot(ctd, which = 1, eos = "unesco")
+mtext(" EXPECT: plot with levels, maybe a calibration", font = 2, col = "purple", line = -1, adj = 0)
 if (!interactive()) dev.off()
-
