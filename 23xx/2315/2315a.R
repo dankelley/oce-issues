8799db4348a4ba131fc707ffc908556fcaa9dd43
dankelley <kelley.dan@gmail.com>
2025-05-28 13:01:00 -0300

    issue 2315 test file

--- 23xx/2315/2315a.R
+++ 23xx/2315/2315a.R
@@ -1,9 +1,10 @@
 library(oce)
 if (!file.exists("ad2cp.rds")) {
+    f <- "~/Downloads/S104098A002_2297d.ad2cp"
     if (file.exists(f)) {
-        f <- "~/Downloads/S104098A002_2297d.ad2cp"
         from <- 3300000
         to <- from + 100
+        # timing [s]: user=1.7, system=3.1, elapsed=4.8
         ad2cp <- read.adp.ad2cp(f, dataType = "average", from = from, to = to)
         saveRDS(ad2cp, file = "ad2cp.rds")
         message("created 'ad2cp.rds' file")
@@ -11,12 +12,24 @@ if (!file.exists("ad2cp.rds")) {
         stop("cannot run this script because \"", f, "\" does not exist")
     }
 }
-ad2cp <- readRDS("ad2cp.rds")
-xyz <- beamToXyz(ad2cp, debug = 2)
-zlim <- c(-1, 1) * quantile(abs(ad2cp[["v"]]), 0.95)
-if (!interactive()) png("2315a_beam.png", width = 7, height = 7, unit = "in", res = 300)
-plot(ad2cp, zlim = zlim)
+beam <- readRDS("ad2cp.rds")
+xyz <- beamToXyz(beam, debug = 2)
+enu <- xyzToEnu(xyz, debug = 2)
+zlim <- c(-1, 1) * quantile(abs(xyz[["v"]]), 0.95)
+
+if (!interactive()) png("2315a_angles.png", width = 7, height = 7, unit = "in", res = 300)
+plot(beam, which = "angles")
 if (!interactive()) dev.off()
+
+if (!interactive()) png("2315a_beam.png", width = 7, height = 7, unit = "in", res = 300)
+plot(beam, zlim = zlim)
+if (!interactive()) dev.off()
+
 if (!interactive()) png("2315a_xyz.png", width = 7, height = 7, unit = "in", res = 300)
 plot(xyz, zlim = zlim)
 if (!interactive()) dev.off()
+
+if (!interactive()) png("2315a_enu.png", width = 7, height = 7, unit = "in", res = 300)
+plot(enu, zlim = zlim)
+if (!interactive()) dev.off()
+
