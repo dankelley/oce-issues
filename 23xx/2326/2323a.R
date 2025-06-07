11c3cf81668698f3a01bc4eb1650bf4977e0017b
dankelley <kelley.dan@gmail.com>
2025-06-07 13:22:02 -0300

    some test plots

--- 23xx/2326/2323a.R
+++ 23xx/2326/2323a.R
@@ -5,4 +5,21 @@ if (file.exists(file)) {
     print(TOC)
     d <- read.adp.ad2cp(file, dataType = "averageAltimeter")
     print(str(d))
+    # imagep(d[["altimeterRawSamples"]])
+
+    # DK messing around to try to find a combination that will yield
+    # an image that seems to make sense.  I tried changing 'byrow' and 'nrow'
+    # below but I don't think any combination looked like clean data.
+    x <- d[["altimeterRawSamples"]]
+    dim <- dim(x)
+    xv <- as.vector(x)
+    X <- matrix(xv, byrow = FALSE, nrow = dim[1])
+    # X <- matrix(xv, byrow=FALSE, nrow=dim[2])
+    # X <- matrix(xv, byrow=!FALSE, nrow=dim[1])
+    # X <- matrix(xv, byrow=!FALSE, nrow=dim[2])
+    par(mfrow = c(2, 1))
+    imagep(x, mar = c(2, 2, 1, 1))
+    title("Original")
+    imagep(X, mar = c(2, 2, 1, 1))
+    title("Modified")
 }
