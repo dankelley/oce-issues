9b31256c8fb387638c84f7dd2fdea4327d7fcf24
dankelley <kelley.dan@gmail.com>
2026-04-02 06:30:51 -0300

    First test with halfway-working oce

--- 23xx/2368/ad2cp_0.R
+++ 23xx/2368/ad2cp_0.R
@@ -5,6 +5,7 @@ files <- c(
     "/Users/kelley/Downloads/S102791A003_Barrow_2022_0001_sub.ad2cp", # Clark's first file
     "/Users/kelley/Downloads/S102791A003_Barrow_2022_0005_sub.ad2cp" # Clark's second file
 )
+createPlots <- TRUE
 
 for (file in files[3]) {
     cat("file=", file, "\n", sep = "")
@@ -12,14 +13,35 @@ for (file in files[3]) {
     if (inherits(d, "try-error")) {
         message("FAILURE TO READ")
     } else {
-        if (FALSE) {
-            pngName <- paste0("ad2cp_", gsub(".*/", "", file), ".png")
-            png(pngName, unit = "in", width = 7, height = 7, pointsize = 9, res = 200)
-            par(mfrow = c(2, 2))
-            oce.plot.ts(d[["time"]], d[["v"]][, 1])
-            oce.plot.ts(d[["time"]], d[["v"]][, 2])
-            oce.plot.ts(d[["time"]], d[["v"]][, 3])
-            oce.plot.ts(d[["time"]], d[["v"]][, 4])
+        res <- 150
+        pointsize <- 14 # make big because with lots of panels, it defaults to too small
+        if (createPlots) {
+            pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_angles.png")
+            png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
+            par(mfrow = c(3, 1))
+            oce.plot.ts(d[["time"]], d[["heading"]])
+            oce.plot.ts(d[["time"]], d[["pitch"]])
+            oce.plot.ts(d[["time"]], d[["roll"]])
+            dev.off()
+            message("Plotted to ", pngName)
+            pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_v.png")
+            png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
+            par(mfrow = c(4, 1))
+            ylim <- c(-1, 1) * max(abs(d[["v"]]))
+            oce.plot.ts(d[["time"]], d[["v"]][, 1], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["v"]][, 2], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["v"]][, 3], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["v"]][, 4], ylim = ylim)
+            dev.off()
+            message("Plotted to ", pngName)
+            pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_distance.png")
+            png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
+            par(mfrow = c(4, 1))
+            ylim <- range(d[["distance"]])
+            oce.plot.ts(d[["time"]], d[["distance"]][, 1], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["distance"]][, 2], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["distance"]][, 3], ylim = ylim)
+            oce.plot.ts(d[["time"]], d[["distance"]][, 4], ylim = ylim)
             dev.off()
             message("Plotted to ", pngName)
         }
