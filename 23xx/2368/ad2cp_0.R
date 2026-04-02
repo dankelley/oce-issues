d8f30ec35ff1107b22b501199a1ae508e124826b
dankelley <kelley.dan@gmail.com>
2026-04-02 08:22:32 -0300

    more plots

--- 23xx/2368/ad2cp_0.R
+++ 23xx/2368/ad2cp_0.R
@@ -1,5 +1,6 @@
+# vim:textwidth=80:expandtab:shiftwidth=4:softtabstop=4:foldmethod=marker
 library(oce)
-# source("~/git/oce/R/adp.nortek.ad2cp.bottom.track.R")
+debug <- 0
 files <- c(
     "/Users/kelley/Downloads/104561_20250219T093916UTC.AD2CP", # issue's file
     "/Users/kelley/Downloads/S102791A003_Barrow_2022_0001_sub.ad2cp", # Clark's first file
@@ -9,13 +10,23 @@ createPlots <- TRUE
 
 for (file in files[3]) {
     cat("file=", file, "\n", sep = "")
-    d <- try(read.adp.ad2cp(file, dataType = "bottomTrack", debug = 2))
+    d <- try(read.adp.ad2cp(file, dataType = "bottomTrack", debug = debug))
     if (inherits(d, "try-error")) {
         message("FAILURE TO READ")
     } else {
         res <- 150
         pointsize <- 14 # make big because with lots of panels, it defaults to too small
         if (createPlots) {
+            # {{{ p,t
+            pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_p_and_T.png")
+            png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
+            par(mfrow = c(2, 1))
+            oce.plot.ts(d[["time"]], d[["pressure"]])
+            oce.plot.ts(d[["time"]], d[["temperature"]])
+            dev.off()
+            cat("Plotted pressure,temperature in", pngName, "\n")
+            # }}}
+            # {{{ head,pitch,roll
             pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_angles.png")
             png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
             par(mfrow = c(3, 1))
@@ -23,7 +34,9 @@ for (file in files[3]) {
             oce.plot.ts(d[["time"]], d[["pitch"]])
             oce.plot.ts(d[["time"]], d[["roll"]])
             dev.off()
-            message("Plotted to ", pngName)
+            cat("Plotted angles in", pngName, "\n")
+            # }}}
+            # {{{ v
             pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_v.png")
             png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
             par(mfrow = c(4, 1))
@@ -33,7 +46,9 @@ for (file in files[3]) {
             oce.plot.ts(d[["time"]], d[["v"]][, 3], ylim = ylim)
             oce.plot.ts(d[["time"]], d[["v"]][, 4], ylim = ylim)
             dev.off()
-            message("Plotted to ", pngName)
+            cat("Plotted v in", pngName, "\n")
+            # }}}
+            # {{{ distance
             pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_distance.png")
             png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
             par(mfrow = c(4, 1))
@@ -43,7 +58,8 @@ for (file in files[3]) {
             oce.plot.ts(d[["time"]], d[["distance"]][, 3], ylim = ylim)
             oce.plot.ts(d[["time"]], d[["distance"]][, 4], ylim = ylim)
             dev.off()
-            message("Plotted to ", pngName)
+            cat("Plotted distance in", pngName, "\n")
+            # }}}
         }
     }
 }
