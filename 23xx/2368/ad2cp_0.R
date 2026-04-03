8fe2db45a44be1951049916b2f4de94e0267deab
dankelley <kelley.dan@gmail.com>
2026-04-03 18:02:36 -0300

    use new plot.adp() capabilities for single-cell

--- 23xx/2368/ad2cp_0.R
+++ 23xx/2368/ad2cp_0.R
@@ -1,6 +1,6 @@
 # vim:textwidth=120:expandtab:shiftwidth=4:softtabstop=4:foldmethod=marker
 library(oce)
-debug <- 0
+debug <- 1
 files <- c(
     "/Users/kelley/Downloads/104561_20250219T093916UTC.AD2CP", # ad2cp_1.R zooms plots
     "/Users/kelley/Downloads/S102791A003_Barrow_2022_0001_sub.ad2cp", # Clark's first file
@@ -8,7 +8,7 @@ files <- c(
 )
 createPlots <- TRUE
 
-for (file in files) {
+for (file in files[3]) {
     cat("file=", file, "\n", sep = "")
     d <- try(read.adp.ad2cp(file, dataType = "bottomTrack", debug = debug))
     if (inherits(d, "try-error")) {
@@ -31,11 +31,7 @@ for (file in files) {
             # {{{ head,pitch,roll
             pngName <- paste0("ad2cp_", gsub(".*/", "", file), "_angles.png")
             png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
-            par(mfrow = c(3, 1))
-            oce.plot.ts(d[["time"]], d[["heading"]])
-            oce.plot.ts(d[["time"]], d[["pitch"]])
-            oce.plot.ts(d[["time"]], d[["roll"]])
-            dev.off()
+            plot(d, "angles")
             cat("Plotted angles in", pngName, "\n")
             # }}}
             # {{{ v
@@ -43,10 +39,7 @@ for (file in files) {
             png(pngName, unit = "in", width = 7, height = 7, pointsize = pointsize, res = res)
             par(mfrow = c(4, 1))
             ylim <- c(-1, 1) * max(abs(d[["v"]]))
-            oce.plot.ts(d[["time"]], d[["v"]][, 1], ylim = ylim)
-            oce.plot.ts(d[["time"]], d[["v"]][, 2], ylim = ylim)
-            oce.plot.ts(d[["time"]], d[["v"]][, 3], ylim = ylim)
-            oce.plot.ts(d[["time"]], d[["v"]][, 4], ylim = ylim)
+            plot(d, ylim = ylim)
             dev.off()
             cat("Plotted v in", pngName, "\n")
             # }}}
